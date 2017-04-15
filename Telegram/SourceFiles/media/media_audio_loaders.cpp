ked33/tdesktop

commit 654e892e1c16502221234c3545dcb5c89ffa7d0c

Remove Q_OBJECT dependency from ApiWrap.
Also remove it from SingleDelayedCall -> SingleQueuedInvocation.

Telegram/SourceFiles/media/media_audio_loaders.cpp
@@ -27,7 +27,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 namespace Media {
 namespace Player {
 
-Loaders::Loaders(QThread *thread) : _fromVideoNotify(this, "onVideoSoundAdded") {
+Loaders::Loaders(QThread *thread) : _fromVideoNotify([this] { videoSoundAdded(); }) {
 	moveToThread(thread);
 	connect(thread, SIGNAL(started()), this, SLOT(onInit()));
 	connect(thread, SIGNAL(finished()), this, SLOT(deleteLater()));
@@ -59,7 +59,7 @@ void Loaders::stopFromVideo() {
 	startFromVideo(0);
 }
 
-void Loaders::onVideoSoundAdded() {
+void Loaders::videoSoundAdded() {
 	bool waitingAndAdded = false;
 	{
 		QMutexLocker lock(&_fromVideoMutex);

Telegram/SourceFiles/apiwrap.cpp
@@ -33,23 +33,29 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "window/themes/window_theme.h"
 #include "window/notifications_manager.h"
 
+namespace {
+
+constexpr auto kReloadChannelMembersTimeout = 1000; // 1 second wait before reload members in channel after adding
+
+} // namespace
+
 ApiWrap::ApiWrap(QObject *parent) : QObject(parent)
-, _messageDataResolveDelayed(new SingleDelayedCall(this, "resolveMessageDatas")) {
+, _messageDataResolveDelayed([this] { resolveMessageDatas(); }) {
 	Window::Theme::Background()->start();
 
-	connect(&_webPagesTimer, SIGNAL(timeout()), this, SLOT(resolveWebPages()));
-	connect(&_draftsSaveTimer, SIGNAL(timeout()), this, SLOT(saveDraftsToCloud()));
+	connect(&_webPagesTimer, &QTimer::timeout, this, [this] { resolveWebPages(); });
+	connect(&_draftsSaveTimer, &QTimer::timeout, this, [this] { saveDraftsToCloud(); });
 }
 
 void ApiWrap::init() {
 }
 
 void ApiWrap::requestMessageData(ChannelData *channel, MsgId msgId, RequestMessageDataCallback callback) {
-	MessageDataRequest &req(channel ? _channelMessageDataRequests[channel][msgId] : _messageDataRequests[msgId]);
+	auto &req = (channel ? _channelMessageDataRequests[channel][msgId] : _messageDataRequests[msgId]);
 	if (callback) {
 		req.callbacks.append(callback);
 	}
-	if (!req.req) _messageDataResolveDelayed->call();
+	if (!req.req) _messageDataResolveDelayed.call();
 }
 
 ApiWrap::MessageIds ApiWrap::collectMessageIds(const MessageDataRequests &requests) {
@@ -202,7 +208,7 @@ void ApiWrap::gotChatFull(PeerData *peer, const MTPmessages_ChatFull &result, mt
 				if (auto user = App::userLoaded(b.vuser_id.v)) {
 					user->setBotInfo(item);
 					App::clearPeerUpdated(user);
-					emit fullPeerUpdated(user);
+					fullPeerUpdated().notify(user);
 				}
 			} break;
 			}
@@ -274,7 +280,7 @@ void ApiWrap::gotChatFull(PeerData *peer, const MTPmessages_ChatFull &result, mt
 				if (auto user = App::userLoaded(b.vuser_id.v)) {
 					user->setBotInfo(item);
 					App::clearPeerUpdated(user);
-					emit fullPeerUpdated(user);
+					fullPeerUpdated().notify(user);
 				}
 			} break;
 			}
@@ -320,7 +326,7 @@ void ApiWrap::gotChatFull(PeerData *peer, const MTPmessages_ChatFull &result, mt
 		requestPeer(peer);
 	}
 	App::clearPeerUpdated(peer);
-	emit fullPeerUpdated(peer);
+	fullPeerUpdated().notify(peer);
 }
 
 void ApiWrap::gotUserFull(PeerData *peer, const MTPUserFull &result, mtpRequestId req) {
@@ -353,7 +359,7 @@ void ApiWrap::gotUserFull(PeerData *peer, const MTPUserFull &result, mtpRequestI
 		}
 	}
 	App::clearPeerUpdated(peer);
-	emit fullPeerUpdated(peer);
+	fullPeerUpdated().notify(peer);
 }
 
 bool ApiWrap::gotPeerFullFailed(PeerData *peer, const RPCError &error) {
@@ -563,7 +569,7 @@ void ApiWrap::lastParticipantsDone(ChannelData *peer, const MTPchannels_ChannelP
 	}
 
 	peer->mgInfo->botStatus = botStatus;
-	if (App::main()) emit fullPeerUpdated(peer);
+	if (App::main()) fullPeerUpdated().notify(peer);
 }
 
 bool ApiWrap::lastParticipantsFail(ChannelData *peer, const RPCError &error, mtpRequestId req) {
@@ -669,7 +675,7 @@ void ApiWrap::kickParticipantDone(KickRequest kick, const MTPUpdates &result, mt
 		}
 	}
 	Notify::peerUpdatedDelayed(kick.first, Notify::PeerUpdate::Flag::MembersChanged);
-	emit fullPeerUpdated(kick.first);
+	fullPeerUpdated().notify(kick.first);
 }
 
 bool ApiWrap::kickParticipantFail(KickRequest kick, const RPCError &error, mtpRequestId req) {
@@ -1345,10 +1351,10 @@ void ApiWrap::resolveWebPages() {
 	if (m < INT_MAX) _webPagesTimer.start(m * 1000);
 }
 
-void ApiWrap::delayedRequestParticipantsCount() {
-	if (App::main() && App::main()->peer() && App::main()->peer()->isChannel()) {
-		requestFullPeer(App::main()->peer());
-	}
+void ApiWrap::requestParticipantsCountDelayed(ChannelData *channel) {
+	QTimer::singleShot(kReloadChannelMembersTimeout, this, [this, channel] {
+		channel->updateFull(true);
+	});
 }
 
 void ApiWrap::gotWebPages(ChannelData *channel, const MTPmessages_Messages &msgs, mtpRequestId req) {

Telegram/SourceFiles/apiwrap.h
@@ -35,8 +35,6 @@ inline const MTPVector<MTPChat> *getChatsFromMessagesChats(const MTPmessages_Cha
 } // namespace Api
 
 class ApiWrap : public QObject, public RPCSender {
-	Q_OBJECT
-
 public:
 	ApiWrap(QObject *parent);
 	void init();
@@ -49,6 +47,7 @@ public:
 	void requestPeers(const QList<PeerData*> &peers);
 	void requestLastParticipants(ChannelData *peer, bool fromStart = true);
 	void requestBots(ChannelData *peer);
+	void requestParticipantsCountDelayed(ChannelData *channel);
 
 	void processFullPeer(PeerData *peer, const MTPmessages_ChatFull &result);
 	void processFullPeer(PeerData *peer, const MTPUserFull &result);
@@ -73,6 +72,7 @@ public:
 	void exportInviteLink(PeerData *peer);
 	void requestNotifySetting(PeerData *peer);
 
+	void saveDraftsToCloud();
 	void saveDraftToCloudDelayed(History *history);
 	bool hasUnsavedDrafts() const;
 
@@ -80,21 +80,16 @@ public:
 	void handlePrivacyChange(mtpTypeId keyTypeId, const MTPVector<MTPPrivacyRule> &rules);
 	int onlineTillFromStatus(const MTPUserStatus &status, int currentOnlineTill);
 
+	base::Observable<PeerData*> &fullPeerUpdated() {
+		return _fullPeerUpdated;
+	}
+
 	~ApiWrap();
-
-signals:
-	void fullPeerUpdated(PeerData *peer);
-
-public slots:
-	void resolveMessageDatas();
-	void resolveWebPages();
-
-	void delayedRequestParticipantsCount();
-	void saveDraftsToCloud();
 
 private:
 	void updatesReceived(const MTPUpdates &updates);
 
+	void resolveMessageDatas();
 	void gotMessageDatas(ChannelData *channel, const MTPmessages_Messages &result, mtpRequestId req);
 	struct MessageDataRequest {
 		using Callbacks = QList<RequestMessageDataCallback>;
@@ -105,7 +100,7 @@ private:
 	MessageDataRequests _messageDataRequests;
 	typedef QMap<ChannelData*, MessageDataRequests> ChannelMessageDataRequests;
 	ChannelMessageDataRequests _channelMessageDataRequests;
-	SingleDelayedCall *_messageDataResolveDelayed;
+	SingleQueuedInvokation _messageDataResolveDelayed;
 	typedef QVector<MTPint> MessageIds;
 	MessageIds collectMessageIds(const MessageDataRequests &requests);
 	MessageDataRequests *messageDataRequests(ChannelData *channel, bool onlyExisting = false);
@@ -138,6 +133,7 @@ private:
 	typedef QMap<ChannelData*, mtpRequestId> SelfParticipantRequests;
 	SelfParticipantRequests _selfParticipantRequests;
 
+	void resolveWebPages();
 	void gotWebPages(ChannelData *channel, const MTPmessages_Messages &result, mtpRequestId req);
 	typedef QMap<WebPageData*, mtpRequestId> WebPagesPending;
 	WebPagesPending _webPagesPending;
@@ -190,5 +186,7 @@ private:
 	mtpRequestId _contactsStatusesRequestId = 0;
 	void contactsStatusesDone(const MTPVector<MTPContactStatus> &result);
 	bool contactsStatusesFail(const RPCError &error);
+
+	base::Observable<PeerData*> _fullPeerUpdated;
 
 };

Telegram/SourceFiles/app.cpp
@@ -2466,7 +2466,9 @@ namespace {
 		if (auto apiwrap = api()) {
 			if (apiwrap->hasUnsavedDrafts()) {
 				apiwrap->saveDraftsToCloud();
-				QTimer::singleShot(SaveDraftBeforeQuitTimeout, QCoreApplication::instance(), SLOT(quit()));
+				QTimer::singleShot(SaveDraftBeforeQuitTimeout, [] {
+					QCoreApplication::quit();
+				});
 				return;
 			}
 		}

Telegram/SourceFiles/application.cpp
@@ -26,7 +26,6 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "storage/localstorage.h"
 #include "autoupdater.h"
 #include "window/notifications_manager.h"
-#include "base/task_queue.h"
 #include "messenger.h"
 
 namespace {
@@ -345,10 +344,6 @@ void Application::closeApplication() {
 	}
 	_updateThread = 0;
 #endif // !TDESKTOP_DISABLE_AUTOUPDATE
-}
-
-void Application::onMainThreadTask() {
-	base::TaskQueue::ProcessMainTasks();
 }
 
 #ifndef TDESKTOP_DISABLE_AUTOUPDATE

Telegram/SourceFiles/application.h
@@ -51,8 +51,6 @@ public slots:
 	void startApplication(); // will be done in exec()
 	void closeApplication(); // will be done in aboutToQuit()
 
-	void onMainThreadTask();
-
 private:
 	typedef QPair<QLocalSocket*, QByteArray> LocalClient;
 	typedef QList<LocalClient> LocalClients;

Telegram/SourceFiles/boxes/add_contact_box.cpp
@@ -1060,7 +1060,7 @@ bool EditChannelBox::onSaveFail(const RPCError &error, mtpRequestId req) {
 		if (err == qstr("CHAT_ABOUT_NOT_MODIFIED")) {
 			if (_channel->setAbout(_sentDescription)) {
 				if (App::api()) {
-					emit App::api()->fullPeerUpdated(_channel);
+					App::api()->fullPeerUpdated().notify(_channel);
 				}
 			}
 			saveSign();
@@ -1090,7 +1090,7 @@ void EditChannelBox::onSaveDescriptionDone(const MTPBool &result) {
 	_saveDescriptionRequestId = 0;
 	if (_channel->setAbout(_sentDescription)) {
 		if (App::api()) {
-			emit App::api()->fullPeerUpdated(_channel);
+			App::api()->fullPeerUpdated().notify(_channel);
 		}
 	}
 	saveSign();

Telegram/SourceFiles/boxes/confirm_box.cpp
@@ -312,25 +312,21 @@ void ConvertToSupergroupBox::convertToSupergroup() {
 void ConvertToSupergroupBox::convertDone(const MTPUpdates &updates) {
 	Ui::hideLayer();
 	App::main()->sentUpdatesReceived(updates);
-	const QVector<MTPChat> *v = 0;
-	switch (updates.type()) {
-	case mtpc_updates: v = &updates.c_updates().vchats.v; break;
-	case mtpc_updatesCombined: v = &updates.c_updatesCombined().vchats.v; break;
-	default: LOG(("API Error: unexpected update cons %1 (ConvertToSupergroupBox::convertDone)").arg(updates.type())); break;
-	}
 
-	PeerData *peer = 0;
-	if (v && !v->isEmpty()) {
-		for (int32 i = 0, l = v->size(); i < l; ++i) {
-			if (v->at(i).type() == mtpc_channel) {
-				peer = App::channel(v->at(i).c_channel().vid.v);
-				Ui::showPeerHistory(peer, ShowAtUnreadMsgId);
-				QTimer::singleShot(ReloadChannelMembersTimeout, App::api(), SLOT(delayedRequestParticipantsCount()));
+	auto handleChats = [](auto &mtpChats) {
+		for_const (auto &mtpChat, mtpChats.v) {
+			if (mtpChat.type() == mtpc_channel) {
+				auto channel = App::channel(mtpChat.c_channel().vid.v);
+				Ui::showPeerHistory(channel, ShowAtUnreadMsgId);
+				App::api()->requestParticipantsCountDelayed(channel);
 			}
 		}
-	}
-	if (!peer) {
-		LOG(("API Error: channel not found in updates (ProfileInner::migrateDone)"));
+	};
+
+	switch (updates.type()) {
+	case mtpc_updates: handleChats(updates.c_updates().vchats); break;
+	case mtpc_updatesCombined: handleChats(updates.c_updatesCombined().vchats); break;
+	default: LOG(("API Error: unexpected update cons %1 (ConvertToSupergroupBox::convertDone)").arg(updates.type())); break;
 	}
 }
 

commit 07b58bacf3fc6f8c7ea23a959460c3ff35758cba

Fix crash in videoplayer audio stream loader.
Regression was introduced in 654e892e1c.

Telegram/SourceFiles/media/media_audio_loaders.cpp
@@ -29,6 +29,7 @@ namespace Player {
 
 Loaders::Loaders(QThread *thread) : _fromVideoNotify([this] { videoSoundAdded(); }) {
 	moveToThread(thread);
+	_fromVideoNotify.moveToThread(thread);
 	connect(thread, SIGNAL(started()), this, SLOT(onInit()));
 	connect(thread, SIGNAL(finished()), this, SLOT(deleteLater()));
 }