ked33/tdesktop

commit ccfe9497f4ff8959a4df82ac7340a39c84bdadcb

Display round video messages using HistoryGif.
Use autodownload and autoplay options from GIFs.
Also improve EditCaptionBox code.

Telegram/SourceFiles/history/history_item.cpp
@@ -756,20 +756,19 @@ void HistoryItem::setId(MsgId newId) {
 bool HistoryItem::canEdit(const QDateTime &cur) const {
 	auto messageToMyself = (_history->peer->id == AuthSession::CurrentUserPeerId());
 	auto messageTooOld = messageToMyself ? false : (date.secsTo(cur) >= Global::EditTimeLimit());
-	if (id < 0 || messageTooOld) return false;
+	if (id < 0 || messageTooOld) {
+		return false;
+	}
 
 	if (auto msg = toHistoryMessage()) {
-		if (msg->Has<HistoryMessageVia>() || msg->Has<HistoryMessageForwarded>()) return false;
+		if (msg->Has<HistoryMessageVia>() || msg->Has<HistoryMessageForwarded>()) {
+			return false;
+		}
 
 		if (auto media = msg->getMedia()) {
-			auto type = media->type();
-			if (type != MediaTypePhoto &&
-				type != MediaTypeVideo &&
-				type != MediaTypeFile &&
-				type != MediaTypeGif &&
-				type != MediaTypeMusicFile &&
-				type != MediaTypeVoiceFile &&
-				type != MediaTypeWebPage) {
+			if (media->canEditCaption()) {
+				return true;
+			} else if (media->type() != MediaTypeWebPage) {
 				return false;
 			}
 		}
@@ -785,8 +784,12 @@ bool HistoryItem::canEdit(const QDateTime &cur) const {
 bool HistoryItem::canDeleteForEveryone(const QDateTime &cur) const {
 	auto messageToMyself = (_history->peer->id == AuthSession::CurrentUserPeerId());
 	auto messageTooOld = messageToMyself ? false : (date.secsTo(cur) >= Global::EditTimeLimit());
-	if (id < 0 || messageToMyself || messageTooOld) return false;
-	if (history()->peer->isChannel()) return false;
+	if (id < 0 || messageToMyself || messageTooOld) {
+		return false;
+	}
+	if (history()->peer->isChannel()) {
+		return false;
+	}
 
 	if (auto msg = toHistoryMessage()) {
 		return !isPost() && out();
@@ -803,21 +806,31 @@ bool HistoryItem::unread() const {
 
 	if (out()) {
 		// Outgoing messages in converted chats are always read.
-		if (history()->peer->migrateTo()) return false;
+		if (history()->peer->migrateTo()) {
+			return false;
+		}
 
 		if (id > 0) {
-			if (id < history()->outboxReadBefore) return false;
+			if (id < history()->outboxReadBefore) {
+				return false;
+			}
 			if (auto user = history()->peer->asUser()) {
-				if (user->botInfo) return false;
+				if (user->botInfo) {
+					return false;
+				}
 			} else if (auto channel = history()->peer->asChannel()) {
-				if (!channel->isMegagroup()) return false;
+				if (!channel->isMegagroup()) {
+					return false;
+				}
 			}
 		}
 		return true;
 	}
 
 	if (id > 0) {
-		if (id < history()->inboxReadBefore) return false;
+		if (id < history()->inboxReadBefore) {
+			return false;
+		}
 		return true;
 	}
 	return (_flags & MTPDmessage_ClientFlag::f_clientside_unread);
@@ -867,11 +880,15 @@ void HistoryItem::setUnreadBarFreezed() {
 void HistoryItem::clipCallback(Media::Clip::Notification notification) {
 	using namespace Media::Clip;
 
-	HistoryMedia *media = getMedia();
-	if (!media) return;
+	auto media = getMedia();
+	if (!media) {
+		return;
+	}
 
-	Reader *reader = media ? media->getClipReader() : 0;
-	if (!reader) return;
+	auto reader = media ? media->getClipReader() : nullptr;
+	if (!reader) {
+		return;
+	}
 
 	switch (notification) {
 	case NotificationReinit: {
@@ -903,7 +920,9 @@ void HistoryItem::clipCallback(Media::Clip::Notification notification) {
 
 void HistoryItem::recountDisplayDate() {
 	bool displayingDate = ([this]() {
-		if (isEmpty()) return false;
+		if (isEmpty()) {
+			return false;
+		}
 
 		if (auto previous = previousItem()) {
 			return previous->isEmpty() || (previous->date.date() != date.date());
@@ -930,7 +949,9 @@ QString HistoryItem::notificationText() const {
 	};
 
 	auto result = getText();
-	if (result.size() > 0xFF) result = result.mid(0, 0xFF) + qsl("...");
+	if (result.size() > 0xFF) {
+		result = result.mid(0, 0xFF) + qsl("...");
+	}
 	return result;
 }
 

Telegram/SourceFiles/boxes/boxes.style
@@ -484,11 +484,6 @@ autoDownloadTopDelta: 10px;
 autoDownloadTitlePosition: point(23px, 18px);
 autoDownloadTitleFont: font(15px semibold);
 
-editTextArea: InputField(defaultInputField) {
-	textMargins: margins(1px, 26px, 1px, 4px);
-	heightMax: 276px;
-}
-
 confirmCaptionArea: InputField(defaultInputField) {
 	textMargins: margins(1px, 26px, 1px, 4px);
 	heightMax: 78px;

Telegram/SourceFiles/boxes/connection_box.cpp
@@ -269,10 +269,9 @@ void AutoDownloadBox::onSave() {
 		bool enabledGroups = ((cAutoDownloadAudio() & dbiadNoGroups) && !(autoDownloadAudio & dbiadNoGroups));
 		cSetAutoDownloadAudio(autoDownloadAudio);
 		if (enabledPrivate || enabledGroups) {
-			const DocumentsData &data(App::documentsData());
-			for (DocumentsData::const_iterator i = data.cbegin(), e = data.cend(); i != e; ++i) {
-				if (i.value()->voice()) {
-					i.value()->automaticLoadSettingsChanged();
+			for (auto document : App::documentsData()) {
+				if (document->voice()) {
+					document->automaticLoadSettingsChanged();
 				}
 			}
 		}
@@ -284,10 +283,9 @@ void AutoDownloadBox::onSave() {
 		bool enabledGroups = ((cAutoDownloadGif() & dbiadNoGroups) && !(autoDownloadGif & dbiadNoGroups));
 		cSetAutoDownloadGif(autoDownloadGif);
 		if (enabledPrivate || enabledGroups) {
-			const DocumentsData &data(App::documentsData());
-			for (DocumentsData::const_iterator i = data.cbegin(), e = data.cend(); i != e; ++i) {
-				if (i.value()->isAnimation()) {
-					i.value()->automaticLoadSettingsChanged();
+			for (auto document : App::documentsData()) {
+				if (document->isAnimation()) {
+					document->automaticLoadSettingsChanged();
 				}
 			}
 		}

Telegram/SourceFiles/boxes/send_files_box.cpp
@@ -450,46 +450,46 @@ void SendFilesBox::closeHook() {
 	}
 }
 
-EditCaptionBox::EditCaptionBox(QWidget*, HistoryItem *msg)
-: _msgId(msg->fullId()) {
+EditCaptionBox::EditCaptionBox(QWidget*, HistoryMedia *media, FullMsgId msgId) : _msgId(msgId) {
+	Expects(media->canEditCaption());
+
 	QSize dimensions;
 	ImagePtr image;
 	QString caption;
 	DocumentData *doc = nullptr;
-	if (auto media = msg->getMedia()) {
-		auto t = media->type();
-		switch (t) {
-		case MediaTypeGif: {
-			_animated = true;
-			doc = static_cast<HistoryGif*>(media)->getDocument();
-			dimensions = doc->dimensions;
-			image = doc->thumb;
-		} break;
 
-		case MediaTypePhoto: {
-			_photo = true;
-			PhotoData *photo = static_cast<HistoryPhoto*>(media)->photo();
-			dimensions = QSize(photo->full->width(), photo->full->height());
-			image = photo->full;
-		} break;
+	switch (media->type()) {
+	case MediaTypeGif: {
+		_animated = true;
+		doc = static_cast<HistoryGif*>(media)->getDocument();
+		dimensions = doc->dimensions;
+		image = doc->thumb;
+	} break;
 
-		case MediaTypeVideo: {
-			_animated = true;
-			doc = static_cast<HistoryVideo*>(media)->getDocument();
-			dimensions = doc->dimensions;
-			image = doc->thumb;
-		} break;
+	case MediaTypePhoto: {
+		_photo = true;
+		auto photo = static_cast<HistoryPhoto*>(media)->photo();
+		dimensions = QSize(photo->full->width(), photo->full->height());
+		image = photo->full;
+	} break;
 
-		case MediaTypeFile:
-		case MediaTypeMusicFile:
-		case MediaTypeVoiceFile: {
-			_doc = true;
-			doc = static_cast<HistoryDocument*>(media)->getDocument();
-			image = doc->thumb;
-		} break;
-		}
-		caption = media->getCaption().text;
+	case MediaTypeVideo: {
+		_animated = true;
+		doc = static_cast<HistoryVideo*>(media)->getDocument();
+		dimensions = doc->dimensions;
+		image = doc->thumb;
+	} break;
+
+	case MediaTypeFile:
+	case MediaTypeMusicFile:
+	case MediaTypeVoiceFile: {
+		_doc = true;
+		doc = static_cast<HistoryDocument*>(media)->getDocument();
+		image = doc->thumb;
+	} break;
 	}
+	caption = media->getCaption().text;
+
 	if ((!_animated && (dimensions.isEmpty() || doc)) || image->isNull()) {
 		_animated = false;
 		if (image->isNull()) {
@@ -563,17 +563,11 @@ EditCaptionBox::EditCaptionBox(QWidget*, HistoryItem *msg)
 		_thumb = App::pixmapFromImageInPlace(_thumb.toImage().scaled(_thumbw * cIntRetinaFactor(), _thumbh * cIntRetinaFactor(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
 		_thumb.setDevicePixelRatio(cRetinaFactor());
 	}
-	if (_animated || _photo || _doc) {
-		_field.create(this, st::confirmCaptionArea, lang(lng_photo_caption), caption);
-		_field->setMaxLength(MaxPhotoCaption);
-		_field->setCtrlEnterSubmit(Ui::CtrlEnterSubmit::Both);
-	} else {
-		auto original = msg->originalText();
-		auto text = textApplyEntities(original.text, original.entities);
-		_field.create(this, st::editTextArea, lang(lng_photo_caption), text);
-//		_field->setMaxLength(MaxMessageSize); // entities can make text in input field larger but still valid
-		_field->setCtrlEnterSubmit(cCtrlEnter() ? Ui::CtrlEnterSubmit::CtrlEnter : Ui::CtrlEnterSubmit::Enter);
-	}
+	t_assert(_animated || _photo || _doc);
+
+	_field.create(this, st::confirmCaptionArea, lang(lng_photo_caption), caption);
+	_field->setMaxLength(MaxPhotoCaption);
+	_field->setCtrlEnterSubmit(Ui::CtrlEnterSubmit::Both);
 }
 
 void EditCaptionBox::prepareGifPreview(DocumentData *document) {
@@ -611,20 +605,6 @@ void EditCaptionBox::clipCallback(Media::Clip::Notification notification) {
 		}
 	} break;
 	}
-}
-
-bool EditCaptionBox::canEdit(HistoryItem *message) {
-	if (auto media = message->getMedia()) {
-		switch (media->type()) {
-		case MediaTypeGif:
-		case MediaTypePhoto:
-		case MediaTypeVideo:
-		case MediaTypeFile:
-		case MediaTypeMusicFile:
-		case MediaTypeVoiceFile: return true;
-		}
-	}
-	return false;
 }
 
 void EditCaptionBox::prepare() {

Telegram/SourceFiles/boxes/send_files_box.h
@@ -115,8 +115,7 @@ class EditCaptionBox : public BoxContent, public RPCSender {
 	Q_OBJECT
 
 public:
-	EditCaptionBox(QWidget*, HistoryItem *msg);
-	static bool canEdit(HistoryItem *message);
+	EditCaptionBox(QWidget*, HistoryMedia *media, FullMsgId msgId);
 
 public slots:
 	void onCaptionResized();

Telegram/SourceFiles/history/history_media.h
@@ -171,6 +171,10 @@ public:
 		return false;
 	}
 
+	virtual bool canEditCaption() const {
+		return false;
+	}
+
 	// Sometimes click on media in message is overloaded by the messsage:
 	// (for example it can open a link or a game instead of opening media)
 	// But the overloading click handler should be used only when media

Telegram/SourceFiles/history/history_media_types.h
@@ -175,6 +175,9 @@ public:
 	bool skipBubbleTail() const override {
 		return isBubbleBottom() && _caption.isEmpty();
 	}
+	bool canEditCaption() const override {
+		return true;
+	}
 	bool isReadyForOpen() const override {
 		return _data->loaded();
 	}
@@ -191,7 +194,7 @@ protected:
 	}
 
 private:
-	PhotoData *_data;
+	gsl::not_null<PhotoData*> _data;
 	int16 _pixw = 1;
 	int16 _pixh = 1;
 	Text _caption;
@@ -266,6 +269,9 @@ public:
 	bool skipBubbleTail() const override {
 		return isBubbleBottom() && _caption.isEmpty();
 	}
+	bool canEditCaption() const override {
+		return true;
+	}
 
 protected:
 	float64 dataProgress() const override {
@@ -279,7 +285,7 @@ protected:
 	}
 
 private:
-	DocumentData *_data;
+	gsl::not_null<DocumentData*> _data;
 	int32 _thumbw;
 	Text _caption;
 
@@ -417,6 +423,9 @@ public:
 	bool hideForwardedFrom() const override {
 		return _data->song();
 	}
+	bool canEditCaption() const override {
+		return true;
+	}
 
 	void step_voiceProgress(float64 ms, bool timer);
 
@@ -445,7 +454,7 @@ private:
 	template <typename Callback>
 	void buildStringRepresentation(Callback callback) const;
 
-	DocumentData *_data;
+	gsl::not_null<DocumentData*> _data;
 
 };
 
@@ -523,6 +532,9 @@ public:
 	bool skipBubbleTail() const override {
 		return isBubbleBottom() && _caption.isEmpty();
 	}
+	bool canEditCaption() const override {
+		return !_data->isRoundVideo();
+	}
 	bool isReadyForOpen() const override {
 		return _data->loaded();
 	}
@@ -535,7 +547,7 @@ protected:
 	bool dataLoaded() const override;
 
 private:
-	DocumentData *_data;
+	gsl::not_null<DocumentData*> _data;
 	int32 _thumbw = 1;
 	int32 _thumbh = 1;
 	Text _caption;
@@ -606,7 +618,7 @@ private:
 	int16 _pixw = 1;
 	int16 _pixh = 1;
 	ClickHandlerPtr _packLink;
-	DocumentData *_data;
+	gsl::not_null<DocumentData*> _data;
 	QString _emoji;
 
 };
@@ -659,8 +671,7 @@ public:
 	}
 
 private:
-
-	int32 _userId;
+	int32 _userId = 0;
 	UserData *_contact = nullptr;
 
 	int _phonew = 0;
@@ -671,6 +682,7 @@ private:
 	ClickHandlerPtr _linkl;
 	int _linkw = 0;
 	QString _link;
+
 };
 
 class HistoryWebPage : public HistoryMedia {
@@ -769,6 +781,7 @@ private:
 
 	int16 _pixw = 0;
 	int16 _pixh = 0;
+
 };
 
 class HistoryGame : public HistoryMedia {

Telegram/SourceFiles/history/history_message.cpp
@@ -434,8 +434,8 @@ MTPDmessage::Flags newForwardedFlags(PeerData *p, int32 from, HistoryMessage *fw
 		if (HistoryMedia *media = fwd->getMedia()) {
 			if (media->type() == MediaTypeVoiceFile) {
 				result |= MTPDmessage::Flag::f_media_unread;
-				//			} else if (media->type() == MediaTypeVideo) {
-				//				result |= MTPDmessage::flag_media_unread;
+//			} else if (media->type() == MediaTypeVideo) {
+//				result |= MTPDmessage::flag_media_unread;
 			}
 		}
 	}

commit d589d4fc6a0897b0ac5d633b3ca98420eff81b65

Remove edit caption UI for not my messages.
Regression was introduced in ccfe9497f4.

Telegram/SourceFiles/history/history_item.cpp
@@ -766,9 +766,7 @@ bool HistoryItem::canEdit(const QDateTime &cur) const {
 		}
 
 		if (auto media = msg->getMedia()) {
-			if (media->canEditCaption()) {
-				return true;
-			} else if (media->type() != MediaTypeWebPage) {
+			if (!media->canEditCaption() && media->type() != MediaTypeWebPage) {
 				return false;
 			}
 		}