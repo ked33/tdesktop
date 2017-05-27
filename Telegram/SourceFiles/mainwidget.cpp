ked33/tdesktop

commit 4254b4b79c7dea53b83cd15e0d7f3f7c9e7fbcb7

Shrink dialogs column when enabling emoji sidebar.
Try to hold the ratio between the chat width and the dialogs list
width when the emoji sidebar is created by shrinking the left column.

Telegram/SourceFiles/mainwidget.cpp
@@ -35,6 +35,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "apiwrap.h"
 #include "dialogswidget.h"
 #include "historywidget.h"
+#include "history/history_service_layout.h"
 #include "overviewwidget.h"
 #include "lang.h"
 #include "boxes/add_contact_box.h"
@@ -3192,11 +3193,12 @@ void MainWidget::handleAdaptiveLayoutUpdate() {
 
 void MainWidget::updateWindowAdaptiveLayout() {
 	auto layout = _controller->computeColumnLayout();
+	auto dialogsWidthRatio = _controller->dialogsWidthRatio().value();
 
 	// Check if we are in a single-column layout in a wide enough window
 	// for the normal layout. If so, switch to the normal layout.
 	if (layout.windowLayout == Adaptive::WindowLayout::OneColumn) {
-		auto chatWidth = layout.dialogsWidth;
+		auto chatWidth = layout.chatWidth;
 		if (AuthSession::Current().data().tabbedSelectorSectionEnabled()
 			&& chatWidth >= _history->minimalWidthForTabbedSelectorSection()) {
 			chatWidth -= _history->tabbedSelectorSectionWidth();
@@ -3205,9 +3207,34 @@ void MainWidget::updateWindowAdaptiveLayout() {
 			// Switch layout back to normal in a wide enough window.
 			layout.windowLayout = Adaptive::WindowLayout::Normal;
 			layout.dialogsWidth = st::dialogsWidthMin;
-			_controller->dialogsWidthRatio().set(float64(layout.dialogsWidth) / layout.bodyWidth, true);
+			layout.chatWidth = layout.bodyWidth - layout.dialogsWidth;
+			dialogsWidthRatio = float64(layout.dialogsWidth) / layout.bodyWidth;
 		}
 	}
+
+	// Check if we are going to create the third column and shrink the
+	// dialogs widget to provide a wide enough chat history column.
+	// Don't shrink the column on the first call, when window is inited.
+	if (layout.windowLayout == Adaptive::WindowLayout::Normal
+		&& _controller->window()->positionInited()) {
+		auto chatWidth = layout.chatWidth;
+		if (_history->willSwitchToTabbedSelectorWithWidth(chatWidth)) {
+			auto thirdColumnWidth = _history->tabbedSelectorSectionWidth();
+			auto twoColumnsWidth = (layout.bodyWidth - thirdColumnWidth);
+			auto sameRatioChatWidth = twoColumnsWidth - qRound(dialogsWidthRatio * twoColumnsWidth);
+			auto desiredChatWidth = qMax(sameRatioChatWidth, HistoryLayout::WideChatWidth());
+			chatWidth -= thirdColumnWidth;
+			auto extendChatBy = desiredChatWidth - chatWidth;
+			accumulate_min(extendChatBy, layout.dialogsWidth - st::dialogsWidthMin);
+			if (extendChatBy > 0) {
+				layout.dialogsWidth -= extendChatBy;
+				layout.chatWidth += extendChatBy;
+				dialogsWidthRatio = float64(layout.dialogsWidth) / layout.bodyWidth;
+			}
+		}
+	}
+
+	_controller->dialogsWidthRatio().set(dialogsWidthRatio, true);
 
 	_dialogsWidth = layout.dialogsWidth;
 	if (layout.windowLayout != Global::AdaptiveWindowLayout()) {

Telegram/SourceFiles/history/history_service_layout.cpp
@@ -167,7 +167,7 @@ void paintPreparedDate(Painter &p, const QString &dateText, int dateTextWidth, i
 	int left = st::msgServiceMargin.left();
 	int maxwidth = w;
 	if (Adaptive::ChatWide()) {
-		maxwidth = qMin(maxwidth, int32(st::msgMaxWidth + 2 * st::msgPhotoSkip + 2 * st::msgMargin.left()));
+		maxwidth = qMin(maxwidth, WideChatWidth());
 	}
 	w = maxwidth - st::msgServiceMargin.left() - st::msgServiceMargin.left();
 
@@ -181,6 +181,10 @@ void paintPreparedDate(Painter &p, const QString &dateText, int dateTextWidth, i
 }
 
 } // namepsace
+
+int WideChatWidth() {
+	return st::msgMaxWidth + 2 * st::msgPhotoSkip + 2 * st::msgMargin.left();
+}
 
 void ServiceMessagePainter::paint(Painter &p, const HistoryService *message, const PaintContext &context, int height) {
 	int left = 0, width = 0;

Telegram/SourceFiles/history/history_service_layout.h
@@ -22,6 +22,8 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 
 namespace HistoryLayout {
 
+int WideChatWidth();
+
 struct PaintContext {
 	PaintContext(TimeMs ms, const QRect &clip, TextSelection selection)
 		: ms(ms)

Telegram/SourceFiles/historywidget.cpp
@@ -3779,8 +3779,16 @@ int HistoryWidget::minimalWidthForTabbedSelectorSection() const {
 	return st::windowMinWidth + tabbedSelectorSectionWidth();
 }
 
+bool HistoryWidget::willSwitchToTabbedSelectorWithWidth(int newWidth) const {
+	if (!AuthSession::Current().data().tabbedSelectorSectionEnabled()) {
+		return false;
+	} else if (_tabbedSectionUsed) {
+		return false;
+	}
+	return (newWidth >= minimalWidthForTabbedSelectorSection());
+}
+
 void HistoryWidget::toggleTabbedSelectorMode() {
-	auto sectionEnabled = AuthSession::Current().data().tabbedSelectorSectionEnabled();
 	if (_tabbedSection) {
 		AuthSession::Current().data().setTabbedSelectorSectionEnabled(false);
 		AuthSession::Current().saveDataDelayed(kSaveTabbedSelectorSectionTimeoutMs);

Telegram/SourceFiles/historywidget.h
@@ -202,6 +202,7 @@ public:
 	QRect historyRect() const;
 	int tabbedSelectorSectionWidth() const;
 	int minimalWidthForTabbedSelectorSection() const;
+	bool willSwitchToTabbedSelectorWithWidth(int newWidth) const;
 
 	void updateSendAction(History *history, SendAction::Type type, int32 progress = 0);
 	void cancelSendAction(History *history, SendAction::Type type);

commit 2aef0518a3aeedbedbf977c9eba4008e6349ba61

Fix the chats list width saving with passcode.
Regression was introduced in 4254b4b79c.

If the passcode is enabled the main widget is created after the main
window was fully initialized, so the first main widget resize already
used the shrink algorithm while creating the third column.

No we shrink the left column only after the main widget was started.

Telegram/SourceFiles/mainwidget.cpp
@@ -3575,7 +3575,7 @@ void MainWidget::updateWindowAdaptiveLayout() {
 	// dialogs widget to provide a wide enough chat history column.
 	// Don't shrink the column on the first call, when window is inited.
 	if (layout.windowLayout == Adaptive::WindowLayout::Normal
-		&& _controller->window()->positionInited()) {
+		&& _started && _controller->window()->positionInited()) {
 		auto chatWidth = layout.chatWidth;
 		if (_history->willSwitchToTabbedSelectorWithWidth(chatWidth)) {
 			auto thirdColumnWidth = _history->tabbedSelectorSectionWidth();