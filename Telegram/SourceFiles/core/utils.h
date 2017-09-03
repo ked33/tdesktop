ked33/tdesktop

commit 3e485bd6b2d1f87b4fa5d0b17e317e99e3753b69

Replace QFlags with base::flags.

Telegram/SourceFiles/core/utils.h
@@ -21,6 +21,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #pragma once
 
 #include "core/basic_types.h"
+#include "base/flags.h"
 
 // Define specializations for QByteArray for Qt 5.3.2, because
 // QByteArray in Qt 5.3.2 doesn't declare "pointer" subtype.
@@ -177,11 +178,6 @@ reversion_wrapper<Container> reversed(Container &&container) {
 // if you have "QVector<T*> v" then "for (T * const p : v)" will still call QVector::detach(),
 // while "for_const (T *p, v)" won't and "for_const (T *&p, v)" won't compile
 #define for_const(range_declaration, range_expression) for (range_declaration : std::as_const(range_expression))
-
-template <typename Enum>
-inline constexpr QFlags<Enum> qFlags(Enum v) {
-	return QFlags<Enum>(v);
-}
 
 template <typename Lambda>
 inline void InvokeQueued(QObject *context, Lambda &&lambda) {
@@ -581,8 +577,8 @@ enum ShowLayerOption {
 	AnimatedShowLayer = 0x00,
 	ForceFastShowLayer = 0x04,
 };
-using ShowLayerOptions = QFlags<ShowLayerOption>;
-Q_DECLARE_OPERATORS_FOR_FLAGS(ShowLayerOptions);
+using ShowLayerOptions = base::flags<ShowLayerOption>;
+inline constexpr auto is_flag_type(ShowLayerOption) { return true; };
 
 static int32 FullArcLength = 360 * 16;
 static int32 QuarterArcLength = (FullArcLength / 4);

Telegram/SourceFiles/dialogs/dialogs_inner_widget.h
@@ -21,6 +21,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #pragma once
 
 #include "dialogs/dialogs_widget.h"
+#include "base/flags.h"
 
 namespace Dialogs {
 class Row;
@@ -180,8 +181,9 @@ private:
 		MessageSearch = 0x08,
 		All = 0x0F,
 	};
-	using UpdateRowSections = QFlags<UpdateRowSection>;
-	Q_DECLARE_FRIEND_OPERATORS_FOR_FLAGS(UpdateRowSections);
+	using UpdateRowSections = base::flags<UpdateRowSection>;
+	friend inline constexpr auto is_flag_type(UpdateRowSection) { return true; };
+
 	void updateDialogRow(PeerData *peer, MsgId msgId, QRect updateRect, UpdateRowSections sections = UpdateRowSection::All);
 
 	int dialogsOffset() const;
@@ -293,5 +295,3 @@ private:
 	base::lambda<void()> _loadMoreCallback;
 
 };
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(DialogsInner::UpdateRowSections);

Telegram/SourceFiles/history/history_item.h
@@ -21,6 +21,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #pragma once
 
 #include "base/runtime_composer.h"
+#include "base/flags.h"
 
 namespace Ui {
 class RippleAnimation;
@@ -170,11 +171,12 @@ struct HistoryMessageReply : public RuntimeComponent<HistoryMessageReply> {
 	void resize(int width) const;
 	void itemRemoved(HistoryMessage *holder, HistoryItem *removed);
 
-	enum PaintFlag {
-		PaintInBubble = 0x01,
-		PaintSelected = 0x02,
+	enum class PaintFlag {
+		InBubble = 0x01,
+		Selected = 0x02,
 	};
-	using PaintFlags = QFlags<PaintFlag>;
+	using PaintFlags = base::flags<PaintFlag>;
+	friend inline constexpr auto is_flag_type(PaintFlag) { return true; };
 	void paint(Painter &p, const HistoryItem *holder, int x, int y, int w, PaintFlags flags) const;
 
 	MsgId replyToId() const {
@@ -196,7 +198,6 @@ struct HistoryMessageReply : public RuntimeComponent<HistoryMessageReply> {
 	std::unique_ptr<HistoryMessageVia> _replyToVia;
 	int toWidth = 0;
 };
-Q_DECLARE_OPERATORS_FOR_FLAGS(HistoryMessageReply::PaintFlags);
 
 class ReplyKeyboard;
 struct HistoryMessageReplyMarkup : public RuntimeComponent<HistoryMessageReplyMarkup> {
@@ -433,7 +434,7 @@ struct HistoryMessageLogEntryOriginal : public RuntimeComponent<HistoryMessageLo
 class HistoryMedia;
 class HistoryMediaPtr {
 public:
-	HistoryMediaPtr() = default;
+	HistoryMediaPtr();
 	HistoryMediaPtr(const HistoryMediaPtr &other) = delete;
 	HistoryMediaPtr &operator=(const HistoryMediaPtr &other) = delete;
 	HistoryMediaPtr(std::unique_ptr<HistoryMedia> other);
@@ -604,7 +605,7 @@ public:
 
 		// optimization: don't create markup component for the case
 		// MTPDreplyKeyboardHide with flags = 0, assume it has f_zero flag
-		return qFlags(MTPDreplyKeyboardMarkup_ClientFlag::f_zero);
+		return MTPDreplyKeyboardMarkup_ClientFlag::f_zero | 0;
 	}
 	bool hasSwitchInlineButton() const {
 		return _flags & MTPDmessage_ClientFlag::f_has_switch_inline_button;

Telegram/SourceFiles/history/history_widget.h
@@ -27,6 +27,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "window/section_widget.h"
 #include "core/single_timer.h"
 #include "ui/widgets/input_fields.h"
+#include "base/flags.h"
 
 namespace InlineBots {
 namespace Layout {
@@ -693,8 +694,8 @@ private:
 		SaveDraft  = 0x01,
 		SendTyping = 0x02,
 	};
-	using TextUpdateEvents = QFlags<TextUpdateEvent>;
-	Q_DECLARE_FRIEND_OPERATORS_FOR_FLAGS(TextUpdateEvents);
+	using TextUpdateEvents = base::flags<TextUpdateEvent>;
+	friend inline constexpr auto is_flag_type(TextUpdateEvent) { return true; };
 
 	void writeDrafts(Data::Draft **localDraft, Data::Draft **editDraft);
 	void writeDrafts(History *history);
@@ -833,7 +834,7 @@ private:
 	bool _nonEmptySelection = false;
 
 	TaskQueue _fileLoader;
-	TextUpdateEvents _textUpdateEvents = (TextUpdateEvent::SaveDraft | TextUpdateEvent::SendTyping);
+	TextUpdateEvents _textUpdateEvents = (TextUpdateEvents() | TextUpdateEvent::SaveDraft | TextUpdateEvent::SendTyping);
 
 	int64 _serviceImageCacheSize = 0;
 	QString _confirmSource;
@@ -866,5 +867,3 @@ private:
 	bool _inGrab = false;
 
 };
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(HistoryWidget::TextUpdateEvents)

Telegram/SourceFiles/inline_bots/inline_bot_layout_internal.h
@@ -20,6 +20,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
+#include "base/flags.h"
 #include "inline_bots/inline_bot_layout_item.h"
 #include "ui/effects/radial_animation.h"
 #include "ui/text/text.h"
@@ -84,11 +85,9 @@ private:
 		Over = 0x01,
 		DeleteOver = 0x02,
 	};
-	using StateFlags = QFlags<StateFlag>;
+	using StateFlags = base::flags<StateFlag>;
+	friend inline constexpr auto is_flag_type(StateFlag) { return true; };
 	StateFlags _state;
-	friend inline StateFlags operator~(StateFlag flag) {
-		return ~StateFlags(flag);
-	}
 
 	Media::Clip::ReaderPointer _gif;
 	ClickHandlerPtr _delete;

Telegram/SourceFiles/observer_peer.h
@@ -21,6 +21,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #pragma once
 
 #include "base/observer.h"
+#include "base/flags.h"
 
 namespace Notify {
 
@@ -74,7 +75,9 @@ struct PeerUpdate {
 		ChannelStickersChanged    = 0x00040000U,
 		ChannelPinnedChanged      = 0x00080000U,
 	};
-	using Flags = QFlags<Flag>;
+	using Flags = base::flags<Flag>;
+	friend inline constexpr auto is_flag_type(Flag) { return true; }
+
 	Flags flags = 0;
 
 	// NameChanged data
@@ -85,7 +88,6 @@ struct PeerUpdate {
 	int32 mediaTypesMask = 0;
 
 };
-Q_DECLARE_OPERATORS_FOR_FLAGS(PeerUpdate::Flags);
 
 void peerUpdatedDelayed(const PeerUpdate &update);
 inline void peerUpdatedDelayed(PeerData *peer, PeerUpdate::Flags events) {

Telegram/SourceFiles/platform/win/main_window_win.h
@@ -21,6 +21,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #pragma once
 
 #include "platform/platform_main_window.h"
+#include "base/flags.h"
 #include <windows.h>
 
 namespace Ui {
@@ -60,7 +61,8 @@ public:
 		Hidden   = 0x08,
 		Activate = 0x10,
 	};
-	using ShadowsChanges = QFlags<ShadowsChange>;
+	using ShadowsChanges = base::flags<ShadowsChange>;
+	friend inline constexpr auto is_flag_type(ShadowsChange) { return true; };
 
 	bool shadowsWorking() const {
 		return _shadowsWorking;
@@ -126,7 +128,5 @@ private:
 	int _deltaTop = 0;
 
 };
-
-Q_DECLARE_OPERATORS_FOR_FLAGS(MainWindow::ShadowsChanges);
 
 } // namespace Platform

Telegram/SourceFiles/storage/localstorage.cpp
@@ -36,6 +36,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 #include "apiwrap.h"
 #include "auth_session.h"
 #include "window/window_controller.h"
+#include "base/flags.h"
 
 #include <openssl/evp.h>
 
@@ -78,8 +79,8 @@ enum class FileOption {
 	User = 0x01,
 	Safe = 0x02,
 };
-using FileOptions = QFlags<FileOption>;
-Q_DECLARE_OPERATORS_FOR_FLAGS(FileOptions);
+using FileOptions = base::flags<FileOption>;
+inline constexpr auto is_flag_type(FileOption) { return true; };
 
 bool keyAlreadyUsed(QString &name, FileOptions options = FileOption::User | FileOption::Safe) {
 	name += '0';
@@ -3312,7 +3313,7 @@ void _readStickerSets(FileKey &stickersKey, Stickers::Order *outOrder = nullptr,
 		return;
 	}
 
-	bool readingInstalled = (readingFlags == qFlags(MTPDstickerSet::Flag::f_installed));
+	bool readingInstalled = (readingFlags == MTPDstickerSet::Flag::f_installed);
 
 	auto &sets = Global::RefStickerSets();
 	if (outOrder) outOrder->clear();
@@ -3329,16 +3330,19 @@ void _readStickerSets(FileKey &stickersKey, Stickers::Order *outOrder = nullptr,
 		qint32 scnt = 0;
 		stickers.stream >> setId >> setAccess >> setTitle >> setShortName >> scnt;
 
-		qint32 setHash = 0, setFlags = 0;
+		qint32 setHash = 0;
+		MTPDstickerSet::Flags setFlags = 0;
 		if (stickers.version > 8033) {
-			stickers.stream >> setHash >> setFlags;
-			if (setFlags & qFlags(MTPDstickerSet_ClientFlag::f_not_loaded__old)) {
-				setFlags &= ~qFlags(MTPDstickerSet_ClientFlag::f_not_loaded__old);
-				setFlags |= qFlags(MTPDstickerSet_ClientFlag::f_not_loaded);
+			qint32 setFlagsValue = 0;
+			stickers.stream >> setHash >> setFlagsValue;
+			setFlags = MTPDstickerSet::Flags{ setFlagsValue };
+			if (setFlags & MTPDstickerSet_ClientFlag::f_not_loaded__old) {
+				setFlags &= ~MTPDstickerSet_ClientFlag::f_not_loaded__old;
+				setFlags |= MTPDstickerSet_ClientFlag::f_not_loaded;
 			}
 		}
 		if (readingInstalled && stickers.version < 9061) {
-			setFlags |= qFlags(MTPDstickerSet::Flag::f_installed);
+			setFlags |= MTPDstickerSet::Flag::f_installed;
 		}
 
 		if (setId == Stickers::DefaultSetId) {
@@ -3349,13 +3353,13 @@ void _readStickerSets(FileKey &stickersKey, Stickers::Order *outOrder = nullptr,
 			}
 		} else if (setId == Stickers::CustomSetId) {
 			setTitle = qsl("Custom stickers");
-			setFlags |= qFlags(MTPDstickerSet_ClientFlag::f_special);
+			setFlags |= MTPDstickerSet_ClientFlag::f_special;
 		} else if (setId == Stickers::CloudRecentSetId) {
 			setTitle = lang(lng_recent_stickers);
-			setFlags |= qFlags(MTPDstickerSet_ClientFlag::f_special);
+			setFlags |= MTPDstickerSet_ClientFlag::f_special;
 		} else if (setId == Stickers::FavedSetId) {
 			setTitle = lang(lng_faved_stickers);
-			setFlags |= qFlags(MTPDstickerSet_ClientFlag::f_special);
+			setFlags |= MTPDstickerSet_ClientFlag::f_special;
 		} else if (setId) {
 			if (readingInstalled && outOrder && stickers.version < 9061) {
 				outOrder->push_back(setId);
@@ -3604,11 +3608,11 @@ void readInstalledStickers() {
 	}
 
 	Global::RefStickerSets().clear();
-	_readStickerSets(_installedStickersKey, &Global::RefStickerSetsOrder(), qFlags(MTPDstickerSet::Flag::f_installed));
+	_readStickerSets(_installedStickersKey, &Global::RefStickerSetsOrder(), MTPDstickerSet::Flag::f_installed);
 }
 
 void readFeaturedStickers() {
-	_readStickerSets(_featuredStickersKey, &Global::RefFeaturedStickerSetsOrder(), qFlags(MTPDstickerSet_ClientFlag::f_featured));
+	_readStickerSets(_featuredStickersKey, &Global::RefFeaturedStickerSetsOrder(), MTPDstickerSet::Flags() | MTPDstickerSet_ClientFlag::f_featured);
 
 	auto &sets = Global::StickerSets();
 	int unreadCount = 0;

commit 41d783fee5fd8ddfdff9d93d1ffb0faddf52af47

Fix working with layers.
Regression was introduced in 3e485bd.

New base::flags work correctly only if all mutually exclusive flag
values use mutually exclusive bits (a & b == 0 for exclusive (a, b)).

Closes #3856.

Telegram/SourceFiles/core/utils.h
@@ -570,12 +570,12 @@ enum ForwardWhatMessages {
 };
 
 enum ShowLayerOption {
-	CloseOtherLayers = 0x00,
-	KeepOtherLayers = 0x01,
-	ShowAfterOtherLayers = 0x03,
+	CloseOtherLayers     = (1 << 0),
+	KeepOtherLayers      = (1 << 1),
+	ShowAfterOtherLayers = (1 << 2),
 
-	AnimatedShowLayer = 0x00,
-	ForceFastShowLayer = 0x04,
+	AnimatedShowLayer    = (1 << 3),
+	ForceFastShowLayer   = (1 << 4),
 };
 using ShowLayerOptions = base::flags<ShowLayerOption>;
 inline constexpr auto is_flag_type(ShowLayerOption) { return true; };

Telegram/SourceFiles/dialogs/dialogs_inner_widget.h
@@ -175,11 +175,11 @@ private:
 
 	void itemRemoved(HistoryItem *item);
 	enum class UpdateRowSection {
-		Default = 0x01,
-		Filtered = 0x02,
-		PeerSearch = 0x04,
-		MessageSearch = 0x08,
-		All = 0x0F,
+		Default       = (1 << 0),
+		Filtered      = (1 << 1),
+		PeerSearch    = (1 << 2),
+		MessageSearch = (1 << 3),
+		All           = Default | Filtered | PeerSearch | MessageSearch,
 	};
 	using UpdateRowSections = base::flags<UpdateRowSection>;
 	friend inline constexpr auto is_flag_type(UpdateRowSection) { return true; };

Telegram/SourceFiles/history/history_item.h
@@ -172,8 +172,8 @@ struct HistoryMessageReply : public RuntimeComponent<HistoryMessageReply> {
 	void itemRemoved(HistoryMessage *holder, HistoryItem *removed);
 
 	enum class PaintFlag {
-		InBubble = 0x01,
-		Selected = 0x02,
+		InBubble = (1 << 0),
+		Selected = (1 << 1),
 	};
 	using PaintFlags = base::flags<PaintFlag>;
 	friend inline constexpr auto is_flag_type(PaintFlag) { return true; };

Telegram/SourceFiles/history/history_widget.h
@@ -691,8 +691,8 @@ private:
 	void countHistoryShowFrom();
 
 	enum class TextUpdateEvent {
-		SaveDraft  = 0x01,
-		SendTyping = 0x02,
+		SaveDraft  = (1 << 0),
+		SendTyping = (1 << 1),
 	};
 	using TextUpdateEvents = base::flags<TextUpdateEvent>;
 	friend inline constexpr auto is_flag_type(TextUpdateEvent) { return true; };

Telegram/SourceFiles/inline_bots/inline_bot_layout_internal.h
@@ -82,8 +82,8 @@ private:
 	QSize countFrameSize() const;
 
 	enum class StateFlag {
-		Over = 0x01,
-		DeleteOver = 0x02,
+		Over       = (1 << 0),
+		DeleteOver = (1 << 1),
 	};
 	using StateFlags = base::flags<StateFlag>;
 	friend inline constexpr auto is_flag_type(StateFlag) { return true; };

Telegram/SourceFiles/observer_peer.h
@@ -35,45 +35,45 @@ struct PeerUpdate {
 	PeerData *peer;
 
 	enum class Flag : uint32 {
-		None                      = 0x00000000U,
+		None                      = 0,
 
 		// Common flags
-		NameChanged               = 0x00000001U,
-		UsernameChanged           = 0x00000002U,
-		PhotoChanged              = 0x00000004U,
-		AboutChanged              = 0x00000008U,
-		NotificationsEnabled      = 0x00000010U,
-		SharedMediaChanged        = 0x00000020U,
-		MigrationChanged          = 0x00000040U,
-		PinnedChanged             = 0x00000080U,
-		RestrictionReasonChanged  = 0x00000100U,
+		NameChanged               = (1 << 0),
+		UsernameChanged           = (1 << 1),
+		PhotoChanged              = (1 << 2),
+		AboutChanged              = (1 << 3),
+		NotificationsEnabled      = (1 << 4),
+		SharedMediaChanged        = (1 << 5),
+		MigrationChanged          = (1 << 6),
+		PinnedChanged             = (1 << 7),
+		RestrictionReasonChanged  = (1 << 8),
 
 		// For chats and channels
-		InviteLinkChanged         = 0x00000200U,
-		MembersChanged            = 0x00000400U,
-		AdminsChanged             = 0x00000800U,
-		BannedUsersChanged        = 0x00001000U,
-		UnreadMentionsChanged     = 0x00002000U,
+		InviteLinkChanged         = (1 << 9),
+		MembersChanged            = (1 << 10),
+		AdminsChanged             = (1 << 11),
+		BannedUsersChanged        = (1 << 12),
+		UnreadMentionsChanged     = (1 << 13),
 
 		// For users
-		UserCanShareContact       = 0x00010000U,
-		UserIsContact             = 0x00020000U,
-		UserPhoneChanged          = 0x00040000U,
-		UserIsBlocked             = 0x00080000U,
-		BotCommandsChanged        = 0x00100000U,
-		UserOnlineChanged         = 0x00200000U,
-		BotCanAddToGroups         = 0x00400000U,
-		UserCommonChatsChanged    = 0x00800000U,
-		UserHasCalls              = 0x01000000U,
+		UserCanShareContact       = (1 << 16),
+		UserIsContact             = (1 << 17),
+		UserPhoneChanged          = (1 << 18),
+		UserIsBlocked             = (1 << 19),
+		BotCommandsChanged        = (1 << 20),
+		UserOnlineChanged         = (1 << 21),
+		BotCanAddToGroups         = (1 << 22),
+		UserCommonChatsChanged    = (1 << 23),
+		UserHasCalls              = (1 << 24),
 
 		// For chats
-		ChatCanEdit               = 0x00010000U,
+		ChatCanEdit               = (1 << 16),
 
 		// For channels
-		ChannelAmIn               = 0x00010000U,
-		ChannelRightsChanged      = 0x00020000U,
-		ChannelStickersChanged    = 0x00040000U,
-		ChannelPinnedChanged      = 0x00080000U,
+		ChannelAmIn               = (1 << 16),
+		ChannelRightsChanged      = (1 << 17),
+		ChannelStickersChanged    = (1 << 18),
+		ChannelPinnedChanged      = (1 << 19),
 	};
 	using Flags = base::flags<Flag>;
 	friend inline constexpr auto is_flag_type(Flag) { return true; }

Telegram/SourceFiles/platform/win/main_window_win.h
@@ -55,11 +55,11 @@ public:
 
 	// Custom shadows.
 	enum class ShadowsChange {
-		Moved    = 0x01,
-		Resized  = 0x02,
-		Shown    = 0x04,
-		Hidden   = 0x08,
-		Activate = 0x10,
+		Moved    = (1 << 0),
+		Resized  = (1 << 1),
+		Shown    = (1 << 2),
+		Hidden   = (1 << 3),
+		Activate = (1 << 4),
 	};
 	using ShadowsChanges = base::flags<ShadowsChange>;
 	friend inline constexpr auto is_flag_type(ShadowsChange) { return true; };

Telegram/SourceFiles/storage/localstorage.cpp
@@ -76,8 +76,8 @@ bool _userWorking() {
 }
 
 enum class FileOption {
-	User = 0x01,
-	Safe = 0x02,
+	User = (1 << 0),
+	Safe = (1 << 1),
 };
 using FileOptions = base::flags<FileOption>;
 inline constexpr auto is_flag_type(FileOption) { return true; };

Telegram/SourceFiles/ui/abstract_button.h
@@ -76,10 +76,10 @@ signals:
 
 protected:
 	enum class StateFlag {
-		None = 0x00,
-		Over = 0x01,
-		Down = 0x02,
-		Disabled = 0x04,
+		None     = 0,
+		Over     = (1 << 0),
+		Down     = (1 << 1),
+		Disabled = (1 << 2),
 	};
 	using State = base::flags<StateFlag>;
 

Telegram/SourceFiles/ui/images.h
@@ -188,18 +188,18 @@ QImage prepareColored(style::color add, QImage image);
 QImage prepareOpaque(QImage image);
 
 enum class Option {
-	None = 0x000,
-	Smooth = 0x001,
-	Blurred = 0x002,
-	Circled = 0x004,
-	RoundedLarge = 0x008,
-	RoundedSmall = 0x010,
-	RoundedTopLeft = 0x020,
-	RoundedTopRight = 0x040,
-	RoundedBottomLeft = 0x080,
-	RoundedBottomRight = 0x100,
-	Colored = 0x200,
-	TransparentBackground = 0x400,
+	None                  = 0,
+	Smooth                = (1 << 0),
+	Blurred               = (1 << 1),
+	Circled               = (1 << 2),
+	RoundedLarge          = (1 << 3),
+	RoundedSmall          = (1 << 4),
+	RoundedTopLeft        = (1 << 5),
+	RoundedTopRight       = (1 << 6),
+	RoundedBottomLeft     = (1 << 7),
+	RoundedBottomRight    = (1 << 8),
+	Colored               = (1 << 9),
+	TransparentBackground = (1 << 10),
 };
 using Options = base::flags<Option>;
 inline constexpr auto is_flag_type(Option) { return true; };

Telegram/SourceFiles/ui/text/text.h
@@ -129,9 +129,9 @@ public:
 
 	struct StateRequest {
 		enum class Flag {
-			BreakEverywhere = 0x01,
-			LookupSymbol    = 0x02,
-			LookupLink      = 0x04,
+			BreakEverywhere = (1 << 0),
+			LookupSymbol    = (1 << 1),
+			LookupLink      = (1 << 2),
 		};
 		using Flags = base::flags<Flag>;
 		friend inline constexpr auto is_flag_type(Flag) { return true; };

Telegram/SourceFiles/ui/twidget.h
@@ -30,34 +30,34 @@ QString GetOverride(const QString &familyName);
 } // namespace
 
 enum class RectPart {
-	None = 0,
+	None        = 0,
 
-	TopLeft = (1 << 0),
-	Top = (1 << 1),
-	TopRight = (1 << 2),
-	Left = (1 << 3),
-	Center = (1 << 4),
-	Right = (1 << 5),
-	BottomLeft = (1 << 6),
-	Bottom = (1 << 7),
+	TopLeft     = (1 << 0),
+	Top         = (1 << 1),
+	TopRight    = (1 << 2),
+	Left        = (1 << 3),
+	Center      = (1 << 4),
+	Right       = (1 << 5),
+	BottomLeft  = (1 << 6),
+	Bottom      = (1 << 7),
 	BottomRight = (1 << 8),
 
-	FullTop = TopLeft | Top | TopRight,
+	FullTop     = TopLeft | Top | TopRight,
 	NoTopBottom = Left | Center | Right,
-	FullBottom = BottomLeft | Bottom | BottomRight,
-	NoTop = NoTopBottom | FullBottom,
-	NoBottom = FullTop | NoTopBottom,
+	FullBottom  = BottomLeft | Bottom | BottomRight,
+	NoTop       = NoTopBottom | FullBottom,
+	NoBottom    = FullTop | NoTopBottom,
 
-	FullLeft = TopLeft | Left | BottomLeft,
+	FullLeft    = TopLeft | Left | BottomLeft,
 	NoLeftRight = Top | Center | Bottom,
-	FullRight = TopRight | Right | BottomRight,
-	NoLeft = NoLeftRight | FullRight,
-	NoRight = FullLeft | NoLeftRight,
+	FullRight   = TopRight | Right | BottomRight,
+	NoLeft      = NoLeftRight | FullRight,
+	NoRight     = FullLeft | NoLeftRight,
 
 	CornersMask = TopLeft | TopRight | BottomLeft | BottomRight,
-	SidesMask = Top | Bottom | Left | Right,
+	SidesMask   = Top | Bottom | Left | Right,
 
-	Full = FullTop | NoTop,
+	Full        = FullTop | NoTop,
 };
 using RectParts = base::flags<RectPart>;
 inline constexpr auto is_flag_type(RectPart) { return true; };

Telegram/SourceFiles/window/window_controller.h
@@ -25,12 +25,12 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 namespace Window {
 
 enum class GifPauseReason {
-	Any = 0,
+	Any           = 0,
 	InlineResults = (1 << 0),
-	SavedGifs = (1 << 1),
-	Layer = (1 << 2),
-	RoundPlaying = (1 << 3),
-	MediaPreview = (1 << 4),
+	SavedGifs     = (1 << 1),
+	Layer         = (1 << 2),
+	RoundPlaying  = (1 << 3),
+	MediaPreview  = (1 << 4),
 };
 using GifPauseReasons = base::flags<GifPauseReason>;
 inline constexpr bool is_flag_type(GifPauseReason) { return true; };