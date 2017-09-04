ked33/tdesktop

commit 82254f8caef0c4c5d99c0fd4d16b04f860afd7a8

Add Catch unittest framework and some tests.

Telegram/Patches/gyp.diff
@@ -1,33 +1,5 @@
-diff --git a/pylib/gyp/generator/cmake.py b/pylib/gyp/generator/cmake.py
-index a2b9629..68d7020 100644
---- a/pylib/gyp/generator/cmake.py
-+++ b/pylib/gyp/generator/cmake.py
-@@ -1070,6 +1070,23 @@ def WriteTarget(namer, qualified_target, target_dicts, build_dir, config_to_use,
- 
-       output.write(')\n')
- 
-+  # Precompile header
-+  precompiled_header = config.get('cmake_precompiled_header', '')
-+  if precompiled_header:
-+    precompiled_header_script = config.get('cmake_precompiled_header_script', '')
-+    if not precompiled_header_script:
-+      print ('ERROR: cmake_precompiled_header requires cmake_precompiled_header_script')
-+    cmake_precompiled_header = NormjoinPath(path_from_cmakelists_to_gyp, precompiled_header)
-+    cmake_precompiled_header_script = NormjoinPathForceCMakeSource(path_from_cmakelists_to_gyp, precompiled_header_script)
-+    output.write('include(')
-+    output.write(cmake_precompiled_header_script)
-+    output.write(')\n')
-+    output.write('add_precompiled_header(')
-+    output.write(cmake_target_name)
-+    output.write(' ')
-+    output.write(cmake_precompiled_header)
-+    output.write(')\n')
-+
-   UnsetVariable(output, 'TOOLSET')
-   UnsetVariable(output, 'TARGET')
- 
 diff --git a/pylib/gyp/generator/xcode.py b/pylib/gyp/generator/xcode.py
-index db99d6a..f8398cc 100644
+index 0e3fb93..4c824ec 100644
 --- a/pylib/gyp/generator/xcode.py
 +++ b/pylib/gyp/generator/xcode.py
 @@ -72,6 +72,10 @@ generator_additional_non_configuration_keys = [
@@ -41,7 +13,7 @@ index db99d6a..f8398cc 100644
    'mac_bundle',
    'mac_bundle_resources',
    'mac_framework_headers',
-@@ -772,6 +776,26 @@ def GenerateOutput(target_list, target_dicts, data, params):
+@@ -761,6 +765,26 @@ def GenerateOutput(target_list, target_dicts, data, params):
      xcode_targets[qualified_target] = xct
      xcode_target_to_target_dict[xct] = spec
  
@@ -68,3 +40,13 @@ index db99d6a..f8398cc 100644
      spec_actions = spec.get('actions', [])
      spec_rules = spec.get('rules', [])
  
+@@ -1130,7 +1154,8 @@ exit 1
+         groups = [x for x in groups if not x.endswith('_excluded')]
+       for group in groups:
+         for item in rule.get(group, []):
+-          pbxp.AddOrGetFileInRootGroup(item)
++          concrete_item = ExpandXcodeVariables(item, rule_input_dict)
++          pbxp.AddOrGetFileInRootGroup(concrete_item)
+ 
+     # Add "sources".
+     for source in spec.get('sources', []):

.gitignore
@@ -18,6 +18,10 @@
 /Telegram/Resources/art/sprite_125x.png
 /Telegram/Resources/art/sprite_150x.png
 /Telegram/Debug/
+/Telegram/Release/
+/Telegram/tests/
+/Telegram/gyp/tests/*.test
+/Telegram/out/
 /Telegram/*.user
 *.vcxproj*
 *.sln

.gitmodules
@@ -7,3 +7,6 @@
 [submodule "Telegram/ThirdParty/GSL"]
 	path = Telegram/ThirdParty/GSL
 	url = https://github.com/Microsoft/GSL.git
+[submodule "Telegram/ThirdParty/Catch"]
+	path = Telegram/ThirdParty/Catch
+	url = https://github.com/philsquared/Catch

Telegram/SourceFiles/base/flat_map.h
@@ -20,6 +20,7 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
+#include <deque>
 #include "base/optional.h"
 
 namespace base {

Telegram/SourceFiles/base/flat_map_tests.cpp
@@ -1,0 +1,51 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop version of Telegram messaging app, see https://telegram.org
+
+Telegram Desktop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+In addition, as a special exception, the copyright holders give permission
+to link the code of portions of this program with the OpenSSL library.
+
+Full license: https://github.com/telegramdesktop/tdesktop/blob/master/LICENSE
+Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
+*/
+#include "catch.hpp"
+
+#include "base/flat_map.h"
+#include <string>
+
+using namespace std;
+
+TEST_CASE("flat_maps should keep items sorted by key", "[flat_map]") {
+	base::flat_map<int, string> v;
+	v.emplace(0, "a");
+	v.emplace(5, "b");
+	v.emplace(4, "d");
+	v.emplace(2, "e");
+
+	auto checkSorted = [&] {
+		auto prev = v.begin();
+		REQUIRE(prev != v.end());
+		for (auto i = prev + 1; i != v.end(); prev = i, ++i) {
+			REQUIRE(prev->first < i->first);
+		}
+	};
+	REQUIRE(v.size() == 4);
+	checkSorted();
+
+	SECTION("adding item puts it in the right position") {
+		v.emplace(3, "c");
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.find(3) != v.end());
+		checkSorted();
+	}
+}

Telegram/SourceFiles/base/flat_set.h
@@ -20,6 +20,8 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
+#include <deque>
+
 namespace base {
 
 template <typename Type>

Telegram/SourceFiles/base/flat_set_tests.cpp
@@ -1,0 +1,48 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop version of Telegram messaging app, see https://telegram.org
+
+Telegram Desktop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+In addition, as a special exception, the copyright holders give permission
+to link the code of portions of this program with the OpenSSL library.
+
+Full license: https://github.com/telegramdesktop/tdesktop/blob/master/LICENSE
+Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
+*/
+#include "catch.hpp"
+
+#include "base/flat_set.h"
+
+TEST_CASE("flat_sets should keep items sorted", "[flat_set]") {
+	base::flat_set<int> v;
+	v.insert(0);
+	v.insert(5);
+	v.insert(4);
+	v.insert(2);
+
+	auto checkSorted = [&] {
+		auto prev = v.begin();
+		REQUIRE(prev != v.end());
+		for (auto i = prev + 1; i != v.end(); prev = i, ++i) {
+			REQUIRE(*prev < *i);
+		}
+	};
+	REQUIRE(v.size() == 4);
+	checkSorted();
+
+	SECTION("adding item puts it in the right position") {
+		v.insert(3);
+		REQUIRE(v.size() == 5);
+		REQUIRE(v.find(3) != v.end());
+		checkSorted();
+	}
+}

Telegram/SourceFiles/base/tests_main.cpp
@@ -1,0 +1,97 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop version of Telegram messaging app, see https://telegram.org
+
+Telegram Desktop is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+In addition, as a special exception, the copyright holders give permission
+to link the code of portions of this program with the OpenSSL library.
+
+Full license: https://github.com/telegramdesktop/tdesktop/blob/master/LICENSE
+Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
+*/
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include "reporters/catch_reporter_compact.hpp"
+#include <QFile>
+
+namespace Catch {
+
+	struct MinimalReporter : CompactReporter {
+		MinimalReporter( ReporterConfig const& _config )
+        : CompactReporter( _config )
+        {}
+
+        virtual void testRunEnded( TestRunStats const& _testRunStats ) {
+            printTotals( _testRunStats.totals );
+        }
+
+    private:
+        // Colour, message variants:
+        // - white: No tests ran.
+        // -   red: Failed [both/all] N test cases, failed [both/all] M assertions.
+        // - white: Passed [both/all] N test cases (no assertions).
+        // -   red: Failed N tests cases, failed M assertions.
+        // - green: Passed [both/all] N tests cases with M assertions.
+
+        std::string bothOrAll( std::size_t count ) const {
+            return count == 1 ? std::string() : count == 2 ? "both " : "all " ;
+        }
+
+        void printTotals( const Totals& totals ) const {
+            if( totals.testCases.total() == 0 ) {
+            }
+            else if( totals.testCases.failed == totals.testCases.total() ) {
+                Colour colour( Colour::ResultError );
+                const std::string qualify_assertions_failed =
+                    totals.assertions.failed == totals.assertions.total() ?
+                        bothOrAll( totals.assertions.failed ) : std::string();
+                stream <<
+                    "Failed " << bothOrAll( totals.testCases.failed )
+                              << pluralise( totals.testCases.failed, "test case"  ) << ", "
+                    "failed " << qualify_assertions_failed <<
+                                 pluralise( totals.assertions.failed, "assertion" ) << '.';
+            }
+            else if( totals.assertions.total() == 0 ) {
+                stream <<
+                    "Passed " << bothOrAll( totals.testCases.total() )
+                              << pluralise( totals.testCases.total(), "test case" )
+                              << " (no assertions).";
+            }
+            else if( totals.assertions.failed ) {
+                Colour colour( Colour::ResultError );
+                stream <<
+                    "Failed " << pluralise( totals.testCases.failed, "test case"  ) << ", "
+                    "failed " << pluralise( totals.assertions.failed, "assertion" ) << '.';
+            }
+            else {
+            }
+        }
+    };
+
+    INTERNAL_CATCH_REGISTER_REPORTER( "minimal", MinimalReporter )
+
+} // end namespace Catch
+
+int main(int argc, const char *argv[]) {
+	const char *catch_argv[] = { argv[0], "-r", "minimal" };
+	constexpr auto catch_argc = sizeof(catch_argv) / sizeof(catch_argv[0]);
+	auto result = Catch::Session().run(catch_argc, catch_argv);
+	if (result == 0) {
+		for (auto i = 0; i != argc; ++i) {
+			if (argv[i] == QString("--touch") && i + 1 != argc) {
+				QFile(QFile::decodeName(argv[++i])).open(QIODevice::WriteOnly);
+			}
+		}
+	}
+	return (result < 0xff ? result : 0xff);
+}
+

commit f8b5da3b4b06dd459f133b7e4a8cb8450488e533

Fix travis build.
Regression was introduced in 82254f8cae.

GYP patch for GCC precompiled header support was accidentally removed.

.travis/build.sh
@@ -601,7 +601,7 @@ buildGYP() {
   git clone https://chromium.googlesource.com/external/gyp
 
   cd "$EXTERNAL/gyp"
-  git checkout 702ac58e4772
+  git checkout 702ac58e47
   git apply "$GYP_PATCH"
   cp -r * "$GYP_PATH/"
 }

Telegram/Patches/gyp.diff
@@ -1,5 +1,33 @@
+diff --git a/pylib/gyp/generator/cmake.py b/pylib/gyp/generator/cmake.py
+index a2b9629..68d7020 100644
+--- a/pylib/gyp/generator/cmake.py
++++ b/pylib/gyp/generator/cmake.py
+@@ -1070,6 +1070,23 @@ def WriteTarget(namer, qualified_target, target_dicts, build_dir, config_to_use,
+ 
+       output.write(')\n')
+ 
++  # Precompile header
++  precompiled_header = config.get('cmake_precompiled_header', '')
++  if precompiled_header:
++    precompiled_header_script = config.get('cmake_precompiled_header_script', '')
++    if not precompiled_header_script:
++      print ('ERROR: cmake_precompiled_header requires cmake_precompiled_header_script')
++    cmake_precompiled_header = NormjoinPath(path_from_cmakelists_to_gyp, precompiled_header)
++    cmake_precompiled_header_script = NormjoinPathForceCMakeSource(path_from_cmakelists_to_gyp, precompiled_header_script)
++    output.write('include(')
++    output.write(cmake_precompiled_header_script)
++    output.write(')\n')
++    output.write('add_precompiled_header(')
++    output.write(cmake_target_name)
++    output.write(' ')
++    output.write(cmake_precompiled_header)
++    output.write(')\n')
++
+   UnsetVariable(output, 'TOOLSET')
+   UnsetVariable(output, 'TARGET')
+ 
 diff --git a/pylib/gyp/generator/xcode.py b/pylib/gyp/generator/xcode.py
-index 0e3fb93..4c824ec 100644
+index db99d6a..8d56baf 100644
 --- a/pylib/gyp/generator/xcode.py
 +++ b/pylib/gyp/generator/xcode.py
 @@ -72,6 +72,10 @@ generator_additional_non_configuration_keys = [
@@ -13,7 +41,7 @@ index 0e3fb93..4c824ec 100644
    'mac_bundle',
    'mac_bundle_resources',
    'mac_framework_headers',
-@@ -761,6 +765,26 @@ def GenerateOutput(target_list, target_dicts, data, params):
+@@ -772,6 +776,26 @@ def GenerateOutput(target_list, target_dicts, data, params):
      xcode_targets[qualified_target] = xct
      xcode_target_to_target_dict[xct] = spec
  
@@ -40,7 +68,7 @@ index 0e3fb93..4c824ec 100644
      spec_actions = spec.get('actions', [])
      spec_rules = spec.get('rules', [])
  
-@@ -1130,7 +1154,8 @@ exit 1
+@@ -1141,7 +1165,8 @@ exit 1
          groups = [x for x in groups if not x.endswith('_excluded')]
        for group in groups:
          for item in rule.get(group, []):