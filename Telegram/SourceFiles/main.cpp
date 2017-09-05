ked33/tdesktop

commit b595bfca003960fe8714d3b5d0aa6e593454ea98

Improve current executable path computing.
Fixes #960 (hopefully).

Telegram/SourceFiles/settings.cpp
@@ -163,13 +163,26 @@ void settingsParseArgs(int argc, char *argv[]) {
 
 	QStringList args;
 	for (int32 i = 0; i < argc; ++i) {
-		args.push_back('"' + fromUtf8Safe(argv[i]) + '"');
+		args.push_back(fromUtf8Safe(argv[i]));
 	}
 	gArguments = args.join(' ');
 
-	gExeDir = psCurrentExeDirectory(argc, argv);
-	gExeName = psCurrentExeName(argc, argv);
-    for (int32 i = 0; i < argc; ++i) {
+	auto path = Platform::CurrentExecutablePath(argc, argv);
+	LOG(("Executable path before check: %1").arg(path));
+	if (!path.isEmpty()) {
+		auto info = QFileInfo(path);
+		if (info.isSymLink()) {
+			info = info.symLinkTarget();
+		}
+		if (info.exists()) {
+			gExeDir = info.absoluteDir().absolutePath() + '/';
+			gExeName = info.fileName();
+		}
+	}
+	if (cExeName().isEmpty()) {
+		LOG(("WARNING: Could not compute executable path, some features will be disabled."));
+	}
+	for (auto i = 0; i != argc; ++i) {
 		if (qstr("-testmode") == argv[i]) {
 			gTestMode = true;
 		} else if (qstr("-debug") == argv[i]) {

Telegram/SourceFiles/application.cpp
@@ -454,7 +454,7 @@ void Application::startUpdateCheck(bool forceWait) {
 	if (!Sandbox::started()) return;
 
 	_updateCheckTimer->stop();
-	if (_updateThread || _updateReply || !cAutoUpdate()) return;
+	if (_updateThread || _updateReply || !cAutoUpdate() || cExeName().isEmpty()) return;
 
 	int32 constDelay = cBetaVersion() ? 600 : UpdateDelayConstPart, randDelay = cBetaVersion() ? 300 : UpdateDelayRandPart;
 	int32 updateInSecs = cLastUpdateCheck() + constDelay + int32(rand() % randDelay) - unixtime();

Telegram/SourceFiles/autoupdater.cpp
@@ -486,7 +486,7 @@ UpdateChecker::~UpdateChecker() {
 
 bool checkReadyUpdate() {
 	QString readyFilePath = cWorkingDir() + qsl("tupdates/temp/ready"), readyPath = cWorkingDir() + qsl("tupdates/temp");
-	if (!QFile(readyFilePath).exists()) {
+	if (!QFile(readyFilePath).exists() || cExeName().isEmpty()) {
 		if (QDir(cWorkingDir() + qsl("tupdates/ready")).exists() || QDir(cWorkingDir() + qsl("tupdates/temp")).exists()) {
 			UpdateChecker::clearAll();
 		}

Telegram/SourceFiles/logs.cpp
@@ -369,7 +369,7 @@ namespace Logs {
 		LOG(("Executable dir: %1, name: %2").arg(cExeDir()).arg(cExeName()));
 		LOG(("Initial working dir: %1").arg(initialWorkingDir));
 		LOG(("Working dir: %1").arg(cWorkingDir()));
-		LOG(("Arguments: %1").arg(cArguments()));
+		LOG(("Command line: %1").arg(cArguments()));
 
 		if (!LogsData) {
 			LOG(("FATAL: Could not open '%1' for writing log!").arg(_logsFilePath(LogDataMain, qsl("_startXX"))));
@@ -391,7 +391,6 @@ namespace Logs {
 			for (LogsInMemoryList::const_iterator i = list.cbegin(), e = list.cend(); i != e; ++i) {
 				if (i->first == LogDataMain) {
 					_logsWrite(i->first, i->second);
-					LOG(("First: %1, %2").arg(i->first).arg(i->second));
 				}
 			}
 		}

Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -40,6 +40,27 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 using namespace Platform;
 using Platform::File::internal::EscapeShell;
 
+namespace Platform {
+
+QString CurrentExecutablePath(int argc, char *argv[]) {
+	constexpr auto kMaxPath = 1024;
+	char result[kMaxPath] = { 0 };
+	auto count = readlink("/proc/self/exe", result, kMaxPath);
+	if (count > 0) {
+		auto filename = QFile::decodeName(result);
+		auto deletedPostfix = qstr(" (deleted)");
+		if (filename.endsWith(deletedPostfix) && !QFileInfo(filename).exists()) {
+			filename.chop(deletedPostfix.size());
+		}
+		return filename;
+	}
+
+	// Fallback to the first command line argument.
+	return argc ? QFile::decodeName(argv[0]) : QString();
+}
+
+} // namespace Platform
+
 namespace {
 
 class _PsEventFilter : public QAbstractNativeEventFilter {
@@ -104,7 +125,7 @@ QString demanglestr(const QString &mangled) {
 
 QStringList addr2linestr(uint64 *addresses, int count) {
 	QStringList result;
-	if (!count) return result;
+	if (!count || cExeName().isEmpty()) return result;
 
 	result.reserve(count);
 	QByteArray cmd = "addr2line -e " + EscapeShell(QFile::encodeName(cExeDir() + cExeName()));
@@ -306,34 +327,6 @@ QString psDownloadPath() {
 	return QStandardPaths::writableLocation(QStandardPaths::DownloadLocation) + '/' + str_const_toString(AppName) + '/';
 }
 
-QString psCurrentExeDirectory(int argc, char *argv[]) {
-	QString first = argc ? QFile::decodeName(argv[0]) : QString();
-	if (!first.isEmpty()) {
-		QFileInfo info(first);
-		if (info.isSymLink()) {
-			info = info.symLinkTarget();
-		}
-		if (info.exists()) {
-			return QDir(info.absolutePath()).absolutePath() + '/';
-		}
-	}
-	return QString();
-}
-
-QString psCurrentExeName(int argc, char *argv[]) {
-	QString first = argc ? QFile::decodeName(argv[0]) : QString();
-	if (!first.isEmpty()) {
-		QFileInfo info(first);
-		if (info.isSymLink()) {
-			info = info.symLinkTarget();
-		}
-		if (info.exists()) {
-			return info.fileName();
-		}
-	}
-	return QString();
-}
-
 void psDoCleanup() {
 	try {
 		psAutoStart(false, true);
@@ -431,7 +424,7 @@ bool _psRunCommand(const QByteArray &command) {
 void psRegisterCustomScheme() {
 #ifndef TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME
 	auto home = getHomeDir();
-	if (home.isEmpty() || cBetaVersion()) return; // don't update desktop file for beta version
+	if (home.isEmpty() || cBetaVersion() || cExeName().isEmpty()) return; // don't update desktop file for beta version
 
 #ifndef TDESKTOP_DISABLE_DESKTOP_FILE_GENERATION
 	DEBUG_LOG(("App Info: placing .desktop file"));
@@ -533,10 +526,13 @@ void psNewVersion() {
 }
 
 bool _execUpdater(bool update = true, const QString &crashreport = QString()) {
+	if (cExeName().isEmpty()) {
+		return false;
+	}
 	static const int MaxLen = 65536, MaxArgsCount = 128;
 
 	char path[MaxLen] = {0};
-	QByteArray data(QFile::encodeName(cExeDir() + (update ? "Updater" : gExeName)));
+	QByteArray data(QFile::encodeName(cExeDir() + (update ? "Updater" : cExeName())));
 	memcpy(path, data.constData(), data.size());
 
 	char *args[MaxArgsCount] = { 0 };
@@ -554,6 +550,8 @@ bool _execUpdater(bool update = true, const QString &crashreport = QString()) {
 	char p_crashreportbuf[MaxLen] = { 0 };
 	char p_exe[] = "-exename";
 	char p_exebuf[MaxLen] = { 0 };
+	char p_exepath[] = "-exepath";
+	char p_exepathbuf[MaxLen] = { 0 };
 	int argIndex = 0;
 	args[argIndex++] = path;
 	if (!update) {
@@ -591,6 +589,12 @@ bool _execUpdater(bool update = true, const QString &crashreport = QString()) {
 		memcpy(p_exebuf, exef.constData(), exef.size());
 		args[argIndex++] = p_exe;
 		args[argIndex++] = p_exebuf;
+	}
+	QByteArray exepathf = QFile::encodeName(cExeDir());
+	if (exepathf.size() > 0 && exepathf.size() < MaxLen) {
+		memcpy(p_exepathbuf, exepathf.constData(), exepathf.size());
+		args[argIndex++] = p_exepath;
+		args[argIndex++] = p_exepathbuf;
 	}
 
 	Logs::closeMain();

Telegram/SourceFiles/platform/linux/specific_linux.h
@@ -37,6 +37,8 @@ inline void DeInitOnTopPanel(QWidget *panel) {
 inline void ReInitOnTopPanel(QWidget *panel) {
 }
 
+QString CurrentExecutablePath(int argc, char *argv[]);
+
 } // namespace Platform
 
 inline QString psServerPrefix() {
@@ -65,8 +67,6 @@ void psActivateProcess(uint64 pid = 0);
 QString psLocalServerPrefix();
 QString psAppDataPath();
 QString psDownloadPath();
-QString psCurrentExeDirectory(int argc, char *argv[]);
-QString psCurrentExeName(int argc, char *argv[]);
 void psAutoStart(bool start, bool silent = false);
 void psSendToMenu(bool send, bool silent = false);
 

Telegram/SourceFiles/platform/mac/specific_mac.h
@@ -25,6 +25,8 @@ inline bool TranslucentWindowsSupported(QPoint globalPosition) {
 	return true;
 }
 
+QString CurrentExecutablePath(int argc, char *argv[]);
+
 namespace ThirdParty {
 
 inline void start() {
@@ -66,8 +68,6 @@ void psActivateProcess(uint64 pid = 0);
 QString psLocalServerPrefix();
 QString psAppDataPath();
 QString psDownloadPath();
-QString psCurrentExeDirectory(int argc, char *argv[]);
-QString psCurrentExeName(int argc, char *argv[]);
 void psAutoStart(bool start, bool silent = false);
 void psSendToMenu(bool send, bool silent = false);
 

commit a7dcdeb7141c5efe1539f2223b4448301586c139

Fix sending files from "Send To" in Windows.
Regression was introduced in b595bfca00.

Telegram/SourceFiles/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 #endif // Q_OS_MAC
 	QCoreApplication::setApplicationName(qsl("TelegramDesktop"));
 
-	settingsParseArgs(argc, argv);
+	InitFromCommandLine(argc, argv);
 	if (cLaunchMode() == LaunchModeFixPrevious) {
 		return psFixPrevious();
 	} else if (cLaunchMode() == LaunchModeCleanup) {

Telegram/SourceFiles/settings.cpp
@@ -119,7 +119,78 @@ int32 gAutoDownloadAudio = 0;
 int32 gAutoDownloadGif = 0;
 bool gAutoPlayGif = true;
 
-void settingsParseArgs(int argc, char *argv[]) {
+void ParseCommandLineArguments(const QStringList &arguments) {
+	enum class KeyFormat {
+		NoValues,
+		OneValue,
+		AllLeftValues,
+	};
+	auto parseMap = std::map<QByteArray, KeyFormat> {
+		{ "-testmode"   , KeyFormat::NoValues },
+		{ "-debug"      , KeyFormat::NoValues },
+		{ "-many"       , KeyFormat::NoValues },
+		{ "-key"        , KeyFormat::OneValue },
+		{ "-autostart"  , KeyFormat::NoValues },
+		{ "-fixprevios" , KeyFormat::NoValues },
+		{ "-cleanup"    , KeyFormat::NoValues },
+		{ "-noupdate"   , KeyFormat::NoValues },
+		{ "-tosettings" , KeyFormat::NoValues },
+		{ "-startintray", KeyFormat::NoValues },
+		{ "-sendpath"   , KeyFormat::AllLeftValues },
+		{ "-workdir"    , KeyFormat::OneValue },
+		{ "--"          , KeyFormat::OneValue },
+	};
+	auto parseResult = QMap<QByteArray, QStringList>();
+	auto parsingKey = QByteArray();
+	auto parsingFormat = KeyFormat::NoValues;
+	for (auto &argument : arguments) {
+		switch (parsingFormat) {
+		case KeyFormat::OneValue: {
+			parseResult[parsingKey] = QStringList(argument.mid(0, 8192));
+			parsingFormat = KeyFormat::NoValues;
+		} break;
+		case KeyFormat::AllLeftValues: {
+			parseResult[parsingKey].push_back(argument.mid(0, 8192));
+		} break;
+		case KeyFormat::NoValues: {
+			parsingKey = argument.toLatin1();
+			auto it = parseMap.find(parsingKey);
+			if (it != parseMap.end()) {
+				parsingFormat = it->second;
+				parseResult[parsingKey] = QStringList();
+			}
+		} break;
+		}
+	}
+
+	gTestMode = parseResult.contains("-testmode");
+	gDebug = parseResult.contains("-debug");
+	gManyInstance = parseResult.contains("-many");
+	gKeyFile = parseResult.value("-key", QStringList(QString())).front();
+	gLaunchMode = parseResult.contains("-autostart") ? LaunchModeAutoStart
+		: parseResult.contains("-fixprevious") ? LaunchModeFixPrevious
+		: parseResult.contains("-cleanup") ? LaunchModeCleanup : LaunchModeNormal;
+	gNoStartUpdate = parseResult.contains("-noupdate");
+	gStartToSettings = parseResult.contains("-tosettings");
+	gStartInTray = parseResult.contains("-startintray");
+	gSendPaths = parseResult.value("-sendpath", QStringList());
+	gWorkingDir = parseResult.value("-workdir", QStringList(QString())).front();
+	if (!gWorkingDir.isEmpty() && !QDir().exists(gWorkingDir)) {
+		gWorkingDir = QString();
+	}
+	gStartUrl = parseResult.value("--", QStringList(QString())).front();
+}
+
+void InitFromCommandLine(int argc, char *argv[]) {
+	Expects(argc >= 0);
+
+	auto arguments = QStringList();
+	arguments.reserve(argc);
+	for (auto i = 0; i != argc; ++i) {
+		arguments.push_back(fromUtf8Safe(argv[i]));
+	}
+	gArguments = arguments.join(' ');
+
 #ifdef Q_OS_MAC
 #ifndef OS_MAC_OLD
 	if (QSysInfo::macVersion() >= QSysInfo::MV_10_11) {
@@ -161,12 +232,6 @@ void settingsParseArgs(int argc, char *argv[]) {
 	break;
 	}
 
-	QStringList args;
-	for (int32 i = 0; i < argc; ++i) {
-		args.push_back(fromUtf8Safe(argv[i]));
-	}
-	gArguments = args.join(' ');
-
 	auto path = Platform::CurrentExecutablePath(argc, argv);
 	LOG(("Executable path before check: %1").arg(path));
 	if (!path.isEmpty()) {
@@ -182,40 +247,8 @@ void settingsParseArgs(int argc, char *argv[]) {
 	if (cExeName().isEmpty()) {
 		LOG(("WARNING: Could not compute executable path, some features will be disabled."));
 	}
-	for (auto i = 0; i != argc; ++i) {
-		if (qstr("-testmode") == argv[i]) {
-			gTestMode = true;
-		} else if (qstr("-debug") == argv[i]) {
-			gDebug = true;
-		} else if (qstr("-many") == argv[i]) {
-			gManyInstance = true;
-		} else if (qstr("-key") == argv[i] && i + 1 < argc) {
-			gKeyFile = fromUtf8Safe(argv[++i]);
-		} else if (qstr("-autostart") == argv[i]) {
-			gLaunchMode = LaunchModeAutoStart;
-		} else if (qstr("-fixprevious") == argv[i]) {
-			gLaunchMode = LaunchModeFixPrevious;
-		} else if (qstr("-cleanup") == argv[i]) {
-			gLaunchMode = LaunchModeCleanup;
-		} else if (qstr("-noupdate") == argv[i]) {
-			gNoStartUpdate = true;
-		} else if (qstr("-tosettings") == argv[i]) {
-			gStartToSettings = true;
-		} else if (qstr("-startintray") == argv[i]) {
-			gStartInTray = true;
-		} else if (qstr("-sendpath") == argv[i] && i + 1 < argc) {
-			for (++i; i < argc; ++i) {
-				gSendPaths.push_back(fromUtf8Safe(argv[i]));
-			}
-		} else if (qstr("-workdir") == argv[i] && i + 1 < argc) {
-			QString dir = fromUtf8Safe(argv[++i]);
-			if (QDir().exists(dir)) {
-				gWorkingDir = dir;
-			}
-		} else if (qstr("--") == argv[i] && i + 1 < argc) {
-			gStartUrl = fromUtf8Safe(argv[++i]).mid(0, 8192);
-		}
-	}
+
+	ParseCommandLineArguments(arguments);
 }
 
 RecentStickerPack &cGetRecentStickers() {

Telegram/SourceFiles/settings.h
@@ -20,6 +20,8 @@ Copyright (c) 2014-2017 John Preston, https://desktop.telegram.org
 */
 #pragma once
 
+void InitFromCommandLine(int argc, char *argv[]);
+
 extern bool gDebug;
 inline bool cDebug() {
 #if defined _DEBUG