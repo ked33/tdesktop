ked33/tdesktop

commit 6a959f8ad3a0ae7e487d38e1aac0210d623b06eb

Use Storage::Cache::Database for file caching.

Telegram/SourceFiles/data/data_photo.cpp
@@ -111,13 +111,29 @@ void PhotoData::forget() {
 
 ImagePtr PhotoData::makeReplyPreview(Data::FileOrigin origin) {
 	if (replyPreview->isNull() && !thumb->isNull()) {
-		if (thumb->loaded()) {
-			int w = thumb->width(), h = thumb->height();
+		const auto previewFromImage = [&](const ImagePtr &image) {
+			if (!image->loaded()) {
+				image->load(origin);
+				return ImagePtr();
+			}
+			int w = image->width(), h = image->height();
 			if (w <= 0) w = 1;
 			if (h <= 0) h = 1;
-			replyPreview = ImagePtr(w > h ? thumb->pix(origin, w * st::msgReplyBarSize.height() / h, st::msgReplyBarSize.height()) : thumb->pix(origin, st::msgReplyBarSize.height()), "PNG");
+			return ImagePtr(
+				(w > h
+					? image->pix(
+						origin,
+						w * st::msgReplyBarSize.height() / h,
+						st::msgReplyBarSize.height())
+					: image->pix(origin, st::msgReplyBarSize.height())),
+				"PNG");
+		};
+		if (thumb->toDelayedStorageImage()
+			&& !full->isNull()
+			&& !full->toDelayedStorageImage()) {
+			return previewFromImage(full);
 		} else {
-			thumb->load(origin);
+			return previewFromImage(thumb);
 		}
 	}
 	return replyPreview;
@@ -128,6 +144,21 @@ MTPInputPhoto PhotoData::mtpInput() const {
 		MTP_long(id),
 		MTP_long(access),
 		MTP_bytes(fileReference));
+}
+
+void PhotoData::collectLocalData(PhotoData *local) {
+	if (local == this) return;
+
+	const auto copyImage = [](const ImagePtr &src, const ImagePtr &dst) {
+		if (const auto from = src->cacheKey()) {
+			if (const auto to = dst->cacheKey()) {
+				Auth().data().cache().copyIfEmpty(*from, *to);
+			}
+		}
+	};
+	copyImage(local->thumb, thumb);
+	copyImage(local->medium, medium);
+	copyImage(local->full, full);
 }
 
 void PhotoOpenClickHandler::onClickImpl() const {

Telegram/SourceFiles/boxes/local_storage_box.cpp
@@ -37,29 +37,14 @@ void LocalStorageBox::prepare() {
 }
 
 void LocalStorageBox::updateControls() {
-	auto rowsHeight = 0;
-	if (_imagesCount > 0 && _audiosCount > 0) {
-		rowsHeight = 2 * (st::linkFont->height + st::localStorageBoxSkip);
-	} else {
-		rowsHeight = st::linkFont->height + st::localStorageBoxSkip;
-	}
-	_clear->setVisible(_imagesCount > 0 || _audiosCount > 0);
+	const auto rowsHeight = st::linkFont->height + st::localStorageBoxSkip;
+	_clear->setVisible(false);
 	setDimensions(st::boxWidth, st::localStorageBoxSkip + rowsHeight + _clear->height());
 	_clear->moveToLeft(st::boxPadding.left(), st::localStorageBoxSkip + rowsHeight);
 	update();
 }
 
 void LocalStorageBox::checkLocalStoredCounts() {
-	int imagesCount = Local::hasImages() + Local::hasStickers() + Local::hasWebFiles();
-	int audiosCount = Local::hasAudios();
-	if (imagesCount != _imagesCount || audiosCount != _audiosCount) {
-		_imagesCount = imagesCount;
-		_audiosCount = audiosCount;
-		if (_imagesCount > 0 || _audiosCount > 0) {
-			_state = State::Normal;
-		}
-		updateControls();
-	}
 }
 
 void LocalStorageBox::paintEvent(QPaintEvent *e) {
@@ -71,19 +56,8 @@ void LocalStorageBox::paintEvent(QPaintEvent *e) {
 	p.setPen(st::windowFg);
 	checkLocalStoredCounts();
 	auto top = st::localStorageBoxSkip;
-	if (_imagesCount > 0) {
-		auto text = lng_settings_images_cached(lt_count, _imagesCount, lt_size, formatSizeText(Local::storageImagesSize() + Local::storageStickersSize() + Local::storageWebFilesSize()));
-		p.drawTextLeft(st::boxPadding.left(), top, width(), text);
-		top += st::boxTextFont->height + st::localStorageBoxSkip;
-	}
-	if (_audiosCount > 0) {
-		auto text = lng_settings_audios_cached(lt_count, _audiosCount, lt_size, formatSizeText(Local::storageAudiosSize()));
-		p.drawTextLeft(st::boxPadding.left(), top, width(), text);
-		top += st::boxTextFont->height + st::localStorageBoxSkip;
-	} else if (_imagesCount <= 0) {
-		p.drawTextLeft(st::boxPadding.left(), top, width(), lang(lng_settings_no_data_cached));
-		top += st::boxTextFont->height + st::localStorageBoxSkip;
-	}
+	p.drawTextLeft(st::boxPadding.left(), top, width(), lang(lng_settings_no_data_cached));
+	top += st::boxTextFont->height + st::localStorageBoxSkip;
 	auto text = ([this]() -> QString {
 		switch (_state) {
 		case State::Clearing: return lang(lng_local_storage_clearing);

Telegram/SourceFiles/boxes/local_storage_box.h
@@ -43,7 +43,4 @@ private:
 
 	object_ptr<Ui::LinkButton> _clear;
 
-	int _imagesCount = -1;
-	int _audiosCount = -1;
-
 };

Telegram/SourceFiles/core/launcher.cpp
@@ -221,7 +221,8 @@ void Launcher::processArguments() {
 	gTestMode = parseResult.contains("-testmode");
 	Logs::SetDebugEnabled(parseResult.contains("-debug"));
 	gManyInstance = parseResult.contains("-many");
-	gKeyFile = parseResult.value("-key", QStringList()).join(QString());
+	gKeyFile = parseResult.value("-key", {}).join(QString()).toLower();
+	gKeyFile = gKeyFile.replace(QRegularExpression("[^a-z0-9\\-_]"), {});
 	gLaunchMode = parseResult.contains("-autostart") ? LaunchModeAutoStart
 		: parseResult.contains("-fixprevious") ? LaunchModeFixPrevious
 		: parseResult.contains("-cleanup") ? LaunchModeCleanup
@@ -229,8 +230,8 @@ void Launcher::processArguments() {
 	gNoStartUpdate = parseResult.contains("-noupdate");
 	gStartToSettings = parseResult.contains("-tosettings");
 	gStartInTray = parseResult.contains("-startintray");
-	gSendPaths = parseResult.value("-sendpath", QStringList());
-	gWorkingDir = parseResult.value("-workdir", QStringList()).join(QString());
+	gSendPaths = parseResult.value("-sendpath", {});
+	gWorkingDir = parseResult.value("-workdir", {}).join(QString());
 	if (!gWorkingDir.isEmpty()) {
 		if (QDir().exists(gWorkingDir)) {
 			_customWorkingDir = true;
@@ -238,7 +239,7 @@ void Launcher::processArguments() {
 			gWorkingDir = QString();
 		}
 	}
-	gStartUrl = parseResult.value("--", QStringList()).join(QString());
+	gStartUrl = parseResult.value("--", {}).join(QString());
 }
 
 int Launcher::executeApplication() {

Telegram/SourceFiles/data/data_document.cpp
@@ -20,6 +20,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history_item.h"
 #include "history/history_media_types.h"
 #include "window/window_controller.h"
+#include "storage/cache/storage_cache_database.h"
 #include "auth_session.h"
 #include "mainwindow.h"
 #include "messenger.h"
@@ -1087,6 +1088,16 @@ MediaKey DocumentData::mediaKey() const {
 	return ::mediaKey(locationType(), _dc, id);
 }
 
+Storage::Cache::Key DocumentData::cacheKey() const {
+	if (hasWebLocation()) {
+		return Data::WebDocumentCacheKey(_urlLocation);
+	} else if (!_access && !_url.isEmpty()) {
+		return Data::UrlCacheKey(_url);
+	} else {
+		return Data::DocumentCacheKey(_dc, id);
+	}
+}
+
 QString DocumentData::composeNameString() const {
 	if (auto songData = song()) {
 		return ComposeNameString(
@@ -1207,17 +1218,9 @@ void DocumentData::setWebLocation(const WebFileLocation &location) {
 void DocumentData::collectLocalData(DocumentData *local) {
 	if (local == this) return;
 
+	_session->data().cache().copyIfEmpty(local->cacheKey(), cacheKey());
 	if (!local->_data.isEmpty()) {
 		_data = local->_data;
-		if (isVoiceMessage()) {
-			if (!Local::copyAudio(local->mediaKey(), mediaKey())) {
-				Local::writeAudio(mediaKey(), _data);
-			}
-		} else {
-			if (!Local::copyStickerImage(local->mediaKey(), mediaKey())) {
-				Local::writeStickerImage(mediaKey(), _data);
-			}
-		}
 	}
 	if (!local->_location.isEmpty()) {
 		_location = local->_location;

Telegram/SourceFiles/data/data_document.h
@@ -9,6 +9,12 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include "data/data_types.h"
 
+namespace Storage {
+namespace Cache {
+struct Key;
+} // namespace Cache
+} // namespace Storage
+
 class AuthSession;
 class mtpFileLoader;
 
@@ -173,6 +179,7 @@ public:
 	void setMimeString(const QString &mime);
 
 	MediaKey mediaKey() const;
+	Storage::Cache::Key cacheKey() const;
 
 	static QString ComposeNameString(
 		const QString &filename,

Telegram/SourceFiles/data/data_media_types.cpp
@@ -312,22 +312,20 @@ bool MediaPhoto::updateInlineResultMedia(const MTPMessageMedia &media) {
 	if (media.type() != mtpc_messageMediaPhoto) {
 		return false;
 	}
-	auto &photo = media.c_messageMediaPhoto();
-	if (photo.has_photo() && !photo.has_ttl_seconds()) {
-		if (auto existing = Auth().data().photo(photo.vphoto)) {
-			if (existing == _photo) {
-				return true;
-			} else {
-				// collect data
-			}
+	auto &data = media.c_messageMediaPhoto();
+	if (data.has_photo() && !data.has_ttl_seconds()) {
+		const auto photo = Auth().data().photo(data.vphoto);
+		if (photo == _photo) {
+			return true;
+		} else {
+			photo->collectLocalData(_photo);
 		}
 	} else {
 		LOG(("API Error: "
 			"Got MTPMessageMediaPhoto without photo "
 			"or with ttl_seconds in updateInlineResultMedia()"));
 	}
-	// Can return false if we collect the data.
-	return true;
+	return false;
 }
 
 bool MediaPhoto::updateSentMedia(const MTPMessageMedia &media) {
@@ -347,6 +345,20 @@ bool MediaPhoto::updateSentMedia(const MTPMessageMedia &media) {
 	if (photo.type() != mtpc_photo) {
 		return false;
 	}
+	const auto saveImageToCache = [](
+			const MTPDfileLocation &location,
+			const ImagePtr &image) {
+		const auto key = StorageImageLocation(0, 0, location);
+		if (key.isNull() || image->isNull() || !image->loaded()) {
+			return;
+		}
+		if (image->savedData().isEmpty()) {
+			image->forget();
+		}
+		Auth().data().cache().putIfEmpty(
+			Data::StorageCacheKey(key),
+			image->savedData());
+	};
 	auto &sizes = photo.c_photo().vsizes.v;
 	auto max = 0;
 	const MTPDfileLocation *maxLocation = 0;
@@ -369,23 +381,24 @@ bool MediaPhoto::updateSentMedia(const MTPMessageMedia &media) {
 		if (!loc || loc->type() != mtpc_fileLocation) {
 			continue;
 		}
+		const auto &location = loc->c_fileLocation();
 		if (size == 's') {
-			Local::writeImage(storageKey(loc->c_fileLocation()), _photo->thumb);
+			saveImageToCache(location, _photo->thumb);
 		} else if (size == 'm') {
-			Local::writeImage(storageKey(loc->c_fileLocation()), _photo->medium);
+			saveImageToCache(location, _photo->medium);
 		} else if (size == 'x' && max < 1) {
 			max = 1;
-			maxLocation = &loc->c_fileLocation();
+			maxLocation = &location;
 		} else if (size == 'y' && max < 2) {
 			max = 2;
-			maxLocation = &loc->c_fileLocation();
+			maxLocation = &location;
 		//} else if (size == 'w' && max < 3) {
 		//	max = 3;
 		//	maxLocation = &loc->c_fileLocation();
 		}
 	}
 	if (maxLocation) {
-		Local::writeImage(storageKey(*maxLocation), _photo->full);
+		saveImageToCache(*maxLocation, _photo->full);
 	}
 	return true;
 }
@@ -637,13 +650,6 @@ bool MediaFile::updateSentMedia(const MTPMessageMedia &media) {
 		return false;
 	}
 	Auth().data().documentConvert(_document, data.vdocument);
-	if (!_document->data().isEmpty()) {
-		if (_document->isVoiceMessage()) {
-			Local::writeAudio(_document->mediaKey(), _document->data());
-		} else {
-			Local::writeStickerImage(_document->mediaKey(), _document->data());
-		}
-	}
 	return true;
 }
 

Telegram/SourceFiles/data/data_photo.h
@@ -43,6 +43,12 @@ public:
 
 	MTPInputPhoto mtpInput() const;
 
+	// When we have some client-side generated photo
+	// (for example for displaying an external inline bot result)
+	// and it has downloaded full image, we can collect image from it
+	// to (this) received from the server "same" photo.
+	void collectLocalData(PhotoData *local);
+
 	PhotoId id = 0;
 	uint64 access = 0;
 	QByteArray fileReference;

commit 9ac4afa7526118ae9979355eae7490dc3f595144

Fix memory leak in reply previews.
Regression was introduced in 6a959f8ad3.

Fixes #5237.

Telegram/SourceFiles/data/data_photo.cpp
@@ -131,9 +131,9 @@ ImagePtr PhotoData::makeReplyPreview(Data::FileOrigin origin) {
 		if (thumb->toDelayedStorageImage()
 			&& !full->isNull()
 			&& !full->toDelayedStorageImage()) {
-			return previewFromImage(full);
+			replyPreview = previewFromImage(full);
 		} else {
-			return previewFromImage(thumb);
+			replyPreview = previewFromImage(thumb);
 		}
 	}
 	return replyPreview;