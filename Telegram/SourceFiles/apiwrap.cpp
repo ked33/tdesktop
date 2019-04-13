ked33/tdesktop

commit 7a5bf23240b82e55c38b76a7a0d985f7c9bb2e47

Mask channel as forbidden on CHANNEL_PRIVATE error.
Fixes #5902.

Telegram/SourceFiles/apiwrap.cpp
@@ -1661,6 +1661,9 @@ void ApiWrap::requestSelfParticipant(not_null<ChannelData*> channel) {
 	}
 
 	const auto finalize = [=](UserId inviter, TimeId inviteDate) {
+		if (inviter < 0) {
+			channel->markForbidden();
+		}
 		channel->inviter = inviter;
 		channel->inviteDate = inviteDate;
 		if (const auto history = _session->data().historyLoaded(channel)) {

Telegram/SourceFiles/data/data_channel.cpp
@@ -267,6 +267,17 @@ void ChannelData::applyEditBanned(not_null<UserData*> user, const MTPChatBannedR
 	Notify::peerUpdatedDelayed(this, flags);
 }
 
+void ChannelData::markForbidden() {
+	owner().processChat(MTP_channelForbidden(
+		MTP_flags(isMegagroup()
+			? MTPDchannelForbidden::Flag::f_megagroup
+			: MTPDchannelForbidden::Flag::f_broadcast),
+		MTP_int(bareId()),
+		MTP_long(access),
+		MTP_string(name),
+		MTPint()));
+}
+
 bool ChannelData::isGroupAdmin(not_null<UserData*> user) const {
 	if (auto info = mgInfo.get()) {
 		return info->admins.contains(peerToUser(user->id));

Telegram/SourceFiles/data/data_channel.h
@@ -180,6 +180,8 @@ public:
 		const MTPChatBannedRights &oldRights,
 		const MTPChatBannedRights &newRights);
 
+	void markForbidden();
+
 	bool isGroupAdmin(not_null<UserData*> user) const;
 
 	bool lastParticipantsCountOutdated() const {

Telegram/SourceFiles/history/history.cpp
@@ -2454,7 +2454,7 @@ void History::dialogEntryApplied() {
 	if (!chatListMessage()) {
 		if (const auto channel = peer->asChannel()) {
 			const auto inviter = channel->inviter;
-			if (inviter != 0 && channel->amIn()) {
+			if (inviter > 0 && channel->amIn()) {
 				if (const auto from = owner().userLoaded(inviter)) {
 					clear(ClearType::Unload);
 					addNewerSlice(QVector<MTPMessage>());

commit b014b4d2b125e2061be786c7f3ce7ded810e99f9

Fix wronly marking channel as forbidden.
Regression was introduced in 7a5bf23240.

Fixes #5923.

Telegram/SourceFiles/apiwrap.cpp
@@ -1661,9 +1661,6 @@ void ApiWrap::requestSelfParticipant(not_null<ChannelData*> channel) {
 	}
 
 	const auto finalize = [=](UserId inviter, TimeId inviteDate) {
-		if (inviter < 0) {
-			channel->markForbidden();
-		}
 		channel->inviter = inviter;
 		channel->inviteDate = inviteDate;
 		if (const auto history = _session->data().historyLoaded(channel)) {
@@ -1708,6 +1705,9 @@ void ApiWrap::requestSelfParticipant(not_null<ChannelData*> channel) {
 		});
 	}).fail([=](const RPCError &error) {
 		_selfParticipantRequests.erase(channel);
+		if (error.type() == qstr("CHANNEL_PRIVATE")) {
+			channel->markForbidden();
+		}
 		finalize(-1, 0);
 	}).afterDelay(kSmallDelayMs).send();
 }