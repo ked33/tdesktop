ked33/tdesktop

commit 22df3310618e52070bf2166c402c8ded186199f5

Show transfer community button, set 2sv password.

Telegram/SourceFiles/boxes/passcode_box.cpp
@@ -226,10 +226,17 @@ void PasscodeBox::setInnerFocus() {
 }
 
 void PasscodeBox::setPasswordDone(const QByteArray &newPasswordBytes) {
+	if (_replacedBy) {
+		_replacedBy->closeBox();
+	}
 	_setRequest = 0;
 	_newPasswordSet.fire_copy(newPasswordBytes);
-	auto text = lang(_reenterPasscode->isHidden() ? lng_cloud_password_removed : (_oldPasscode->isHidden() ? lng_cloud_password_was_set : lng_cloud_password_updated));
-	getDelegate()->show(Box<InformBox>(text), LayerOption::CloseOther);
+	const auto weak = make_weak(this);
+	const auto text = lang(_reenterPasscode->isHidden() ? lng_cloud_password_removed : (_oldPasscode->isHidden() ? lng_cloud_password_was_set : lng_cloud_password_updated));
+	getDelegate()->show(Box<InformBox>(text));
+	if (weak) {
+		closeBox();
+	}
 }
 
 void PasscodeBox::closeReplacedBy() {
@@ -353,8 +360,7 @@ void PasscodeBox::validateEmail(
 			submit,
 			resend,
 			errors->events(),
-			resent->events()),
-		LayerOption::KeepOther);
+			resent->events()));
 
 	box->setCloseByOutsideClick(false);
 	box->setCloseByEscape(false);
@@ -441,9 +447,12 @@ void PasscodeBox::save(bool force) {
 		}
 		if (!_recoverEmail->isHidden() && email.isEmpty() && !force) {
 			_skipEmailWarning = true;
-			_replacedBy = getDelegate()->show(Box<ConfirmBox>(lang(lng_cloud_password_about_recover), lang(lng_cloud_password_skip_email), st::attentionBoxButton, crl::guard(this, [this] {
-				save(true);
-			})));
+			_replacedBy = getDelegate()->show(
+				Box<ConfirmBox>(
+					lang(lng_cloud_password_about_recover),
+					lang(lng_cloud_password_skip_email),
+					st::attentionBoxButton,
+					crl::guard(this, [this] { save(true); })));
 		} else if (_newPasscode->isHidden()) {
 			clearCloudPassword(old);
 		} else if (_oldPasscode->isHidden()) {
@@ -452,6 +461,7 @@ void PasscodeBox::save(bool force) {
 			changeCloudPassword(old, pwd);
 		}
 	} else {
+		closeReplacedBy();
 		const auto weak = make_weak(this);
 		cSetPasscodeBadTries(0);
 		Local::setPasscode(pwd.toUtf8());

Telegram/Resources/langs/lang.strings
@@ -1651,6 +1651,20 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 "lng_rights_chat_banned_custom" = "Custom";
 "lng_rights_chat_banned_custom_date" = "Until {date}";
 
+"lng_rights_transfer_group" = "Transfer group ownership";
+"lng_rights_transfer_channel" = "Transfer channel ownership";
+"lng_rights_transfer_check" = "Security check";
+"lng_rights_transfer_check_about" = "You can transfer this group to {user} only if you have:";
+"lng_rights_transfer_check_password" = "• Enabled **2-Step Verification** more than **7 days** ago.";
+"lng_rights_transfer_check_session" = "• Logged in on this device more than **24 hours** ago.";
+"lng_rights_transfer_check_later" = "Please come back later.";
+"lng_rights_transfer_set_password" = "Set password";
+"lng_rights_transfer_about" = "This will transfer the full **owner rights** for {group} to {user}.";
+"lng_rights_transfer_sure" = "Change owner";
+"lng_rights_transfer_password" = "Please enter your password to complete the transfer.";
+"lng_rights_transfer_done_group" = "{user} is now the owner of the group.";
+"lng_rights_transfer_done_channel" = "{user} is now the owner of the channel.";
+
 "lng_restricted_send_message" = "The admins of this group restricted you from writing here.";
 "lng_restricted_send_media" = "The admins of this group restricted you from posting media content here.";
 "lng_restricted_send_stickers" = "The admins of this group restricted you from posting stickers here.";

Telegram/SourceFiles/boxes/boxes.style
@@ -985,3 +985,5 @@ addContactWarningMargin: margins(19px, 10px, 19px, 5px);
 blockUserConfirmation: FlatLabel(boxLabel) {
 	minWidth: 240px;
 }
+
+transferCheckWidth: 300px;

Telegram/SourceFiles/boxes/peers/edit_linked_chat_box.cpp
@@ -163,7 +163,7 @@ void Controller::choose(not_null<ChatData*> chat) {
 	}
 	text.append("\n\n" + lang(lng_manage_discussion_group_private));
 	text.append("\n\n");
-	text.append(lng_manage_discussion_group_warning__generic(
+	text.append(lng_manage_discussion_group_warning__rich(
 		lt_visible,
 		Ui::Text::Bold(lang(lng_manage_discussion_group_visible))));
 	const auto box = std::make_shared<QPointer<BoxContent>>();

Telegram/SourceFiles/boxes/peers/edit_participant_box.cpp
@@ -10,24 +10,92 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "lang/lang_keys.h"
 #include "ui/wrap/vertical_layout.h"
 #include "ui/wrap/padding_wrap.h"
+#include "ui/wrap/slide_wrap.h"
 #include "ui/widgets/checkbox.h"
 #include "ui/widgets/labels.h"
 #include "ui/widgets/buttons.h"
+#include "ui/text/text_utilities.h"
 #include "ui/text_options.h"
 #include "ui/special_buttons.h"
+#include "info/profile/info_profile_button.h"
+#include "settings/settings_privacy_security.h"
 #include "boxes/calendar_box.h"
+#include "boxes/generic_box.h"
 #include "boxes/peers/edit_peer_permissions_box.h"
+#include "boxes/peers/edit_peer_info_box.h"
 #include "data/data_peer_values.h"
 #include "data/data_channel.h"
 #include "data/data_chat.h"
 #include "data/data_user.h"
+#include "core/core_cloud_password.h"
+#include "apiwrap.h"
+#include "auth_session.h"
 #include "styles/style_boxes.h"
+#include "styles/style_info.h"
 
 namespace {
 
 constexpr auto kMaxRestrictDelayDays = 366;
 constexpr auto kSecondsInDay = 24 * 60 * 60;
 constexpr auto kSecondsInWeek = 7 * kSecondsInDay;
+
+enum class PasswordErrorType {
+	None,
+	NoPassword,
+	Later,
+};
+
+void SetCloudPassword(not_null<GenericBox*> box, not_null<UserData*> user) {
+	user->session().api().reloadPasswordState();
+	user->session().api().passwordState(
+	) | rpl::start_with_next([=] {
+		using namespace Settings;
+		const auto weak = make_weak(box);
+		if (CheckEditCloudPassword()) {
+			box->getDelegate()->show(
+				EditCloudPasswordBox(&user->session()));
+		} else {
+			box->getDelegate()->show(CloudPasswordAppOutdatedBox());
+		}
+		if (weak) {
+			weak->closeBox();
+		}
+	}, box->lifetime());
+}
+
+void TransferPasswordError(
+		not_null<GenericBox*> box,
+		not_null<UserData*> user,
+		PasswordErrorType error) {
+	box->setTitle(langFactory(lng_rights_transfer_check));
+	box->setWidth(st::transferCheckWidth);
+
+	auto text = lng_rights_transfer_check_about__rich(
+		lt_user,
+		Ui::Text::Bold(user->shortName())
+	).append('\n').append('\n').append(
+		Ui::Text::RichLangValue(lang(lng_rights_transfer_check_password))
+	).append('\n').append('\n').append(
+		Ui::Text::RichLangValue(lang(lng_rights_transfer_check_session))
+	);
+	if (error == PasswordErrorType::Later) {
+		text.append('\n').append('\n').append(
+			Ui::Text::RichLangValue(lang(lng_rights_transfer_check_later))
+		);
+	}
+	box->addRow(object_ptr<Ui::FlatLabel>(
+		box,
+		rpl::single(text),
+		st::boxLabel));
+	if (error == PasswordErrorType::Later) {
+		box->addButton(langFactory(lng_box_ok), [=] { box->closeBox(); });
+	} else {
+		box->addButton(langFactory(lng_rights_transfer_set_password), [=] {
+			SetCloudPassword(box, user);
+		});
+		box->addButton(langFactory(lng_cancel), [=] { box->closeBox(); });
+	}
+}
 
 } // namespace
 
@@ -244,6 +312,7 @@ void EditAdminBox::prepare() {
 		return result;
 	}();
 
+	const auto isGroup = chat || channel->isMegagroup();
 	const auto anyoneCanAddMembers = chat
 		? chat->anyoneCanAddMembers()
 		: channel->anyoneCanAddMembers();
@@ -252,18 +321,31 @@ void EditAdminBox::prepare() {
 		lng_rights_edit_admin_header,
 		prepareFlags,
 		disabledMessages,
-		peer()->isChat() || peer()->isMegagroup(),
+		isGroup,
 		anyoneCanAddMembers);
 	addControl(std::move(checkboxes), QMargins());
 
-	_aboutAddAdmins = addControl(
-		object_ptr<Ui::FlatLabel>(this, st::boxLabel),
-		st::rightsAboutMargin);
-	rpl::single(
+	auto selectedFlags = rpl::single(
 		getChecked()
 	) | rpl::then(std::move(
 		changes
-	)) | rpl::map(
+	));
+	if (canTransferOwnership()) {
+		const auto allFlags = FullAdminRights(isGroup);
+		setupTransferButton(
+			isGroup
+		)->toggleOn(rpl::duplicate(
+			selectedFlags
+		) | rpl::map(
+			((_1 & allFlags) == allFlags)
+		))->setDuration(0);
+	}
+	_aboutAddAdmins = addControl(
+		object_ptr<Ui::FlatLabel>(this, st::boxLabel),
+		st::rightsAboutMargin);
+	std::move(
+		selectedFlags
+	) | rpl::map(
 		(_1 & Flag::f_add_admins) != 0
 	) | rpl::distinct_until_changed(
 	) | rpl::start_with_next([=](bool checked) {
@@ -287,6 +369,88 @@ void EditAdminBox::prepare() {
 	} else {
 		addButton(langFactory(lng_box_ok), [this] { closeBox(); });
 	}
+}
+
+bool EditAdminBox::canTransferOwnership() const {
+	if (user()->isInaccessible() || user()->isBot()) {
+		return false;
+	} else if (const auto chat = peer()->asChat()) {
+		return chat->amCreator();
+	} else if (const auto channel = peer()->asChannel()) {
+		return channel->amCreator();
+	}
+	Unexpected("Chat type in EditAdminBox::canTransferOwnership.");
+}
+
+not_null<Ui::SlideWrap<Ui::RpWidget>*> EditAdminBox::setupTransferButton(
+		bool isGroup) {
+	const auto wrap = addControl(
+		object_ptr<Ui::SlideWrap<Ui::VerticalLayout>>(
+			this,
+			object_ptr<Ui::VerticalLayout>(this)));
+
+	const auto container = wrap->entity();
+	const auto addDivider = [&] {
+		container->add(
+			object_ptr<BoxContentDivider>(container),
+			{ 0, st::infoProfileSkip, 0, st::infoProfileSkip });
+	};
+
+	addDivider();
+	container->add(EditPeerInfoBox::CreateButton(
+		this,
+		Lang::Viewer(isGroup
+			? lng_rights_transfer_group
+			: lng_rights_transfer_channel),
+		rpl::single(QString()),
+		[=] { transferOwnership(); },
+		st::peerPermissionsButton));
+	addDivider();
+
+	return wrap;
+}
+
+void EditAdminBox::transferOwnership() {
+	if (_checkTransferRequestId) {
+		return;
+	}
+	const auto channel = peer()->isChannel()
+		? peer()->asChannel()->inputChannel
+		: MTP_inputChannelEmpty();
+	const auto api = &peer()->session().api();
+	_checkTransferRequestId = api->request(MTPchannels_EditCreator(
+		channel,
+		MTP_inputUserEmpty(),
+		MTP_inputCheckPasswordEmpty()
+	)).fail([=](const RPCError &error) {
+		_checkTransferRequestId = 0;
+		if (!handleTransferPasswordError(error)) {
+			requestTransferPassword();
+		}
+	}).send();
+}
+
+bool EditAdminBox::handleTransferPasswordError(const RPCError &error) {
+	const auto type = [&] {
+		const auto &type = error.type();
+		if (type == qstr("PASSWORD_MISSING")) {
+			return PasswordErrorType::NoPassword;
+		} else if (type.startsWith(qstr("PASSWORD_TOO_FRESH_"))
+			|| type.startsWith(qstr("SESSION_TOO_FRESH_"))) {
+			return PasswordErrorType::Later;
+		}
+		return PasswordErrorType::None;
+	}();
+	if (type == PasswordErrorType::None) {
+		return false;
+	}
+
+	getDelegate()->show(Box(TransferPasswordError, user(), type));
+	return true;
+}
+
+void EditAdminBox::requestTransferPassword() {
+
 }
 
 void EditAdminBox::refreshAboutAddAdminsText(bool canAddAdmins) {

Telegram/SourceFiles/boxes/peers/edit_participant_box.h
@@ -16,6 +16,8 @@ class LinkButton;
 class Checkbox;
 class Radiobutton;
 class RadiobuttonGroup;
+template <typename Widget>
+class SlideWrap;
 } // namespace Ui
 
 class CalendarBox;
@@ -77,15 +79,21 @@ private:
 
 	static MTPChatAdminRights Defaults(not_null<PeerData*> peer);
 
+	void transferOwnership();
+	bool handleTransferPasswordError(const RPCError &error);
+	void requestTransferPassword();
 	bool canSave() const {
 		return !!_saveCallback;
 	}
 	void refreshAboutAddAdminsText(bool canAddAdmins);
+	bool canTransferOwnership() const;
+	not_null<Ui::SlideWrap<Ui::RpWidget>*> setupTransferButton(bool isGroup);
 
 	const MTPChatAdminRights _oldRights;
 	Fn<void(MTPChatAdminRights, MTPChatAdminRights)> _saveCallback;
 
 	QPointer<Ui::FlatLabel> _aboutAddAdmins;
+	mtpRequestId _checkTransferRequestId = 0;
 
 };
 

Telegram/SourceFiles/boxes/peers/edit_peer_permissions_box.cpp
@@ -95,7 +95,7 @@ std::vector<std::pair<ChatRestrictions, LangKey>> RestrictionLabels() {
 	const auto restrictions = Data::ListOfRestrictions();
 	auto i = 0;
 	for (const auto key : langKeys) {
-		vector.push_back({restrictions[i++], key});
+		vector.emplace_back(restrictions[i++], key);
 	}
 	return vector;
 }
@@ -274,6 +274,14 @@ ChatRestrictions FixDependentRestrictions(ChatRestrictions restrictions) {
 	while (fixOne()) {
 	}
 	return restrictions;
+}
+
+ChatAdminRights FullAdminRights(bool isGroup) {
+	auto result = ChatAdminRights();
+	for (const auto &[flag, label] : AdminRightLabels(isGroup, true)) {
+		result |= flag;
+	}
+	return result;
 }
 
 EditPeerPermissionsBox::EditPeerPermissionsBox(

Telegram/SourceFiles/boxes/peers/edit_peer_permissions_box.h
@@ -58,3 +58,4 @@ EditFlagsControl<MTPDchatAdminRights::Flags> CreateEditAdminRights(
 
 ChatAdminRights DisabledByDefaultRestrictions(not_null<PeerData*> peer);
 ChatRestrictions FixDependentRestrictions(ChatRestrictions restrictions);
+ChatAdminRights FullAdminRights(bool isGroup);

commit 44f8564ab4198ecfe24285902f9adfc67f38caff

Fix 2SV setup.
Regression was introduced in 22df331061.

Telegram/SourceFiles/boxes/passcode_box.cpp
@@ -343,7 +343,7 @@ void PasscodeBox::validateEmail(
 	const auto errors = std::make_shared<rpl::event_stream<QString>>();
 	const auto resent = std::make_shared<rpl::event_stream<QString>>();
 	const auto set = std::make_shared<bool>(false);
-	const auto submit = [=](QString code) {
+	const auto submit = crl::guard(this, [=](QString code) {
 		if (_setRequest) {
 			return;
 		}
@@ -372,8 +372,8 @@ void PasscodeBox::validateEmail(
 				errors->fire(Lang::Hard::ServerError());
 			}
 		}).handleFloodErrors().send();
-	};
-	const auto resend = [=] {
+	});
+	const auto resend = crl::guard(this, [=] {
 		if (_setRequest) {
 			return;
 		}
@@ -385,8 +385,8 @@ void PasscodeBox::validateEmail(
 			_setRequest = 0;
 			errors->fire(Lang::Hard::ServerError());
 		}).send();
-	};
-	const auto box = getDelegate()->show(
+	});
+	const auto box = _replacedBy = getDelegate()->show(
 		Passport::VerifyEmailBox(
 			email,
 			codeLength,