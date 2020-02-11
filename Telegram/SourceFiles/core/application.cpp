ked33/tdesktop

commit fc9b571eb1736cec376923e984fb6482806b79cd

Change show order of MainWindow/OverlayWindow.
Fixes #6804.

Telegram/SourceFiles/window/main_window.cpp
@@ -392,14 +392,18 @@ void MainWindow::initSize() {
 	auto position = cWindowPos();
 	DEBUG_LOG(("Window Pos: Initializing first %1, %2, %3, %4 (maximized %5)").arg(position.x).arg(position.y).arg(position.w).arg(position.h).arg(Logs::b(position.maximized)));
 
-	auto avail = QDesktopWidget().availableGeometry();
+	const auto primaryScreen = QGuiApplication::primaryScreen();
+	auto geometryScreen = primaryScreen;
+	const auto available = primaryScreen
+		? primaryScreen->availableGeometry()
+		: QRect(0, 0, st::windowDefaultWidth, st::windowDefaultHeight);
 	bool maximized = false;
-	auto geom = QRect(
-		avail.x() + std::max(
-			(avail.width() - st::windowDefaultWidth) / 2,
+	auto geometry = QRect(
+		available.x() + std::max(
+			(available.width() - st::windowDefaultWidth) / 2,
 			0),
-		avail.y() + std::max(
-			(avail.height() - st::windowDefaultHeight) / 2,
+		available.y() + std::max(
+			(available.height() - st::windowDefaultHeight) / 2,
 			0),
 		st::windowDefaultWidth,
 		st::windowDefaultHeight);
@@ -420,7 +424,8 @@ void MainWindow::initSize() {
 					if (position.x + st::windowMinWidth <= screenGeometry.x() + screenGeometry.width() &&
 						position.y + st::windowMinHeight <= screenGeometry.y() + screenGeometry.height()) {
 						DEBUG_LOG(("Window Pos: Resulting geometry is %1, %2, %3, %4").arg(position.x).arg(position.y).arg(position.w).arg(position.h));
-						geom = QRect(position.x, position.y, position.w, position.h);
+						geometry = QRect(position.x, position.y, position.w, position.h);
+						geometryScreen = screen;
 					}
 				}
 				break;
@@ -428,8 +433,17 @@ void MainWindow::initSize() {
 		}
 		maximized = position.maximized;
 	}
-	DEBUG_LOG(("Window Pos: Setting first %1, %2, %3, %4").arg(geom.x()).arg(geom.y()).arg(geom.width()).arg(geom.height()));
-	setGeometry(geom);
+	DEBUG_LOG(("Window Pos: Setting first %1, %2, %3, %4").arg(geometry.x()).arg(geometry.y()).arg(geometry.width()).arg(geometry.height()));
+	setGeometry(geometry);
+	if (geometryScreen != primaryScreen) {
+		// In case screen DPI changed we show the window now,
+		// so that when we call setGeometry() once again after
+		// make_unique<Media::View::OverlayWidget> it already
+		// has adjusted by dpi geometry saved in QWidget.
+		//
+		// Somehow should fix https://github.com/telegramdesktop/tdesktop/issues/6804
+		show();
+	}
 }
 
 void MainWindow::positionUpdated() {

commit 398bef124cf95d242b7f99f78401bd98fc9b921b

Fix invisible passcode field.
Regression was introduced in fc9b571eb.

Telegram/SourceFiles/core/application.cpp
@@ -207,10 +207,6 @@ void Application::run() {
 
 	_window = std::make_unique<Window::Controller>(&activeAccount());
 
-	const auto currentGeometry = _window->widget()->geometry();
-	_mediaView = std::make_unique<Media::View::OverlayWidget>();
-	_window->widget()->setGeometry(currentGeometry);
-
 	QCoreApplication::instance()->installEventFilter(this);
 	connect(
 		static_cast<QGuiApplication*>(QCoreApplication::instance()),
@@ -241,6 +237,8 @@ void Application::run() {
 	}
 	DEBUG_LOG(("Application Info: showing."));
 	_window->firstShow();
+
+	_mediaView = std::make_unique<Media::View::OverlayWidget>();
 
 	if (!locked() && cStartToSettings()) {
 		_window->showSettings();

Telegram/SourceFiles/mainwindow.cpp
@@ -194,6 +194,7 @@ void MainWindow::setupPasscodeLock() {
 	if (animated) {
 		_passcodeLock->showAnimated(bg);
 	} else {
+		_passcodeLock->showFinished();
 		setInnerFocus();
 	}
 }

Telegram/SourceFiles/platform/win/main_window_win.cpp
@@ -833,12 +833,8 @@ void MainWindow::psFirstShow() {
 		if (Global::WorkMode().value() == dbiwmTrayOnly
 			|| Global::WorkMode().value() == dbiwmWindowAndTray) {
 			hide();
-		} else {
-			show();
 		}
 		showShadows = false;
-	} else {
-		show();
 	}
 
 	setPositionInited();

Telegram/SourceFiles/window/main_window.cpp
@@ -435,15 +435,6 @@ void MainWindow::initSize() {
 	}
 	DEBUG_LOG(("Window Pos: Setting first %1, %2, %3, %4").arg(geometry.x()).arg(geometry.y()).arg(geometry.width()).arg(geometry.height()));
 	setGeometry(geometry);
-	if (geometryScreen != primaryScreen) {
-		// In case screen DPI changed we show the window now,
-		// so that when we call setGeometry() once again after
-		// make_unique<Media::View::OverlayWidget> it already
-		// has adjusted by dpi geometry saved in QWidget.
-		//
-		// Somehow should fix https://github.com/telegramdesktop/tdesktop/issues/6804
-		show();
-	}
 }
 
 void MainWindow::positionUpdated() {

Telegram/SourceFiles/window/window_lock_widgets.cpp
@@ -69,13 +69,15 @@ void LockWidget::showAnimated(const QPixmap &bgAnimCache, bool back) {
 void LockWidget::animationCallback() {
 	update();
 	if (!_a_show.animating()) {
-		showChildren();
-		_window->widget()->setInnerFocus();
-
-		Ui::showChatsList();
-
-		_cacheUnder = _cacheOver = QPixmap();
+		showFinished();
 	}
+}
+
+void LockWidget::showFinished() {
+	showChildren();
+	_window->widget()->setInnerFocus();
+	Ui::showChatsList();
+	_cacheUnder = _cacheOver = QPixmap();
 }
 
 void LockWidget::paintEvent(QPaintEvent *e) {

Telegram/SourceFiles/window/window_lock_widgets.h
@@ -32,6 +32,7 @@ public:
 	virtual void setInnerFocus();
 
 	void showAnimated(const QPixmap &bgAnimCache, bool back = false);
+	void showFinished();
 
 protected:
 	void paintEvent(QPaintEvent *e) override;