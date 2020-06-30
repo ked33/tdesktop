ked33/tdesktop

commit 2fbd7be930c2486dd5fd854cc38858f6d4968696

Move some more settings to Core::App.

Telegram/SourceFiles/core/core_settings.cpp
@@ -11,13 +11,18 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/widgets/input_fields.h"
 #include "storage/serialize_common.h"
 #include "window/themes/window_theme.h"
+#include "window/section_widget.h"
+#include "base/platform/base_platform_info.h"
 #include "facades.h"
 
 namespace Core {
 
 Settings::Settings()
 : _sendFilesWay(SendFilesWay::Album)
-, _sendSubmitWay(Ui::InputSubmitSettings::Enter) {
+, _sendSubmitWay(Ui::InputSubmitSettings::Enter)
+, _floatPlayerColumn(Window::Column::Second)
+, _floatPlayerCorner(RectPart::TopRight)
+, _dialogsWidthRatio(DefaultDialogsWidthRatio()) {
 }
 
 QByteArray Settings::serialize() const {
@@ -291,6 +296,66 @@ QString Settings::getSoundPath(const QString &key) const {
 	return qsl(":/sounds/") + key + qsl(".mp3");
 }
 
+void Settings::setTabbedSelectorSectionEnabled(bool enabled) {
+	_tabbedSelectorSectionEnabled = enabled;
+	if (enabled) {
+		setThirdSectionInfoEnabled(false);
+	}
+	setTabbedReplacedWithInfo(false);
+}
+
+rpl::producer<bool> Settings::tabbedReplacedWithInfoValue() const {
+	return _tabbedReplacedWithInfoValue.events_starting_with(
+		tabbedReplacedWithInfo());
+}
+
+void Settings::setThirdSectionInfoEnabled(bool enabled) {
+	if (_thirdSectionInfoEnabled != enabled) {
+		_thirdSectionInfoEnabled = enabled;
+		if (enabled) {
+			setTabbedSelectorSectionEnabled(false);
+		}
+		setTabbedReplacedWithInfo(false);
+		_thirdSectionInfoEnabledValue.fire_copy(enabled);
+	}
+}
+
+rpl::producer<bool> Settings::thirdSectionInfoEnabledValue() const {
+	return _thirdSectionInfoEnabledValue.events_starting_with(
+		thirdSectionInfoEnabled());
+}
+
+void Settings::setTabbedReplacedWithInfo(bool enabled) {
+	if (_tabbedReplacedWithInfo != enabled) {
+		_tabbedReplacedWithInfo = enabled;
+		_tabbedReplacedWithInfoValue.fire_copy(enabled);
+	}
+}
+
+void Settings::setDialogsWidthRatio(float64 ratio) {
+	_dialogsWidthRatio = ratio;
+}
+
+float64 Settings::dialogsWidthRatio() const {
+	return _dialogsWidthRatio.current();
+}
+
+rpl::producer<float64> Settings::dialogsWidthRatioChanges() const {
+	return _dialogsWidthRatio.changes();
+}
+
+void Settings::setThirdColumnWidth(int width) {
+	_thirdColumnWidth = width;
+}
+
+int Settings::thirdColumnWidth() const {
+	return _thirdColumnWidth.current();
+}
+
+rpl::producer<int> Settings::thirdColumnWidthChanges() const {
+	return _thirdColumnWidth.changes();
+}
+
 void Settings::resetOnLastLogout() {
 	_adaptiveForWide = true;
 	_moderateModeEnabled = false;
@@ -340,6 +405,24 @@ void Settings::resetOnLastLogout() {
 	_dictionariesEnabled = std::vector<int>();
 	_autoDownloadDictionaries = true;
 	_mainMenuAccountsShown = false;
+	_tabbedSelectorSectionEnabled = false; // per-window
+	_floatPlayerColumn = Window::Column::Second; // per-window
+	_floatPlayerCorner = RectPart::TopRight; // per-window
+	_thirdSectionInfoEnabled = true; // per-window
+	_thirdSectionExtendedBy = -1; // per-window
+	_dialogsWidthRatio = DefaultDialogsWidthRatio(); // per-window
+	_thirdColumnWidth = kDefaultThirdColumnWidth; // p-w
+	_tabbedReplacedWithInfo = false; // per-window
+}
+
+bool Settings::ThirdColumnByDefault() {
+	return Platform::IsMacStoreBuild();
+}
+
+float64 Settings::DefaultDialogsWidthRatio() {
+	return ThirdColumnByDefault()
+		? kDefaultBigDialogsWidthRatio
+		: kDefaultDialogsWidthRatio;
 }
 
 } // namespace Core

Telegram/SourceFiles/core/core_settings.h
@@ -10,10 +10,15 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "window/themes/window_themes_embedded.h"
 
 enum class SendFilesWay;
+enum class RectPart;
 
 namespace Ui {
 enum class InputSubmitSettings;
 } // namespace Ui
+
+namespace Window {
+enum class Column;
+} // namespace Window
 
 namespace Core {
 
@@ -358,7 +363,47 @@ public:
 	void setMainMenuAccountsShown(bool value) {
 		_mainMenuAccountsShown = value;
 	}
+	[[nodiscard]] bool tabbedSelectorSectionEnabled() const {
+		return _tabbedSelectorSectionEnabled;
+	}
+	void setTabbedSelectorSectionEnabled(bool enabled);
+	[[nodiscard]] bool thirdSectionInfoEnabled() const {
+		return _thirdSectionInfoEnabled;
+	}
+	void setThirdSectionInfoEnabled(bool enabled);
+	[[nodiscard]] rpl::producer<bool> thirdSectionInfoEnabledValue() const;
+	[[nodiscard]] int thirdSectionExtendedBy() const {
+		return _thirdSectionExtendedBy;
+	}
+	void setThirdSectionExtendedBy(int savedValue) {
+		_thirdSectionExtendedBy = savedValue;
+	}
+	[[nodiscard]] bool tabbedReplacedWithInfo() const {
+		return _tabbedReplacedWithInfo;
+	}
+	void setTabbedReplacedWithInfo(bool enabled);
+	[[nodiscard]] rpl::producer<bool> tabbedReplacedWithInfoValue() const;
+	void setFloatPlayerColumn(Window::Column column) {
+		_floatPlayerColumn = column;
+	}
+	[[nodiscard]] Window::Column floatPlayerColumn() const {
+		return _floatPlayerColumn;
+	}
+	void setFloatPlayerCorner(RectPart corner) {
+		_floatPlayerCorner = corner;
+	}
+	[[nodiscard]] RectPart floatPlayerCorner() const {
+		return _floatPlayerCorner;
+	}
+	void setDialogsWidthRatio(float64 ratio);
+	[[nodiscard]] float64 dialogsWidthRatio() const;
+	[[nodiscard]] rpl::producer<float64> dialogsWidthRatioChanges() const;
+	void setThirdColumnWidth(int width);
+	[[nodiscard]] int thirdColumnWidth() const;
+	[[nodiscard]] rpl::producer<int> thirdColumnWidthChanges() const;
 
+	[[nodiscard]] static bool ThirdColumnByDefault();
+	[[nodiscard]] float64 DefaultDialogsWidthRatio();
 	[[nodiscard]] static qint32 SerializePlaybackSpeed(float64 speed) {
 		return int(std::round(std::clamp(speed * 4., 2., 8.))) - 2;
 	}
@@ -369,16 +414,17 @@ public:
 	void resetOnLastLogout();
 
 private:
+	static constexpr auto kDefaultThirdColumnWidth = 0;
+	static constexpr auto kDefaultDialogsWidthRatio = 5. / 14;
+	static constexpr auto kDefaultBigDialogsWidthRatio = 0.275;
+
 	bool _adaptiveForWide = true;
 	bool _moderateModeEnabled = false;
-
 	rpl::variable<float64> _songVolume = kDefaultVolume;
 	rpl::variable<float64> _videoVolume = kDefaultVolume;
-
 	bool _askDownloadPath = false;
 	rpl::variable<QString> _downloadPath;
 	QByteArray _downloadPathBookmark;
-
 	bool _voiceMsgPlaybackDoubled = false;
 	bool _soundNotify = true;
 	bool _desktopNotify = true;
@@ -391,20 +437,16 @@ private:
 	bool _countUnreadMessages = true;
 	rpl::variable<bool> _notifyAboutPinned = true;
 	int _autoLock = 3600;
-
 	QString _callOutputDeviceID = u"default"_q;
 	QString _callInputDeviceID = u"default"_q;
 	int _callOutputVolume = 100;
 	int _callInputVolume = 100;
 	bool _callAudioDuckingEnabled = true;
-
 	Window::Theme::AccentColors _themesAccentColors;
-
 	bool _lastSeenWarningSeen = false;
 	SendFilesWay _sendFilesWay;
 	Ui::InputSubmitSettings _sendSubmitWay;
 	base::flat_map<QString, QString> _soundOverrides;
-
 	bool _exeLaunchWarning = true;
 	bool _loopAnimatedStickers = true;
 	rpl::variable<bool> _largeEmoji = true;
@@ -417,6 +459,17 @@ private:
 	rpl::variable<std::vector<int>> _dictionariesEnabled;
 	rpl::variable<bool> _autoDownloadDictionaries = true;
 	rpl::variable<bool> _mainMenuAccountsShown = false;
+	bool _tabbedSelectorSectionEnabled = false; // per-window
+	Window::Column _floatPlayerColumn; // per-window
+	RectPart _floatPlayerCorner; // per-window
+	bool _thirdSectionInfoEnabled = true; // per-window
+	rpl::event_stream<bool> _thirdSectionInfoEnabledValue; // per-window
+	int _thirdSectionExtendedBy = -1; // per-window
+	rpl::variable<float64> _dialogsWidthRatio; // per-window
+	rpl::variable<int> _thirdColumnWidth = kDefaultThirdColumnWidth; // p-w
+
+	bool _tabbedReplacedWithInfo = false; // per-window
+	rpl::event_stream<bool> _tabbedReplacedWithInfoValue; // per-window
 
 	float64 _rememberedSongVolume = kDefaultVolume;
 	bool _rememberedSoundNotifyFromTray = false;

Telegram/SourceFiles/history/history_widget.cpp
@@ -4154,11 +4154,11 @@ bool HistoryWidget::pushTabbedSelectorToThirdSection(
 	if (!_tabbedPanel) {
 		return true;
 	} else if (!peer->canWrite()) {
-		session().settings().setTabbedReplacedWithInfo(true);
+		Core::App().settings().setTabbedReplacedWithInfo(true);
 		controller()->showPeerInfo(peer, params.withThirdColumn());
 		return false;
 	}
-	session().settings().setTabbedReplacedWithInfo(false);
+	Core::App().settings().setTabbedReplacedWithInfo(false);
 	controller()->resizeForThirdSection();
 	controller()->showSection(
 		ChatHelpers::TabbedMemento(),
@@ -4198,8 +4198,8 @@ void HistoryWidget::toggleTabbedSelectorMode() {
 	}
 	if (_tabbedPanel) {
 		if (controller()->canShowThirdSection() && !Adaptive::OneColumn()) {
-			session().settings().setTabbedSelectorSectionEnabled(true);
-			session().saveSettingsDelayed();
+			Core::App().settings().setTabbedSelectorSectionEnabled(true);
+			Core::App().saveSettingsDelayed();
 			pushTabbedSelectorToThirdSection(
 				_peer,
 				Window::SectionShow::Way::ClearStack);

Telegram/SourceFiles/history/view/history_view_compose_controls.cpp
@@ -21,8 +21,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "chat_helpers/message_field.h"
 #include "chat_helpers/emoji_suggestions_widget.h"
 #include "window/window_session_controller.h"
-#include "main/main_session.h"
-#include "main/main_session_settings.h"
 #include "core/application.h"
 #include "core/core_settings.h"
 #include "inline_bots/inline_results_widget.h"
@@ -347,11 +345,11 @@ bool ComposeControls::pushTabbedSelectorToThirdSection(
 	if (!_tabbedPanel) {
 		return true;
 	//} else if (!_canSendMessages) {
-	//	session().settings().setTabbedReplacedWithInfo(true);
+	//	Core::App().settings().setTabbedReplacedWithInfo(true);
 	//	_window->showPeerInfo(_peer, params.withThirdColumn());
 	//	return;
 	}
-	session().settings().setTabbedReplacedWithInfo(false);
+	Core::App().settings().setTabbedReplacedWithInfo(false);
 	_tabbedSelectorToggle->setColorOverrides(
 		&st::historyAttachEmojiActive,
 		&st::historyRecordVoiceFgActive,
@@ -396,8 +394,8 @@ void ComposeControls::toggleTabbedSelectorMode() {
 	}
 	if (_tabbedPanel) {
 		if (_window->canShowThirdSection() && !Adaptive::OneColumn()) {
-			session().settings().setTabbedSelectorSectionEnabled(true);
-			session().saveSettingsDelayed();
+			Core::App().settings().setTabbedSelectorSectionEnabled(true);
+			Core::App().saveSettingsDelayed();
 			pushTabbedSelectorToThirdSection(
 				_history->peer,
 				Window::SectionShow::Way::ClearStack);

Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
@@ -18,10 +18,11 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "mainwidget.h"
 #include "mainwindow.h"
 #include "main/main_session.h"
-#include "main/main_session_settings.h"
 #include "mtproto/mtproto_config.h"
 #include "lang/lang_keys.h"
 #include "core/shortcuts.h"
+#include "core/application.h"
+#include "core/core_settings.h"
 #include "ui/widgets/buttons.h"
 #include "ui/widgets/dropdown_menu.h"
 #include "ui/effects/radial_animation.h"
@@ -137,17 +138,17 @@ TopBarWidget::TopBarWidget(
 	}, lifetime());
 
 	rpl::combine(
-		session().settings().thirdSectionInfoEnabledValue(),
-		session().settings().tabbedReplacedWithInfoValue()
-	) | rpl::start_with_next(
-		[this] { updateInfoToggleActive(); },
-		lifetime());
+		Core::App().settings().thirdSectionInfoEnabledValue(),
+		Core::App().settings().tabbedReplacedWithInfoValue()
+	) | rpl::start_with_next([=] {
+		updateInfoToggleActive();
+	}, lifetime());
 
 	rpl::single(rpl::empty_value()) | rpl::then(
 		base::ObservableViewer(Global::RefConnectionTypeChanged())
-	) | rpl::start_with_next(
-		[=] { updateConnectingState(); },
-		lifetime());
+	) | rpl::start_with_next([=] {
+		updateConnectingState();
+	}, lifetime());
 
 	setCursor(style::cur_pointer);
 }
@@ -252,13 +253,13 @@ void TopBarWidget::showMenu() {
 
 void TopBarWidget::toggleInfoSection() {
 	if (Adaptive::ThreeColumn()
-		&& (session().settings().thirdSectionInfoEnabled()
-			|| session().settings().tabbedReplacedWithInfo())) {
+		&& (Core::App().settings().thirdSectionInfoEnabled()
+			|| Core::App().settings().tabbedReplacedWithInfo())) {
 		_controller->closeThirdSection();
 	} else if (_activeChat.peer()) {
 		if (_controller->canShowThirdSection()) {
-			session().settings().setThirdSectionInfoEnabled(true);
-			session().saveSettingsDelayed();
+			Core::App().settings().setThirdSectionInfoEnabled(true);
+			Core::App().saveSettingsDelayed();
 			if (Adaptive::ThreeColumn()) {
 				_controller->showSection(
 					Info::Memento::Default(_activeChat.peer()),
@@ -791,8 +792,8 @@ void TopBarWidget::updateUnreadBadge() {
 
 void TopBarWidget::updateInfoToggleActive() {
 	auto infoThirdActive = Adaptive::ThreeColumn()
-		&& (session().settings().thirdSectionInfoEnabled()
-			|| session().settings().tabbedReplacedWithInfo());
+		&& (Core::App().settings().thirdSectionInfoEnabled()
+			|| Core::App().settings().tabbedReplacedWithInfo());
 	auto iconOverride = infoThirdActive
 		? &st::topBarInfoActive
 		: nullptr;

commit 949970d65b82efdc8edeee0644fd8e392b1b93f6

Fix settings saving.
Regression was introduced in 2fbd7be930.

Fixes #8168.

Telegram/SourceFiles/core/core_settings.cpp
@@ -94,7 +94,17 @@ QByteArray Settings::serialize() const {
 		}
 		stream
 			<< qint32(_autoDownloadDictionaries.current() ? 1 : 0)
-			<< qint32(_mainMenuAccountsShown.current() ? 1 : 0);
+			<< qint32(_mainMenuAccountsShown.current() ? 1 : 0)
+			<< qint32(_tabbedSelectorSectionEnabled ? 1 : 0)
+			<< qint32(_floatPlayerColumn)
+			<< qint32(_floatPlayerCorner)
+			<< qint32(_thirdSectionInfoEnabled ? 1 : 0)
+			<< qint32(snap(
+				qRound(_dialogsWidthRatio.current() * 1000000),
+				0,
+				1000000))
+			<< qint32(_thirdColumnWidth.current())
+			<< qint32(_thirdSectionExtendedBy);
 	}
 	return result;
 }
@@ -150,6 +160,13 @@ void Settings::addFromSerialized(const QByteArray &serialized) {
 	std::vector<int> dictionariesEnabled;
 	qint32 autoDownloadDictionaries = _autoDownloadDictionaries.current() ? 1 : 0;
 	qint32 mainMenuAccountsShown = _mainMenuAccountsShown.current() ? 1 : 0;
+	qint32 tabbedSelectorSectionEnabled = 1;
+	qint32 floatPlayerColumn = static_cast<qint32>(Window::Column::Second);
+	qint32 floatPlayerCorner = static_cast<qint32>(RectPart::TopRight);
+	qint32 thirdSectionInfoEnabled = 0;
+	float64 dialogsWidthRatio = _dialogsWidthRatio.current();
+	qint32 thirdColumnWidth = _thirdColumnWidth.current();
+	qint32 thirdSectionExtendedBy = _thirdSectionExtendedBy;
 
 	stream >> themesAccentColors;
 	if (!stream.atEnd()) {
@@ -210,6 +227,18 @@ void Settings::addFromSerialized(const QByteArray &serialized) {
 		stream
 			>> autoDownloadDictionaries
 			>> mainMenuAccountsShown;
+	}
+	if (!stream.atEnd()) {
+		auto dialogsWidthRatioInt = qint32();
+		stream
+			>> tabbedSelectorSectionEnabled
+			>> floatPlayerColumn
+			>> floatPlayerCorner
+			>> thirdSectionInfoEnabled
+			>> dialogsWidthRatioInt
+			>> thirdColumnWidth
+			>> thirdSectionExtendedBy;
+		dialogsWidthRatio = snap(dialogsWidthRatioInt / 1000000., 0., 1.);
 	}
 	if (stream.status() != QDataStream::Ok) {
 		LOG(("App Error: "
@@ -281,6 +310,27 @@ void Settings::addFromSerialized(const QByteArray &serialized) {
 	_dictionariesEnabled = std::move(dictionariesEnabled);
 	_autoDownloadDictionaries = (autoDownloadDictionaries == 1);
 	_mainMenuAccountsShown = (mainMenuAccountsShown == 1);
+	_tabbedSelectorSectionEnabled = (tabbedSelectorSectionEnabled == 1);
+	auto uncheckedColumn = static_cast<Window::Column>(floatPlayerColumn);
+	switch (uncheckedColumn) {
+	case Window::Column::First:
+	case Window::Column::Second:
+	case Window::Column::Third: _floatPlayerColumn = uncheckedColumn; break;
+	}
+	auto uncheckedCorner = static_cast<RectPart>(floatPlayerCorner);
+	switch (uncheckedCorner) {
+	case RectPart::TopLeft:
+	case RectPart::TopRight:
+	case RectPart::BottomLeft:
+	case RectPart::BottomRight: _floatPlayerCorner = uncheckedCorner; break;
+	}
+	_thirdSectionInfoEnabled = thirdSectionInfoEnabled;
+	_dialogsWidthRatio = dialogsWidthRatio;
+	_thirdColumnWidth = thirdColumnWidth;
+	_thirdSectionExtendedBy = thirdSectionExtendedBy;
+	if (_thirdSectionInfoEnabled) {
+		_tabbedSelectorSectionEnabled = false;
+	}
 }
 
 bool Settings::chatWide() const {

Telegram/SourceFiles/media/player/media_player_volume_controller.cpp
@@ -39,7 +39,7 @@ VolumeController::VolumeController(
 			Core::App().settings().setRememberedSongVolume(volume);
 		}
 		applyVolumeChange(volume);
-		controller->session().saveSettingsDelayed();
+		Core::App().saveSettingsDelayed();
 	});
 	Core::App().settings().songVolumeChanges(
 	) | rpl::start_with_next([=](float64 volume) {

Telegram/SourceFiles/media/player/media_player_widget.cpp
@@ -132,6 +132,7 @@ Widget::Widget(QWidget *parent, not_null<Main::Session*> session)
 			? 0.
 			: Core::App().settings().rememberedSongVolume();
 		Core::App().settings().setSongVolume(volume);
+		Core::App().saveSettingsDelayed();
 		mixer()->setSongVolume(volume);
 	});
 	Core::App().settings().songVolumeChanges(

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -2653,10 +2653,8 @@ void OverlayWidget::playbackControlsSeekFinished(crl::time position) {
 
 void OverlayWidget::playbackControlsVolumeChanged(float64 volume) {
 	Core::App().settings().setVideoVolume(volume);
+	Core::App().saveSettingsDelayed();
 	updateMixerVideoVolume();
-	if (_document) {
-		_document->session().saveSettingsDelayed();
-	}
 }
 
 float64 OverlayWidget::playbackControlsCurrentVolume() {

Telegram/SourceFiles/settings/settings_calls.cpp
@@ -52,7 +52,7 @@ Calls::Calls(
 
 Calls::~Calls() {
 	if (_needWriteSettings) {
-		_controller->session().saveSettingsDelayed();
+		Core::App().saveSettingsDelayed();
 	}
 }
 
@@ -134,7 +134,7 @@ void Calls::setupContent() {
 				: "default";
 			Core::App().settings().setCallOutputDeviceID(
 				QString::fromStdString(deviceId));
-			_controller->session().saveSettingsDelayed();
+			Core::App().saveSettingsDelayed();
 			if (const auto call = Core::App().calls().currentCall()) {
 				call->setCurrentAudioDevice(false, deviceId);
 			}
@@ -210,7 +210,7 @@ void Calls::setupContent() {
 				: "default";
 			Core::App().settings().setCallInputDeviceID(
 				QString::fromStdString(deviceId));
-			_controller->session().saveSettingsDelayed();
+			Core::App().saveSettingsDelayed();
 			if (_micTester) {
 				stopTestingMicrophone();
 			}