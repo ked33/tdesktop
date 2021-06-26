ked33/tdesktop

commit c9a0f67712b1e086b230356eee4cfc168db64d3d

Fix volume slider in voice chats.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -260,7 +260,14 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 		: seekRect.height();
 	const auto from = 0;
 	const auto length = (horizontal ? width() : height());
-	const auto mid = qRound(from + value * length);
+	const auto alwaysSeekSize = horizontal
+		? _st.seekSize.width()
+		: _st.seekSize.height();
+	const auto mid = _alwaysDisplayMarker
+		? qRound(from
+			+ (alwaysSeekSize / 2.)
+			+ value * (length - alwaysSeekSize))
+		: qRound(from + value * length);
 	const auto till = std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled

commit b5eb858888a85524d4798f5f37faf1cfcc358d07

Fix crash in vertical sliders.
Regression was introduced in c9a0f67712.

Telegram/SourceFiles/ui/widgets/continuous_sliders.cpp
@@ -268,7 +268,9 @@ void MediaSlider::paintEvent(QPaintEvent *e) {
 			+ (alwaysSeekSize / 2.)
 			+ value * (length - alwaysSeekSize))
 		: qRound(from + value * length);
-	const auto till = std::max(mid, qRound(from + receivedTill * length));
+	const auto till = horizontal
+		? mid
+		: std::max(mid, qRound(from + receivedTill * length));
 	const auto end = from + length;
 	const auto activeFg = disabled
 		? _st.activeFgDisabled