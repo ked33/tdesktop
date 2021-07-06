ked33/tdesktop

commit 46442f0697118d5880f54106825ed0e9d9489530

Make caption area height bigger.

Telegram/SourceFiles/boxes/boxes.style
@@ -446,7 +446,7 @@ autoDownloadLimitPadding: margins(22px, 8px, 22px, 8px);
 
 confirmCaptionArea: InputField(defaultInputField) {
 	textMargins: margins(1px, 26px, 31px, 4px);
-	heightMax: 78px;
+	heightMax: 158px;
 }
 confirmBg: windowBgOver;
 confirmMaxHeight: 245px;

commit 9f7c072a76956a30e043fbd2a2ef56069aa8e470

Fixed caption area height in EditCaptionBox.
Regression was introduced in 46442f0697.

Telegram/SourceFiles/boxes/boxes.style
@@ -382,6 +382,7 @@ confirmCaptionArea: InputField(defaultInputField) {
 	textMargins: margins(1px, 26px, 31px, 4px);
 	heightMax: 158px;
 }
+confirmEditCaptionAreaHeightMax: 78px;
 confirmBg: windowBgOver;
 confirmMaxHeight: 245px;
 confirmMaxHeightSkip: 25px;

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -660,6 +660,7 @@ void EditCaptionBox::updateEditPreview() {
 		}
 	}
 	updateEditMediaButton();
+	updateCaptionMaxHeight();
 	captionResized();
 }
 
@@ -783,6 +784,7 @@ void EditCaptionBox::prepare() {
 	auto cursor = _field->textCursor();
 	cursor.movePosition(QTextCursor::End);
 	_field->setTextCursor(cursor);
+	updateCaptionMaxHeight();
 
 	setupDragArea();
 }
@@ -824,6 +826,28 @@ void EditCaptionBox::captionResized() {
 	resizeEvent(0);
 	updateEmojiPanelGeometry();
 	update();
+}
+
+void EditCaptionBox::updateCaptionMaxHeight() {
+	// Save.
+	const auto wasCursor = _field->textCursor();
+	const auto position = wasCursor.position();
+	const auto anchor = wasCursor.anchor();
+	const auto text = _field->getTextWithAppliedMarkdown();
+	_field->setTextWithTags({});
+
+	_field->setMaxHeight(_doc
+		? st::confirmCaptionArea.heightMax
+		: st::confirmEditCaptionAreaHeightMax);
+
+	// Restore.
+	_field->setTextWithTags(text);
+	auto cursor = _field->textCursor();
+	cursor.setPosition(anchor);
+	if (position != anchor) {
+		cursor.setPosition(position, QTextCursor::KeepAnchor);
+	}
+	_field->setTextCursor(cursor);
 }
 
 void EditCaptionBox::setupEmojiPanel() {

Telegram/SourceFiles/boxes/edit_caption_box.h
@@ -91,6 +91,7 @@ private:
 	bool fileFromClipboard(not_null<const QMimeData*> data);
 	void updateEditPreview();
 	void updateEditMediaButton();
+	void updateCaptionMaxHeight();
 
 	int errorTopSkip() const;
 