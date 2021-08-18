ked33/tdesktop

commit 2d267f69810909ee22b1233ec67843cd5eb7f915

Fix -Wunused-variable warnings

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,16 +164,13 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
-		auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";
 		}
 		++data;
 		skipWhitespacesAndComments(data, end);
-		auto valueStart = data;
 		auto value = readValue(data, end);
-		auto valueEnd = data;
 		if (value.size() == 0) {
 			return "error";
 		}

commit 557ea3bf0b9f823ab7447d04609a37fa138a8072

Fix creating a theme from a default one.
Regression was introduced in 2d267f6981.

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,6 +164,7 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
+		[[maybe_unused]] auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";