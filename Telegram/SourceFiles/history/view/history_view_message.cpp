ked33/tdesktop

commit 76bd6315b573c1f00fd51f7b7def985772479698

Destroy comments button when switched off.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2785,6 +2785,8 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
+		} else {
+			_comments = nullptr;
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {

commit 3eb590ff8122e387776e62580aa0e1cd9d4d4103

Fix crash after comments button destruction.
Regression was introduced in 76bd6315b5.

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -2790,8 +2790,9 @@ int Message::resizeContentGetHeight(int newWidth) {
 
 		if (item->repliesAreComments() || item->externalReply()) {
 			newHeight += st::historyCommentsButtonHeight;
-		} else {
+		} else if (_comments) {
 			_comments = nullptr;
+			checkHeavyPart();
 		}
 		newHeight += viewButtonHeight();
 	} else if (mediaDisplayed) {