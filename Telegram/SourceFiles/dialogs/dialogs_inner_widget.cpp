ked33/tdesktop

commit 5c67e03e00ab2e58869ce84c23e8d5f6c9c9c3ac

Track dialog row offline status by timer.
Fixes #6410.

Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -193,7 +193,7 @@ InnerWidget::InnerWidget(
 
 	session().data().sendActionManager().speakingAnimationUpdated(
 	) | rpl::start_with_next([=](not_null<History*> history) {
-		updateDialogRowCornerStatus(history);
+		repaintDialogRowCornerStatus(history);
 	}, lifetime());
 
 	setupOnlineStatusCheck();
@@ -3181,15 +3181,15 @@ void InnerWidget::setupOnlineStatusCheck() {
 		Data::PeerUpdate::Flag::OnlineStatus
 		| Data::PeerUpdate::Flag::GroupCall
 	) | rpl::start_with_next([=](const Data::PeerUpdate &update) {
-		if (update.peer->isUser()) {
-			userOnlineUpdated(update.peer);
+		if (const auto user = update.peer->asUser()) {
+			userOnlineUpdated(user);
 		} else {
 			groupHasCallUpdated(update.peer);
 		}
 	}, lifetime());
 }
 
-void InnerWidget::updateDialogRowCornerStatus(not_null<History*> history) {
+void InnerWidget::repaintDialogRowCornerStatus(not_null<History*> history) {
 	const auto user = history->peer->isUser();
 	const auto size = user
 		? st::dialogsOnlineBadgeSize
@@ -3217,16 +3217,15 @@ void InnerWidget::updateDialogRowCornerStatus(not_null<History*> history) {
 		UpdateRowSection::Default | UpdateRowSection::Filtered);
 }
 
-void InnerWidget::userOnlineUpdated(not_null<PeerData*> peer) {
-	const auto user = peer->isSelf() ? nullptr : peer->asUser();
-	if (!user) {
+void InnerWidget::userOnlineUpdated(not_null<UserData*> user) {
+	if (!user->isSelf()) {
 		return;
 	}
 	const auto history = session().data().historyLoaded(user);
 	if (!history) {
 		return;
 	}
-	updateRowCornerStatusShown(history, Data::IsUserOnline(user));
+	updateRowCornerStatusShown(history);
 }
 
 void InnerWidget::groupHasCallUpdated(not_null<PeerData*> peer) {
@@ -3238,14 +3237,12 @@ void InnerWidget::groupHasCallUpdated(not_null<PeerData*> peer) {
 	if (!history) {
 		return;
 	}
-	updateRowCornerStatusShown(history, Data::ChannelHasActiveCall(group));
+	updateRowCornerStatusShown(history);
 }
 
-void InnerWidget::updateRowCornerStatusShown(
-		not_null<History*> history,
-		bool shown) {
+void InnerWidget::updateRowCornerStatusShown(not_null<History*> history) {
 	const auto repaint = [=] {
-		updateDialogRowCornerStatus(history);
+		repaintDialogRowCornerStatus(history);
 	};
 	repaint();
 

Telegram/SourceFiles/api/api_updates.cpp
@@ -945,6 +945,7 @@ void Updates::updateOnline(crl::time lastNonIdleTime, bool gotOtherOffline) {
 			Data::PeerUpdate::Flag::OnlineStatus);
 		if (!isOnline) { // Went offline, so we need to save message draft to the cloud.
 			api().saveCurrentDraftToCloud();
+			session().data().maybeStopWatchForOffline(self);
 		}
 
 		_lastSetOnline = ms;
@@ -1856,6 +1857,7 @@ void Updates::feedUpdate(const MTPUpdate &update) {
 			session().changes().peerUpdated(
 				user,
 				Data::PeerUpdate::Flag::OnlineStatus);
+			session().data().maybeStopWatchForOffline(user);
 		}
 		if (UserId(d.vuser_id()) == session().userId()) {
 			if (d.vstatus().type() == mtpc_userStatusOffline

Telegram/SourceFiles/apiwrap.cpp
@@ -1843,6 +1843,7 @@ void ApiWrap::updatePrivacyLastSeens() {
 		session().changes().peerUpdated(
 			user,
 			Data::PeerUpdate::Flag::OnlineStatus);
+		session().data().maybeStopWatchForOffline(user);
 	});
 
 	if (_contactsStatusesRequestId) {
@@ -1856,12 +1857,15 @@ void ApiWrap::updatePrivacyLastSeens() {
 			auto &data = item.c_contactStatus();
 			if (auto user = _session->data().userLoaded(data.vuser_id())) {
 				auto oldOnlineTill = user->onlineTill;
-				auto newOnlineTill = OnlineTillFromStatus(data.vstatus(), oldOnlineTill);
+				auto newOnlineTill = OnlineTillFromStatus(
+					data.vstatus(),
+					oldOnlineTill);
 				if (oldOnlineTill != newOnlineTill) {
 					user->onlineTill = newOnlineTill;
 					session().changes().peerUpdated(
 						user,
 						Data::PeerUpdate::Flag::OnlineStatus);
+					session().data().maybeStopWatchForOffline(user);
 				}
 			}
 		}

Telegram/SourceFiles/data/data_peer_values.cpp
@@ -460,8 +460,11 @@ bool OnlineTextActive(not_null<UserData*> user, TimeId now) {
 	return OnlineTextActive(user->onlineTill, now);
 }
 
-bool IsUserOnline(not_null<UserData*> user) {
-	return OnlineTextActive(user, base::unixtime::now());
+bool IsUserOnline(not_null<UserData*> user, TimeId now) {
+	if (!now) {
+		now = base::unixtime::now();
+	}
+	return OnlineTextActive(user, now);
 }
 
 bool ChannelHasActiveCall(not_null<ChannelData*> channel) {

Telegram/SourceFiles/data/data_peer_values.h
@@ -122,7 +122,7 @@ inline auto PeerFullFlagValue(
 [[nodiscard]] QString OnlineTextFull(not_null<UserData*> user, TimeId now);
 [[nodiscard]] bool OnlineTextActive(TimeId online, TimeId now);
 [[nodiscard]] bool OnlineTextActive(not_null<UserData*> user, TimeId now);
-[[nodiscard]] bool IsUserOnline(not_null<UserData*> user);
+[[nodiscard]] bool IsUserOnline(not_null<UserData*> user, TimeId now = 0);
 [[nodiscard]] bool ChannelHasActiveCall(not_null<ChannelData*> channel);
 
 [[nodiscard]] rpl::producer<QImage> PeerUserpicImageValue(

Telegram/SourceFiles/data/data_session.cpp
@@ -246,6 +246,7 @@ Session::Session(not_null<Main::Session*> session)
 , _ttlCheckTimer([=] { checkTTLs(); })
 , _selfDestructTimer([=] { checkSelfDestructItems(); })
 , _pollsClosingTimer([=] { checkPollsClosings(); })
+, _watchForOfflineTimer([=] { checkLocalUsersWentOffline(); })
 , _groups(this)
 , _chatsFilters(std::make_unique<ChatFilters>(this))
 , _scheduledMessages(std::make_unique<ScheduledMessages>(this))
@@ -644,6 +645,7 @@ not_null<UserData*> Session::processUser(const MTPUser &data) {
 		if (oldOnlineTill != newOnlineTill) {
 			result->onlineTill = newOnlineTill;
 			flags |= UpdateFlag::OnlineStatus;
+			session().data().maybeStopWatchForOffline(result);
 		}
 	}
 
@@ -966,6 +968,64 @@ void Session::unregisterGroupCall(not_null<GroupCall*> call) {
 GroupCall *Session::groupCall(CallId callId) const {
 	const auto i = _groupCalls.find(callId);
 	return (i != end(_groupCalls)) ? i->second.get() : nullptr;
+}
+
+void Session::watchForOffline(not_null<UserData*> user, TimeId now) {
+	if (!now) {
+		now = base::unixtime::now();
+	}
+	if (!Data::IsUserOnline(user, now)) {
+		return;
+	}
+	const auto till = user->onlineTill;
+	const auto [i, ok] = _watchingForOffline.emplace(user, till);
+	if (!ok) {
+		if (i->second == till) {
+			return;
+		}
+		i->second = till;
+	}
+	const auto timeout = Data::OnlineChangeTimeout(till, now);
+	const auto fires = _watchForOfflineTimer.isActive()
+		? _watchForOfflineTimer.remainingTime()
+		: -1;
+	if (fires >= 0 && fires <= timeout) {
+		return;
+	}
+	_watchForOfflineTimer.callOnce(std::max(timeout, crl::time(1)));
+}
+
+void Session::maybeStopWatchForOffline(not_null<UserData*> user) {
+	if (Data::IsUserOnline(user)) {
+		return;
+	} else if (_watchingForOffline.remove(user)
+		&& _watchingForOffline.empty()) {
+		_watchForOfflineTimer.cancel();
+	}
+}
+
+void Session::checkLocalUsersWentOffline() {
+	_watchForOfflineTimer.cancel();
+
+	auto minimal = 86400 * crl::time(1000);
+	const auto now = base::unixtime::now();
+	for (auto i = begin(_watchingForOffline)
+		; i != end(_watchingForOffline);) {
+		const auto user = i->first;
+		if (!Data::IsUserOnline(user, now)) {
+			i = _watchingForOffline.erase(i);
+			session().changes().peerUpdated(
+				user,
+				PeerUpdate::Flag::OnlineStatus);
+		} else {
+			const auto timeout = Data::OnlineChangeTimeout(user, now);
+			accumulate_min(minimal, timeout);
+			++i;
+		}
+	}
+	if (!_watchingForOffline.empty()) {
+		_watchForOfflineTimer.callOnce(std::max(minimal, crl::time(1)));
+	}
 }
 
 auto Session::invitedToCallUsers(CallId callId) const

Telegram/SourceFiles/data/data_session.h
@@ -192,6 +192,9 @@ public:
 	void unregisterGroupCall(not_null<GroupCall*> call);
 	GroupCall *groupCall(CallId callId) const;
 
+	void watchForOffline(not_null<UserData*> user, TimeId now = 0);
+	void maybeStopWatchForOffline(not_null<UserData*> user);
+
 	[[nodiscard]] auto invitedToCallUsers(CallId callId) const
 		-> const base::flat_set<not_null<UserData*>> &;
 	void registerInvitedToCallUser(
@@ -717,6 +720,7 @@ private:
 	void setupUserIsContactViewer();
 
 	void checkSelfDestructItems();
+	void checkLocalUsersWentOffline();
 
 	void scheduleNextTTLs();
 	void checkTTLs();
@@ -975,6 +979,9 @@ private:
 
 	std::vector<WallPaper> _wallpapers;
 	uint64 _wallpapersHash = 0;
+
+	base::flat_map<not_null<UserData*>, TimeId> _watchingForOffline;
+	base::Timer _watchForOfflineTimer;
 
 	rpl::event_stream<WebViewResultSent> _webViewResultSent;
 

Telegram/SourceFiles/dialogs/dialogs_inner_widget.h
@@ -248,13 +248,11 @@ private:
 
 	int defaultRowTop(not_null<Row*> row) const;
 	void setupOnlineStatusCheck();
-	void userOnlineUpdated(not_null<PeerData*> peer);
+	void userOnlineUpdated(not_null<UserData*> user);
 	void groupHasCallUpdated(not_null<PeerData*> peer);
 
-	void updateRowCornerStatusShown(
-		not_null<History*> history,
-		bool shown);
-	void updateDialogRowCornerStatus(not_null<History*> history);
+	void updateRowCornerStatusShown(not_null<History*> history);
+	void repaintDialogRowCornerStatus(not_null<History*> history);
 
 	void setupShortcuts();
 	RowDescriptor computeJump(

commit 679c8a2b8c3e5df38605d97d846dfa180f5f9f37

Fix dialog row updating on user online status change.
Regression was introduced in 5c67e03e00.

Fixes #6410. Again. I hope.

Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -3320,7 +3320,7 @@ void InnerWidget::repaintDialogRowCornerStatus(not_null<History*> history) {
 }
 
 void InnerWidget::userOnlineUpdated(not_null<UserData*> user) {
-	if (!user->isSelf()) {
+	if (user->isSelf()) {
 		return;
 	}
 	const auto history = session().data().historyLoaded(user);