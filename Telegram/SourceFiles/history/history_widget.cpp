ked33/tdesktop

commit 8c0dae194d69eaa69aaad82717ea55d51b067d07

Fixed injecting of sponsored messages at first load of history messages.

Telegram/SourceFiles/history/history_widget.cpp
@@ -2065,6 +2065,7 @@ void HistoryWidget::showHistory(
 			}
 			return;
 		} else {
+			_sponsoredMessagesStateKnown = false;
 			session().data().sponsoredMessages().clearItems(_history);
 			session().data().hideShownSpoilers();
 			_composeSearch = nullptr;
@@ -2299,6 +2300,7 @@ void HistoryWidget::showHistory(
 				auto &sponsored = session().data().sponsoredMessages();
 				using State = Data::SponsoredMessages::State;
 				const auto state = sponsored.state(_history);
+				_sponsoredMessagesStateKnown = (state != State::None);
 				if (state == State::AppendToEnd) {
 					_scroll->setTrackingContent(
 						sponsored.canHaveFor(_history));
@@ -3074,7 +3076,7 @@ void HistoryWidget::messagesReceived(
 		int requestId) {
 	Expects(_history != nullptr);
 
-	bool toMigrated = (peer == _peer->migrateFrom());
+	const auto toMigrated = (peer == _peer->migrateFrom());
 	if (peer != _peer && !toMigrated) {
 		if (_preloadRequest == requestId) {
 			_preloadRequest = 0;
@@ -3148,6 +3150,7 @@ void HistoryWidget::messagesReceived(
 		}
 
 		historyLoaded();
+		injectSponsoredMessages();
 	} else if (_delayedShowAtRequest == requestId) {
 		if (toMigrated) {
 			_history->clear(History::ClearType::Unload);
@@ -3352,7 +3355,7 @@ void HistoryWidget::loadMessagesDown() {
 
 	auto loadMigrated = _migrated && !(_migrated->isEmpty() || _migrated->loadedAtBottom() || (!_history->isEmpty() && !_history->loadedAtTop()));
 	auto from = loadMigrated ? _migrated : _history;
-	if (from->loadedAtBottom()) {
+	if (from->loadedAtBottom() && _sponsoredMessagesStateKnown) {
 		session().data().sponsoredMessages().request(_history, nullptr);
 		return;
 	}

Telegram/SourceFiles/history/history_widget.h
@@ -793,6 +793,8 @@ private:
 		ItemRevealAnimation> _itemRevealAnimations;
 	int _itemsRevealHeight = 0;
 
+	bool _sponsoredMessagesStateKnown = false;
+
 	object_ptr<Ui::PlainShadow> _topShadow;
 	bool _inGrab = false;
 

commit 3c50c4e6a86cfd915ad1ad527815143a3bdd5353

Beta version 4.6.9: Fix critical loading bug.
Regression was introduced in 8c0dae194d.

The HistoryWidget was infinitely trying to load history down even
when there was nothing to load and kept flooding with requests.

Telegram/SourceFiles/history/history_widget.cpp
@@ -3358,8 +3358,10 @@ void HistoryWidget::loadMessagesDown() {
 
 	auto loadMigrated = _migrated && !(_migrated->isEmpty() || _migrated->loadedAtBottom() || (!_history->isEmpty() && !_history->loadedAtTop()));
 	auto from = loadMigrated ? _migrated : _history;
-	if (from->loadedAtBottom() && _sponsoredMessagesStateKnown) {
-		session().data().sponsoredMessages().request(_history, nullptr);
+	if (from->loadedAtBottom()) {
+		if (_sponsoredMessagesStateKnown) {
+			session().data().sponsoredMessages().request(_history, nullptr);
+		}
 		return;
 	}
 