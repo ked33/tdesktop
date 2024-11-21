ked33/tdesktop

commit f3ce9fa281bfa1b40221ad32f75fe846aa00706b

Fit transactions id from credits history entries to table cell.

Telegram/SourceFiles/boxes/gift_premium_box.cpp
@@ -1242,12 +1242,24 @@ void AddCreditsHistoryEntryTable(
 		}
 	}
 	if (!entry.id.isEmpty()) {
-		constexpr auto kOneLineCount = 18;
-		const auto oneLine = entry.id.length() <= kOneLineCount;
+		constexpr auto kOneLineCount = 22;
+		const auto oneLine = entry.id.size() <= kOneLineCount;
+		auto multiLine = QString();
+		if (!oneLine) {
+			for (auto i = 0; i < entry.id.size(); ++i) {
+				multiLine.append(entry.id[i]);
+				if ((i + 1) % kOneLineCount == 0) {
+					multiLine.append('\n');
+				}
+			}
+		}
 		auto label = object_ptr<Ui::FlatLabel>(
 			table,
 			rpl::single(
-				Ui::Text::Wrapped({ entry.id }, EntityType::Code, {})),
+				Ui::Text::Wrapped(
+					{ oneLine ? entry.id : std::move(multiLine) },
+					EntityType::Code,
+					{})),
 			oneLine
 				? st::giveawayGiftCodeValue
 				: st::giveawayGiftCodeValueMultiline);

Telegram/SourceFiles/info/channel_statistics/boosts/giveaway/giveaway.style
@@ -96,6 +96,10 @@ giveawayGiftCodeValue: FlatLabel(defaultFlatLabel) {
 giveawayGiftCodeValueMultiline: FlatLabel(giveawayGiftCodeValue) {
 	minWidth: 128px;
 	maxHeight: 100px;
+	style: TextStyle(defaultTextStyle) {
+		font: font(10px);
+		linkUnderline: kLinkUnderlineNever;
+	}
 }
 giveawayGiftMessage: FlatLabel(giveawayGiftCodeValue) {
 	minWidth: 128px;

commit 778ffe737ced17bd2494b05f4629c78b2c22a768

Fixed ability to copy transactions id from credits history entries.
Regression was introduced in f3ce9fa281.

Telegram/SourceFiles/boxes/gift_premium_box.cpp
@@ -1255,24 +1255,12 @@ void AddCreditsHistoryEntryTable(
 				Ui::Text::WithEntities));
 	}
 	if (!entry.id.isEmpty()) {
-		constexpr auto kOneLineCount = 22;
-		const auto oneLine = entry.id.size() <= kOneLineCount;
-		auto multiLine = QString();
-		if (!oneLine) {
-			for (auto i = 0; i < entry.id.size(); ++i) {
-				multiLine.append(entry.id[i]);
-				if ((i + 1) % kOneLineCount == 0) {
-					multiLine.append('\n');
-				}
-			}
-		}
+		constexpr auto kOneLineCount = 24;
+		const auto oneLine = entry.id.length() <= kOneLineCount;
 		auto label = object_ptr<Ui::FlatLabel>(
 			table,
 			rpl::single(
-				Ui::Text::Wrapped(
-					{ oneLine ? entry.id : std::move(multiLine) },
-					EntityType::Code,
-					{})),
+				Ui::Text::Wrapped({ entry.id }, EntityType::Code, {})),
 			oneLine
 				? st::giveawayGiftCodeValue
 				: st::giveawayGiftCodeValueMultiline);