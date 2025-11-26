ked33/tdesktop

commit 013d2e7ecd827567f6f65e81a2b21aa658fddff2

Split verified check icon.

Telegram/SourceFiles/inline_bots/bot_attach_web_view.cpp
@@ -82,7 +82,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_boxes.h"
 #include "styles/style_channel_earn.h"
 #include "styles/style_chat.h"
-#include "styles/style_info.h" // infoVerifiedCheck.
+#include "styles/style_info.h" // infoVerifiedStar.
 #include "styles/style_layers.h"
 #include "styles/style_menu_icons.h"
 #include "styles/style_window.h"
@@ -412,7 +412,7 @@ void FillBotUsepic(
 		title,
 		rpl::single(bot->name()),
 		box->getDelegate()->style().title);
-	const auto icon = bot->isVerified() ? &st::infoVerifiedCheck : nullptr;
+	const auto icon = bot->isVerified() ? &st::infoVerifiedStar : nullptr;
 	title->resize(
 		titleLabel->width() + (icon ? icon->width() : 0),
 		titleLabel->height());
@@ -1346,9 +1346,9 @@ void WebViewInstance::show(ShowArgs &&args) {
 		const auto raw = titleBadge.data();
 		raw->paintRequest() | rpl::start_with_next([=] {
 			auto p = Painter(raw);
-			st::infoVerifiedCheck.paint(p, st::lineWidth, 0, raw->width());
+			st::infoVerifiedStar.paint(p, st::lineWidth, 0, raw->width());
 		}, raw->lifetime());
-		raw->resize(st::infoVerifiedCheck.size() + QSize(0, st::lineWidth));
+		raw->resize(st::infoVerifiedStar.size() + QSize(0, st::lineWidth));
 	}
 
 	const auto &bots = _session->attachWebView().attachBots();

Telegram/SourceFiles/info/info.style
@@ -23,6 +23,7 @@ InfoToggle {
 
 InfoPeerBadge {
 	verified: icon;
+	verifiedCheck: icon;
 	premium: icon;
 	premiumFg: color;
 	position: point;
@@ -554,14 +555,12 @@ infoRatingDeductedBadge: RoundButton(customEmojiTextBadge) {
 infoProfileInaccessibleUserpic: icon {{ "info/inaccessible_userpic", historyPeerUserpicFg }};
 
 infoVerifiedCheckPosition: point(4px, 2px);
-infoVerifiedCheck: icon {
-	{ "profile_verified_star", profileVerifiedCheckBg },
-	{ "profile_verified_check", profileVerifiedCheckFg }
-};
+infoVerifiedStar: icon {{ "profile_verified_star", profileVerifiedCheckBg }};
 infoPremiumStar: icon {{ "profile_premium", profileVerifiedCheckBg }};
 
 infoPeerBadge: InfoPeerBadge {
-	verified: infoVerifiedCheck;
+	verified: infoVerifiedStar;
+	verifiedCheck: icon {{ "profile_verified_check", profileVerifiedCheckFg }};
 	premium: infoPremiumStar;
 	premiumFg: profileVerifiedCheckBg;
 	position: infoVerifiedCheckPosition;
@@ -571,10 +570,8 @@ infoBotVerifyBadge: InfoPeerBadge(infoPeerBadge) {
 	position: point(-2px, 2px);
 }
 infoColoredPeerBadge: InfoPeerBadge(infoPeerBadge) {
-	verified: icon {
-		{ "profile_verified_star", groupCallMembersFg },
-		{ "profile_verified_check", groupCallBg }
-	};
+	verified: icon {{ "profile_verified_star", groupCallMembersFg }}; // Will be colorized.
+	verifiedCheck: icon {{ "profile_verified_check", groupCallMembersFg }};
 	premium: icon {{ "profile_premium", groupCallMembersFg }};
 	premiumFg: groupCallMembersFg;
 }

commit 0c3b931e315e2b5fa0688657864c8f4fa8e073e3

Fixed display of verified icon in box from web bot preview.
Regression was introduced in 013d2e7ecd.

Telegram/SourceFiles/inline_bots/bot_attach_web_view.cpp
@@ -413,6 +413,9 @@ void FillBotUsepic(
 		rpl::single(bot->name()),
 		box->getDelegate()->style().title);
 	const auto icon = bot->isVerified() ? &st::infoVerifiedStar : nullptr;
+	const auto iconCheck = icon
+		? &st::infoPeerBadge.verifiedCheck
+		: nullptr;
 	title->resize(
 		titleLabel->width() + (icon ? icon->width() : 0),
 		titleLabel->height());
@@ -422,17 +425,16 @@ void FillBotUsepic(
 			- (icon ? icon->width() + st::lineWidth : 0));
 	}, title->lifetime());
 	if (icon) {
-		title->paintRequest(
-		) | rpl::start_with_next([=] {
+		title->paintRequest() | rpl::start_with_next([=] {
 			auto p = Painter(title);
 			p.fillRect(title->rect(), Qt::transparent);
-			icon->paint(
-				p,
-				std::min(
-					titleLabel->textMaxWidth() + st::lineWidth,
-					title->width() - st::lineWidth - icon->width()),
-				(title->height() - icon->height()) / 2,
-				title->width());
+			const auto x = std::min(
+				titleLabel->textMaxWidth() + st::lineWidth,
+				title->width() - st::lineWidth - icon->width());
+			const auto y = (title->height() - icon->height()) / 2;
+			const auto w = title->width();
+			icon->paint(p, x, y, w);
+			iconCheck->paint(p, x, y, w);
 		}, title->lifetime());
 	}
 