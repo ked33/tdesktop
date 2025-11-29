ked33/tdesktop

commit e79222cd3a563ca36c7e2e10d33f4820c645637b

Fixed unwanted switching to default filter on creation of tabs strip.

Telegram/SourceFiles/ui/widgets/chat_filters_tabs_strip.cpp
@@ -227,6 +227,7 @@ not_null<Ui::RpWidget*> AddChatFiltersTabsStrip(
 				? st::dialogsSearchTabs
 				: st::chatsFiltersTabs));
 	const auto state = wrap->lifetime().make_state<State>();
+	state->lastFilterId = controller->activeChatsFilterCurrent();
 	const auto reassignUnreadValue = [=] {
 		state->reorderLifetime.destroy();
 		const auto &list = session->data().chatsFilters().list();

commit d64f4b3dacade1b5ff4f78fa9adb12f29993e6b8

Fixed once again unwanted reset filter on creation of tabs strip.
Regression was introduced in e79222cd3a.

Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -1677,6 +1677,7 @@ void Widget::toggleFiltersMenu(bool enabled) {
 
 		_chatFilters = base::make_unique_q<NoScrollPropagationWidget>(this);
 		const auto raw = _chatFilters.get();
+		const auto idBeforeTabs = controller()->activeChatsFilterCurrent();
 		const auto inner = Ui::AddChatFiltersTabsStrip(
 			_chatFilters.get(),
 			&session(),
@@ -1689,6 +1690,9 @@ void Widget::toggleFiltersMenu(bool enabled) {
 			Window::GifPauseReason::Any,
 			controller(),
 			true);
+		if (controller()->activeChatsFilterCurrent() != idBeforeTabs) {
+			controller()->setActiveChatsFilter(idBeforeTabs);
+		}
 		raw->show();
 		raw->stackUnder(_scroll);
 		raw->resizeToWidth(width());

Telegram/SourceFiles/ui/widgets/chat_filters_tabs_strip.cpp
@@ -227,7 +227,6 @@ not_null<Ui::RpWidget*> AddChatFiltersTabsStrip(
 				? st::dialogsSearchTabs
 				: st::chatsFiltersTabs));
 	const auto state = wrap->lifetime().make_state<State>();
-	state->lastFilterId = controller->activeChatsFilterCurrent();
 	const auto reassignUnreadValue = [=] {
 		state->reorderLifetime.destroy();
 		const auto &list = session->data().chatsFilters().list();