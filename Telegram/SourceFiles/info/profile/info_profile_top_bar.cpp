c70d84520bc789db358053f758cc82e3ba5e75f7
John Preston <johnprestonmail@gmail.com>
2026-01-29 17:04:31 +0400

    Show correct profile colors preview when edit.

--- Telegram/SourceFiles/info/profile/info_profile_top_bar.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_top_bar.cpp
@@ -1405,11 +1405,7 @@ void TopBar::setPatternEmojiId(std::optional<DocumentId> patternEmojiId) {
 
 void TopBar::setLocalEmojiStatusId(EmojiStatusId emojiStatusId) {
 	_localCollectible = emojiStatusId.collectible;
-	if (!emojiStatusId.collectible) {
-		_badgeContent = Badge::Content{ BadgeType::Premium, emojiStatusId };
-	} else {
-		_badgeContent = BadgeContentForPeer(_peer);
-	}
+	_badgeContent = Badge::Content{ BadgeType::Premium, emojiStatusId };
 	updateCollectibleStatus();
 }
 
