ked33/tdesktop

commit 7ba6651927504c09ad1da901588e68bc7c446389

Fixed initial tray icon color on macOS dark theme.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,7 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = status.button.effectiveAppearance;
+	const auto appearance = [NSApp effectiveAppearance];
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 

commit 15393b9d951e1a9de4f1aa9974855385d01f8fee

Fixed crash on macOS 10.13.
Regression was introduced in 7ba6651927.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,12 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = [NSApp effectiveAppearance];
+	const auto appearance = [&] {
+		if (@available(macOS 10.14, *)) {
+			return [NSApp effectiveAppearance];
+		}
+		return status.button.effectiveAppearance;
+	}();
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 