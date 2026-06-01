ked33/tdesktop

commit 1ad8d49acd9da1f71ede79eafece7889b75b7797

Synchronize system accent color avilability between toggle and setting

Telegram/SourceFiles/window/themes/window_themes_embedded.cpp
@@ -179,6 +179,11 @@ style::colorizer ColorizerFrom(
 }
 
 std::optional<QColor> SystemAccentColor() {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+	if (Platform::IsWindows() && !Platform::IsWindows8OrGreater()) {
+		return std::nullopt;
+	}
+#endif // Qt < 6.0.0
 #if QT_VERSION >= QT_VERSION_CHECK(6, 6, 0)
 	constexpr auto kAccentRole = QPalette::ColorRole::Accent;
 #else

Telegram/SourceFiles/settings/sections/settings_chat.cpp
@@ -99,11 +99,7 @@ const auto kSchemesList = Window::Theme::EmbeddedThemes();
 constexpr auto kCustomColorButtonParts = 7;
 
 [[nodiscard]] bool IsSystemAccentColorSupported() {
-#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-	return true;
-#else
-	return !Platform::IsWindows() || !Platform::IsWindows8OrGreater();
-#endif
+	return Window::Theme::SystemAccentColor().has_value();
 }
 
 class ColorsPalette final {

commit 5d5058314ebdfa0b17cfe081db50a95dafe4a58e

Fix system accent color Windows option condition
It actually works only on Windows 7 with Qt 5

Regression was introduced in 1ad8d49acd9da1f71ede79eafece7889b75b7797

Telegram/SourceFiles/window/themes/window_themes_embedded.cpp
@@ -181,7 +181,7 @@ style::colorizer ColorizerFrom(
 
 std::optional<QColor> SystemAccentColor() {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-	if (Platform::IsWindows() && !Platform::IsWindows8OrGreater()) {
+	if (Platform::IsWindows() && Platform::IsWindows8OrGreater()) {
 		return std::nullopt;
 	}
 #endif // Qt < 6.0.0