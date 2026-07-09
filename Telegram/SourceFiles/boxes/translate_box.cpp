ee361c65b7350ab7e9ab9715b9a96eaaf8b0f457
John Preston <johnprestonmail@gmail.com>
2026-07-10 01:48:05 +0400

    Use interactive markdown widget in Translate Box

--- Telegram/SourceFiles/boxes/translate_box.cpp
+++ Telegram/SourceFiles/boxes/translate_box.cpp
@@ -10,8 +10,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "lang/translate_provider.h"
 
 #include "base/weak_ptr.h"
-#include "chat_helpers/spellchecker_common.h"
 #include "core/application.h"
+#include "core/click_handler_types.h"
 #include "core/core_settings.h"
 #include "core/ui_integration.h"
 #include "data/data_msg_id.h"
@@ -20,9 +20,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history.h"
 #include "history/history_item.h"
 #include "iv/markdown/iv_markdown_article.h"
-#include "iv/markdown/iv_markdown_article_scroll_forwarder.h"
-#include "iv/markdown/iv_markdown_media_block.h"
 #include "iv/markdown/iv_markdown_prepare.h"
+#include "iv/markdown/iv_markdown_view_widget.h"
 #include "iv/iv_cached_media.h"
 #include "iv/iv_rich_page.h"
 #include "lang/lang_instance.h"
@@ -31,8 +30,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "mtproto/sender.h"
 #include "spellcheck/platform/platform_language.h"
 #include "ui/boxes/choose_language_box.h"
-#include "ui/chat/chat_style.h"
-#include "ui/chat/chat_theme.h"
 #include "ui/effects/loading_element.h"
 #include "ui/layers/generic_box.h"
 #include "ui/text/text_utilities.h"
@@ -40,10 +37,11 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/widgets/multi_select.h"
 #include "ui/wrap/fade_wrap.h"
 #include "ui/wrap/slide_wrap.h"
-#include "ui/painter.h"
+#include "ui/basic_click_handlers.h"
+#include "ui/integration.h"
 #include "ui/power_saving.h"
 #include "ui/vertical_list.h"
-#include "window/themes/window_theme.h"
+#include "window/window_session_controller.h"
 
 #include "styles/style_boxes.h"
 #include "styles/style_iv.h"
@@ -54,163 +52,147 @@ namespace {
 
 constexpr auto kSkipAtLeastOneDuration = 3 * crl::time(1000);
 
-class TranslateMediaBlockHost final : public Iv::Markdown::MediaBlockHost {
-public:
-	TranslateMediaBlockHost(
-		Fn<void(QRect)> repaint,
-		Fn<void(QRect)> relayout);
+void ActivateRichTranslateLink(
+		not_null<Iv::Markdown::MarkdownDocumentWidget*> body,
+		const Iv::Markdown::PreparedLink &link,
+		Qt::MouseButton button,
+		const QVariant &context) {
+	if (button != Qt::LeftButton && button != Qt::MiddleButton) {
+		return;
+	}
+	using Kind = Iv::Markdown::PreparedLinkKind;
+	switch (link.kind) {
+	case Kind::External: {
+		if (link.target.isEmpty()) {
+			return;
+		}
+		switch (link.entityType) {
+		case EntityType::Url:
+		case EntityType::CustomUrl:
+		case EntityType::Email:
+			break;
+		default:
+			return;
+		}
+		const auto handler = Ui::Integration::Instance().createLinkHandler(
+			EntityLinkData{
+				.text = (!link.copyText.isEmpty()
+					? link.copyText
+					: link.target),
+				.data = link.target,
+				.type = link.entityType,
+				.shown = link.shown,
+			},
+			Ui::Text::MarkedContext());
+		if (handler) {
+			auto click = ClickContext();
+			click.button = button;
+			click.other = context;
+			handler->onClick(std::move(click));
+		}
+	} break;
+	case Kind::InstantViewPage: {
+		auto target = link.target;
+		if (!link.fragment.isEmpty()) {
+			target += u"#"_q + link.fragment;
+		}
+		UrlClickHandler::Open(target, context);
+	} break;
+	case Kind::ToggleDetails:
+		static_cast<void>(body->toggleDetails(link.target));
+		break;
+	default:
+		break;
+	}
+}
 
-	void requestRepaint(QRect articleRect) override;
-	void requestRelayout(QRect articleRect) override;
+[[nodiscard]] bool ActivateRichTranslateMedia(
+		const Iv::Markdown::MediaActivation &activation,
+		Qt::MouseButton button,
+		const QVariant &context) {
+	if (button != Qt::LeftButton && button != Qt::MiddleButton) {
+		return false;
+	}
+	using Kind = Iv::Markdown::MediaActivationKind;
+	switch (activation.kind) {
+	case Kind::ExternalUrl:
+		if (activation.url.isEmpty()) {
+			return false;
+		}
+		HiddenUrlClickHandler::Open(activation.url, context);
+		return true;
+	case Kind::Photo:
+		if (!activation.photo) {
+			return false;
+		}
+		activation.photo->open(button);
+		return true;
+	case Kind::Document:
+		if (!activation.document) {
+			return false;
+		}
+		activation.document->open(button);
+		return true;
+	case Kind::OpenChannel:
+		if (!activation.channel) {
+			return false;
+		}
+		activation.channel->open(button);
+		return true;
+	case Kind::JoinChannel:
+		if (!activation.channel) {
+			return false;
+		}
+		activation.channel->join(button);
+		return true;
+	default:
+		return false;
+	}
+}
 
-private:
-	const Fn<void(QRect)> _repaint;
-	const Fn<void(QRect)> _relayout;
-
-};
-
-class RichTranslateArticle final : public RpWidget {
-public:
-	RichTranslateArticle(
-		QWidget *parent,
+void SetupRichArticleBody(
+		not_null<GenericBox*> box,
+		not_null<Iv::Markdown::MarkdownDocumentWidget*> body,
 		not_null<Main::Session*> session,
-		FullMsgId itemId);
-	~RichTranslateArticle();
-
-	[[nodiscard]] bool showPage(std::shared_ptr<const Iv::RichPage> page);
-
-protected:
-	void paintEvent(QPaintEvent *e) override;
-	int resizeGetHeight(int newWidth) override;
-	void wheelEvent(QWheelEvent *e) override;
-	void mousePressEvent(QMouseEvent *e) override;
-	void mouseMoveEvent(QMouseEvent *e) override;
-	void mouseReleaseEvent(QMouseEvent *e) override;
-	bool eventHook(QEvent *e) override;
-
-private:
-	void paintArticle(Painter &p, QRect clip);
-	void requestArticleRepaint(QRect articleRect);
-	void requestArticleRelayout();
-	void detachArticleBindings();
-	[[nodiscard]] QRect articleRect() const;
-	[[nodiscard]] Iv::Markdown::MarkdownArticle *scrollTarget();
-
-	const not_null<Main::Session*> _session;
-	const FullMsgId _itemId;
-	std::shared_ptr<Iv::Markdown::MediaRuntime> _mediaRuntime;
-	std::unique_ptr<Iv::Markdown::MediaBlockHost> _host;
-	Iv::Markdown::MarkdownArticle _article;
-	std::unique_ptr<ChatTheme> _theme;
-	std::unique_ptr<ChatStyle> _style;
-	Iv::Markdown::MarkdownArticleScrollForwarder _scrollForwarder;
-	int _articleHeight = 0;
-	int _paletteVersion = -1;
-	bool _hasArticle = false;
-
-};
-
-TranslateMediaBlockHost::TranslateMediaBlockHost(
-	Fn<void(QRect)> repaint,
-	Fn<void(QRect)> relayout)
-: _repaint(std::move(repaint))
-, _relayout(std::move(relayout)) {
-}
-
-void TranslateMediaBlockHost::requestRepaint(QRect articleRect) {
-	crl::on_main([repaint = _repaint, articleRect] {
-		if (repaint) {
-			repaint(articleRect);
-		}
+		not_null<PeerData*> peer,
+		FullMsgId itemId) {
+	auto clickHandlerContext = ClickHandlerContext();
+	clickHandlerContext.itemId = itemId;
+	clickHandlerContext.sessionWindow = base::make_weak(
+		session->tryResolveWindow(peer));
+	clickHandlerContext.show = box->uiShow();
+	const auto context = QVariant::fromValue(clickHandlerContext);
+	body->setClickHandlerContext(context);
+	body->setLinkActivationCallback([=](
+			const Iv::Markdown::PreparedLink &link,
+			Qt::MouseButton button) {
+		ActivateRichTranslateLink(body, link, button, context);
 	});
-}
-
-void TranslateMediaBlockHost::requestRelayout(QRect articleRect) {
-	crl::on_main([relayout = _relayout, articleRect] {
-		if (relayout) {
-			relayout(articleRect);
-		}
+	body->setMediaActivationCallback([=](
+			const Iv::Markdown::MediaActivation &activation,
+			Qt::MouseButton button) {
+		return ActivateRichTranslateMedia(activation, button, context);
 	});
+	style::PaletteChanged() | rpl::on_next([=] {
+		body->refreshPalette();
+	}, body->lifetime());
 }
 
-RichTranslateArticle::RichTranslateArticle(
-	QWidget *parent,
-	not_null<Main::Session*> session,
-	FullMsgId itemId)
-: RpWidget(parent)
-, _session(session)
-, _itemId(itemId)
-, _article(st::messageMarkdown)
-, _theme(Window::Theme::DefaultChatThemeOn(lifetime()))
-, _style(std::make_unique<ChatStyle>(session->colorIndicesValue())) {
-	_style->apply(_theme.get());
-	_paletteVersion = _style->paletteVersion();
-	setAttribute(Qt::WA_AcceptTouchEvents);
-
-	const auto weak = base::make_weak(this);
-	_host = std::make_unique<TranslateMediaBlockHost>(
-		[weak](QRect articleRect) {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRepaint(articleRect);
-			}
-		},
-		[weak](QRect) {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRelayout();
-			}
-		});
-	_article.setMediaBlockHost(_host.get());
-	_article.setTextRepaintCallbacks(
-		[weak] {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRepaint(QRect());
-			}
-		},
-		[weak](QRect articleRect) {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRepaint(articleRect);
-			}
-		});
-
-	Spellchecker::HighlightReady(
-	) | rpl::on_next([=](Spellchecker::HighlightProcessId processId) {
-		if (_article.highlightProcessDone(processId)) {
-			requestArticleRepaint(QRect());
-		}
-	}, lifetime());
-
-	_style->paletteChanged(
-	) | rpl::on_next([=] {
-		update();
-	}, lifetime());
-
-	Window::Theme::Background()->updates(
-	) | rpl::on_next([=](const Window::Theme::BackgroundUpdate &) {
-		update();
-	}, lifetime());
-}
-
-RichTranslateArticle::~RichTranslateArticle() {
-	detachArticleBindings();
-}
-
-void RichTranslateArticle::detachArticleBindings() {
-	_article.setTextRepaintCallbacks(nullptr, nullptr);
-	_article.setMediaBlockHost(nullptr);
-}
-
-bool RichTranslateArticle::showPage(
+[[nodiscard]] bool ShowRichArticlePage(
+		not_null<Main::Session*> session,
+		FullMsgId itemId,
+		not_null<Iv::Markdown::MarkdownDocumentWidget*> body,
+		not_null<std::shared_ptr<Iv::Markdown::MarkdownArticle>*> article,
 		std::shared_ptr<const Iv::RichPage> page) {
-	const auto limits = Iv::ResolveRichMessageLimits(_session);
-	auto mediaRuntime = Iv::CreateMessageMediaRuntime(
-		_session,
-		_itemId,
-		[](QString) {},
-		[](QString) {},
-		::Data::FileOrigin());
+	const auto limits = Iv::ResolveRichMessageLimits(session);
 	auto prepared = Iv::Markdown::TryPrepareNativeInstantView({
 		.richPage = page,
-		.mediaRuntime = mediaRuntime,
+		.mediaRuntime = Iv::CreateMessageMediaRuntime(
+			session,
+			itemId,
+			[](QString) {},
+			[](QString) {},
+			::Data::FileOrigin()),
 		.dimensionsOverride = Iv::Markdown::CaptureMarkdownPrepareDimensions(
 			st::messageMarkdown),
 		.tableRenderLimits
@@ -219,142 +201,16 @@ bool RichTranslateArticle::showPage(
 	if (!prepared.supported()) {
 		return false;
 	}
-	_mediaRuntime = std::move(mediaRuntime);
-	_article.setContent(std::move(prepared.content));
-	_hasArticle = true;
-	if (width() > 0) {
-		resizeToWidth(width());
+	if (!*article) {
+		*article = std::make_shared<Iv::Markdown::MarkdownArticle>(
+			st::messageMarkdown);
+		(*article)->setContent(std::move(prepared.content));
+		body->setArticle(*article);
+	} else {
+		(*article)->setContent(std::move(prepared.content));
+		body->articleContentChanged();
 	}
-	update();
 	return true;
-}
-
-void RichTranslateArticle::requestArticleRepaint(QRect rect) {
-	crl::on_main(this, [=] {
-		if (rect.isEmpty()) {
-			update();
-		} else {
-			update(rect.translated(articleRect().topLeft()));
-		}
-	});
-}
-
-void RichTranslateArticle::requestArticleRelayout() {
-	_article.invalidateLayout();
-	if (width() > 0) {
-		resizeToWidth(width());
-	}
-	update();
-}
-
-QRect RichTranslateArticle::articleRect() const {
-	return QRect(0, 0, width(), _articleHeight);
-}
-
-Iv::Markdown::MarkdownArticle *RichTranslateArticle::scrollTarget() {
-	return _hasArticle ? &_article : nullptr;
-}
-
-void RichTranslateArticle::wheelEvent(QWheelEvent *e) {
-	_scrollForwarder.handleWheel(scrollTarget(), e, articleRect().topLeft());
-}
-
-void RichTranslateArticle::mousePressEvent(QMouseEvent *e) {
-	if (!_scrollForwarder.handleMousePress(
-			scrollTarget(),
-			e,
-			articleRect().topLeft())) {
-		RpWidget::mousePressEvent(e);
-	}
-}
-
-void RichTranslateArticle::mouseMoveEvent(QMouseEvent *e) {
-	if (!_scrollForwarder.handleMouseMove(
-			scrollTarget(),
-			e,
-			articleRect().topLeft())) {
-		RpWidget::mouseMoveEvent(e);
-	}
-}
-
-void RichTranslateArticle::mouseReleaseEvent(QMouseEvent *e) {
-	if (!_scrollForwarder.handleMouseRelease(
-			scrollTarget(),
-			e,
-			articleRect().topLeft())) {
-		RpWidget::mouseReleaseEvent(e);
-	}
-}
-
-bool RichTranslateArticle::eventHook(QEvent *e) {
-	if (Iv::Markdown::MarkdownArticleScrollForwarder::IsTouchEvent(e)
-		&& _scrollForwarder.handleTouchHook(
-			scrollTarget(),
-			this,
-			e,
-			articleRect().topLeft())) {
-		return true;
-	}
-	return RpWidget::eventHook(e);
-}
-
-void RichTranslateArticle::paintArticle(Painter &p, QRect clip) {
-	if (!_hasArticle) {
-		return;
-	}
-	if (_paletteVersion != _style->paletteVersion()) {
-		_paletteVersion = _style->paletteVersion();
-		_article.invalidatePaletteCache();
-	}
-	const auto content = articleRect();
-	if (content.isEmpty()) {
-		return;
-	}
-	const auto articleClip = content.intersected(clip).translated(
-		-content.topLeft());
-	if (articleClip.isEmpty()) {
-		return;
-	}
-	auto context = Iv::Markdown::MarkdownArticlePaintContext(
-		_theme->preparePaintContext(
-			_style.get(),
-			QRect(QPoint(), content.size()),
-			QRect(QPoint(), content.size()),
-			articleClip,
-			false));
-	const auto messageStyle = context.messageStyle();
-	context.caches = {
-		.pre = messageStyle->preCache.get(),
-		.blockquote = context.quoteCache({}, 0),
-		.colors = _style->highlightColors(),
-		.st = &messageStyle->richPageStyle,
-		.repaint = [weak = base::make_weak(this)] {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRepaint(QRect());
-			}
-		},
-		.repaintRect = [weak = base::make_weak(this)](QRect rect) {
-			if (const auto owner = weak.get()) {
-				owner->requestArticleRepaint(rect);
-			}
-		},
-	};
-	_article.setVisibleTopBottom(0, content.height());
-	p.save();
-	p.setClipRect(content.intersected(clip));
-	p.translate(content.topLeft());
-	_article.paint(p, context);
-	p.restore();
-}
-
-void RichTranslateArticle::paintEvent(QPaintEvent *e) {
-	auto p = Painter(this);
-	paintArticle(p, e->rect());
-}
-
-int RichTranslateArticle::resizeGetHeight(int newWidth) {
-	_articleHeight = _hasArticle ? _article.resizeGetHeight(newWidth) : 0;
-	return _articleHeight;
 }
 
 [[nodiscard]] bool TranslateRichBox(
@@ -364,16 +220,6 @@ int RichTranslateArticle::resizeGetHeight(int newWidth) {
 		std::shared_ptr<const Iv::RichPage> page,
 		TextWithEntities summaryText,
 		bool hasCopyRestriction) {
-	const auto session = &peer->session();
-	const auto itemId = FullMsgId(peer->id, msgId);
-	auto originalArticle = object_ptr<RichTranslateArticle>(
-		box,
-		session,
-		itemId);
-	if (!originalArticle->showPage(page)) {
-		return false;
-	}
-
 	struct State {
 		State(not_null<Main::Session*> session)
 		: api(&session->mtp()) {
@@ -382,8 +228,21 @@ int RichTranslateArticle::resizeGetHeight(int newWidth) {
 		MTP::Sender api;
 		rpl::variable<LanguageId> to;
 		mtpRequestId requestId = 0;
+		std::shared_ptr<Iv::Markdown::MarkdownArticle> original;
+		std::shared_ptr<Iv::Markdown::MarkdownArticle> translated;
 	};
+	const auto session = &peer->session();
+	const auto itemId = FullMsgId(peer->id, msgId);
 	const auto state = box->lifetime().make_state<State>(session);
+	auto originalBody = object_ptr<Iv::Markdown::MarkdownDocumentWidget>(box);
+	if (!ShowRichArticlePage(
+			session,
+			itemId,
+			originalBody.data(),
+			&state->original,
+			page)) {
+		return false;
+	}
 	state->to = ChooseTranslateTo(peer->owner().history(peer));
 
 	box->setWidth(st::boxWideWidth);
@@ -437,10 +296,11 @@ int RichTranslateArticle::resizeGetHeight(int newWidth) {
 	}, show->lifetime());
 
 	const auto original = box->addRow(
-		object_ptr<SlideWrap<RichTranslateArticle>>(
+		object_ptr<SlideWrap<Iv::Markdown::MarkdownDocumentWidget>>(
 			box,
-			std::move(originalArticle)));
+			std::move(originalBody)));
 	original->hide(anim::type::instant);
+	SetupRichArticleBody(box, original->entity(), session, peer, itemId);
 
 	show->entity()->clicks() | rpl::on_next([=] {
 		show->hide(anim::type::instant);
@@ -466,10 +326,11 @@ int RichTranslateArticle::resizeGetHeight(int newWidth) {
 	}
 
 	const auto translated = box->addRow(
-		object_ptr<SlideWrap<RichTranslateArticle>>(
+		object_ptr<SlideWrap<Iv::Markdown::MarkdownDocumentWidget>>(
 			box,
-			object_ptr<RichTranslateArticle>(box, session, itemId)));
+			object_ptr<Iv::Markdown::MarkdownDocumentWidget>(box)));
 	translated->hide(anim::type::instant);
+	SetupRichArticleBody(box, translated->entity(), session, peer, itemId);
 
 	const auto error = box->addRow(object_ptr<SlideWrap<FlatLabel>>(
 		box,
@@ -493,7 +354,13 @@ int RichTranslateArticle::resizeGetHeight(int newWidth) {
 		loading->hide(anim::type::instant);
 	};
 	const auto showResult = [=](std::shared_ptr<const Iv::RichPage> result) {
-		if (result && translated->entity()->showPage(result)) {
+		if (result
+			&& ShowRichArticlePage(
+				session,
+				itemId,
+				translated->entity(),
+				&state->translated,
+				result)) {
 			translated->show(anim::type::instant);
 			loading->hide(anim::type::instant);
 		} else {
