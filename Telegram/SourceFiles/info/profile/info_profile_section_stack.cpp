ked33/tdesktop

commit 0dc2212315300d08a139956cd1b6d6d56160fb74

Fixed divider overlap on visibility change in profile section stack.

Telegram/SourceFiles/info/profile/info_profile_section_stack.cpp
@@ -13,6 +13,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/wrap/vertical_layout.h"
 #include "ui/vertical_list.h"
 #include "ui/rect_part.h"
+#include "ui/ui_utility.h"
 
 #include "styles/style_info.h"
 
@@ -235,6 +236,13 @@ void SectionStack::finalize() {
 				return (i < int(v.size())) && v[i];
 			}) | rpl::distinct_until_changed());
 	}
+	visible->changes() | rpl::on_next([layout = _layout] {
+		// A toggle landing inside a layout resize pass is skipped there,
+		// leaving the rows below the separator overlapped by it.
+		InvokeQueued(layout, [=] {
+			layout->resizeToWidth(layout->width());
+		});
+	}, _layout->lifetime());
 }
 
 } // namespace Info::Profile

commit ad30b19e9ca7e4e75887801c01f79818564a0ca5

Fixed width growth of profile section stack on visibility change.
Regression was introduced in 0dc2212315.

Telegram/SourceFiles/info/profile/info_profile_section_stack.cpp
@@ -240,7 +240,7 @@ void SectionStack::finalize() {
 		// A toggle landing inside a layout resize pass is skipped there,
 		// leaving the rows below the separator overlapped by it.
 		InvokeQueued(layout, [=] {
-			layout->resizeToWidth(layout->width());
+			layout->resizeToWidth(layout->widthNoMargins());
 		});
 	}, _layout->lifetime());
 }